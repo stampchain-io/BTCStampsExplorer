9af17ee740b830233591ef2dee2100565ef04232
javiercervilla <javier@cervilla.es>
2024-01-11 18:53:50 +0100

    fix(BigFloat):[+] Fix BigFloat issue on src/index

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -15,6 +15,8 @@ import * as $api_v2_cursed_index from "./routes/api/v2/cursed/index.ts";
 import * as $api_v2_issuances_id_ from "./routes/api/v2/issuances/[id].ts";
 import * as $api_v2_src20_balance_address_ from "./routes/api/v2/src20/balance/[address].ts";
 import * as $api_v2_src20_balance_address_tick_ from "./routes/api/v2/src20/balance/[address]/[tick].ts";
+import * as $api_v2_src20_block_block_index_tick_ from "./routes/api/v2/src20/block/[block_index]/[tick].ts";
+import * as $api_v2_src20_block_block_index_index from "./routes/api/v2/src20/block/[block_index]/index.ts";
 import * as $api_v2_src20_index from "./routes/api/v2/src20/index.ts";
 import * as $api_v2_src20_tick_tick_deploy from "./routes/api/v2/src20/tick/[tick]/deploy.ts";
 import * as $api_v2_src20_tick_tick_index from "./routes/api/v2/src20/tick/[tick]/index.ts";
@@ -66,6 +68,10 @@ const manifest = {
       $api_v2_src20_balance_address_,
     "./routes/api/v2/src20/balance/[address]/[tick].ts":
       $api_v2_src20_balance_address_tick_,
+    "./routes/api/v2/src20/block/[block_index]/[tick].ts":
+      $api_v2_src20_block_block_index_tick_,
+    "./routes/api/v2/src20/block/[block_index]/index.ts":
+      $api_v2_src20_block_block_index_index,
     "./routes/api/v2/src20/index.ts": $api_v2_src20_index,
     "./routes/api/v2/src20/tick/[tick]/deploy.ts":
       $api_v2_src20_tick_tick_deploy,
---
+++ routes/api/v2/src20/block/[block_index]/[tick].ts
@@ -0,0 +1,16 @@
+import { HandlerContext } from "$fresh/server.ts";
+import { api_get_src20_valid_tx } from "$lib/controller/wallet.ts";
+
+export const handler = async (_req: Request, ctx: HandlerContext): Response => {
+  const { block_index, tick } = ctx.params;
+  try {
+    const tx_info = await api_get_src20_valid_tx(tx_hash);
+    const body = JSON.stringify({
+      data: tx_info,
+    });
+    return new Response(body);
+  } catch {
+    const body = JSON.stringify({ error: `Error: Internal server error` });
+    return new Response(body);
+  }
+};
