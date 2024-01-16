13ab6404e9dbbd40a554c44d5fa03fd88978ca02
javiercervilla <javier@cervilla.es>
2024-01-16 11:47:28 +0100

    feat(SRC20):[+] Holders data in src20

---
+++ components/src20/SRC20HoldersInfo.tsx
@@ -0,0 +1,82 @@
+import { Chart } from "$fresh_charts/mod.ts";
+import { ChartColors, transparentize } from "$fresh_charts/utils.ts";
+import { short_address } from "utils/util.ts";
+
+interface HoldersInfoProps {
+  holders: {}[];
+  total_holders: number;
+}
+export const SRC20HoldersInfo = (props: HoldersInfoProps) => {
+  const { holders, total_holders } = props;
+  const labels = holders.map((holder) => holder.percentage);
+
+  return (
+    <div class="mx-auto w-full p-4 flex flex-col md:flex-row space-between">
+      <Chart
+        type="pie"
+        width={350}
+        options={{
+          responsive: true,
+          devicePixelRatio: 1,
+          plugins: {
+            legend: {
+              display: false,
+            },
+          },
+          //scales: { yAxes: [{ ticks: { beginAtZero: true } }] },
+        }}
+        data={{
+          datasets: [{
+            label: "Holders",
+            data: labels,
+            borderColor: "#000000",
+            backgroundColor: [
+              transparentize(ChartColors.Red, 0.5),
+              transparentize(ChartColors.Orange, 0.5),
+              transparentize(ChartColors.Yellow, 0.5),
+              transparentize(ChartColors.Green, 0.5),
+              transparentize(ChartColors.Blue, 0.5),
+            ],
+            borderWidth: 1,
+          }],
+        }}
+      />
+      <div class="flex flex-col gap-2 items-center w-full">
+        <div class="text-3xl font-bold text-center">
+          {total_holders}
+        </div>
+        <div class="text-xl font-bold text-center">
+          Total Holders
+        </div>
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg max-h-72 w-full md:px-16">
+          <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" class="px-6 py-3">address</th>
+                <th scope="col" class="px-6 py-3">amount</th>
+                <th scope="col" class="px-6 py-3">%</th>
+              </tr>
+            </thead>
+            <tbody>
+              {holders.map((src20) => {
+                return (
+                  <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                    <td class="px-6 py-4">
+                      {short_address(src20.address)}
+                    </td>
+                    <td class="px-6 py-4">
+                      {src20.amt}
+                    </td>
+                    <td class="px-6 py-4 text-sm">
+                      {src20.percentage}%
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
--- components/src20/TickInfo.tsx
+++ components/src20/TickInfo.tsx
@@ -1,69 +1,34 @@
-import { convertToEmoji } from "utils/util.ts";
+import { SRC20TickHeader } from "./SRC20TickHeader.tsx";
+import { SRC20HoldersInfo } from "./SRC20HoldersInfo.tsx";
+//import { SRC20HoldersInfo } from "$islands/src20/SRC20HoldersInfo.tsx";
 
 export const TickInfo = ({
   deployment,
   mint_status,
   total_holders,
+  holders,
 }) => {
   if (
     !deployment ||
     !mint_status ||
-    !total_holders
+    !total_holders ||
+    !holders
   ) {
     return null;
   }
 
   deployment = deployment[0];
   return (
-    <div class="flex w-full flex-col md:flex-row gap-4 items-center justify-center">
-      <img
-        src={`/content/${deployment.tx_hash}.svg`}
-        class="w-full h-full md:w-60 md:h-60"
+    <div class="flex flex-col gap-2">
+      <SRC20TickHeader
+        deployment={deployment}
+        mint_status={mint_status}
+        total_holders={total_holders}
       />
-
-      <div class="relative w-full overflow-x-auto shadow-md sm:rounded-lg">
-        <table class="w-full text-sm text-center rtl:text-right text-gray-500 dark:text-gray-400 uppercase">
-          <tbody>
-            <tr class="border-b">
-              <th scope="row" class="whitespace-nowrap px-6 py-3">Tick</th>
-              <td class="whitespace-nowrap">
-                {convertToEmoji(deployment.tick)}
-              </td>
-              <th scope="row" class="whitespace-nowrap px-6 py-3">Block</th>
-              <td class="whitespace-nowrap">
-                {deployment.block_index}
-              </td>
-              <th scope="row" class="px-6 py-3">Time</th>
-              <td class="whitespace-nowrap">
-                {new Date(deployment.block_time).toLocaleString("default", {
-                  month: "short",
-                  year: "numeric",
-                })}
-              </td>
-            </tr>
-            <tr>
-              <th scope="row" class="whitespace-nowrap px-6 py-3">
-                Total Supply
-              </th>
-              <td class="whitespace-nowrap">
-                {deployment.max}
-              </td>
-              <th scope="row" class="whitespace-nowrap px-6 py-3">
-                Total Holders
-              </th>
-              <td class="whitespace-nowrap">
-                {total_holders}
-              </td>
-              <th scope="row" class="whitespace-nowrap px-6 py-3">
-                Total minted
-              </th>
-              <td class="whitespace-nowrap">
-                {mint_status.progress}%
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <SRC20HoldersInfo
+        holders={holders}
+        total_holders={total_holders}
+      />
     </div>
   );
 };
