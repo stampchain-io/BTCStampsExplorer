bcdbc6426d2ba1f36fd7a708ada680b4b4bd1a33
javiercervilla <javier@cervilla.es>
2024-01-16 23:23:22 +0100

    feat(SRC20):[+] Holders data in src20

--- components/src20/SRC20HoldersInfo.tsx
+++ components/src20/SRC20HoldersInfo.tsx
@@ -81,6 +81,9 @@ export const SRC20HoldersInfo = (props: HoldersInfoProps) => {
         </div>
         <div class="relative overflow-x-auto shadow-md sm:rounded-lg max-h-80 py-4 w-full">
           <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <caption class="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white dark:text-white dark:bg-gray-800">
+              Holders
+            </caption>
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
               <tr>
                 <th scope="col" class="px-6 py-3">address</th>
