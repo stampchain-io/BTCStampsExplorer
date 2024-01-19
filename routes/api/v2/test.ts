5c57aa0a16833ebbe881bfb1a42d331f7fb3bb01
javiercervilla <javier@cervilla.es>
2024-01-19 02:34:45 +0100

    fix(env_vars):[+] Testing env vars

--- routes/api/v2/test.ts
+++ routes/api/v2/test.ts
@@ -1,12 +1,15 @@
-import { conf } from "../../../lib/utils/config.ts";
+import { conf } from "utils/config.ts";
 
 export const handler = async (
   _req: Request,
   ctx,
 ): Promise<Response> => {
+  console.log(conf);
+  const img_from_env = Deno.env.get("IMAGES_SRC_PATH");
   const body = JSON.stringify({
     img_path: conf.IMAGES_SRC_PATH,
     fee: conf.MINTING_SERVICE_FEE_FIXED_SATS,
+    img_from_env,
   });
   return new Response(body);
 };
