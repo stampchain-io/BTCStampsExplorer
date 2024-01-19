57500066d08595918895a2b8d1a5376834a06416
javiercervilla <javier@cervilla.es>
2024-01-19 02:16:19 +0100

    fix(tailwind-build):[+] Fix tailwind build removing swaggerui.css

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -12,8 +12,7 @@ export default function App({ Component }: AppProps) {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Bitcoin stamps</title>
-        {/* <link rel="stylesheet" href="/styles.css" /> */}
-        <link rel="stylesheet" href="/main.css" />
+        <link rel="stylesheet" href="/styles.css" />
       </head>
       <body class="bg-black">
         <ToastProvider>
