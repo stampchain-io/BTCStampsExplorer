fd80502bea2d91af6bbf5d34c8c0707784ed7e8e
derp <derp@derpfinance.com>
2024-01-22 16:57:26 -0500

    added binary returning for any item that doesnt have a local file

--- routes/s/[...id].tsx
+++ routes/s/[...id].tsx
@@ -1,7 +1,9 @@
 import { connectDb, StampsClass } from "$lib/database/index.ts";
+import { CommonClass } from "../../lib/database";
+import * as base64 from "https://denopkg.com/chiefbiiko/base64/mod.ts";
 
 export const handler: Handlers<StampRow> = {
-  async GET(req: Request, ctx: HandlerContext) {
+  async GET(req: Request, ctx: HandlerContext) {   
     const { id } = ctx.params;
     const client = await connectDb();
     const file_name = await StampsClass
@@ -9,10 +11,22 @@ export const handler: Handlers<StampRow> = {
         client,
         id as string,
       );
-    client.close();
+    // if the file doesnt exist, get the base64
     if (!file_name) {
-      return ctx.renderNotFound();
+      const issuance_data = await CommonClass
+      .get_issuances_by_identifier_with_client(
+        client,
+        id as string,
+      );
+      // if the base64 exists, convert it to a uint8array and return it
+      if(issuance_data.stamp_base64){
+        return new Response(base64.toUint8Array(issuance_data.stamp_base64))        
+      }
+      // otherwise, 404
+      else
+        return ctx.renderNotFound();
     }
+    client.close();
     return new Response("", {
       status: 301,
       headers: { Location: `/content/${file_name}` },
