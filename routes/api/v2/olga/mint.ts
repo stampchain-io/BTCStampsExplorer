d4b406f3f78f2ed734d73fe3a14c9a2f7aed58c8
javiercervilla <javier@cervilla.es>
2024-04-03 18:02:41 +0200

    feat(OPENAPI):[+] Add open api docs for minting olga stamps

--- routes/api/v2/olga/mint.ts
+++ routes/api/v2/olga/mint.ts
@@ -4,6 +4,40 @@ import { conf } from "utils/config.ts";
 import { mintStampCIP33 } from "utils/minting/olga/mint.ts";
 import { generateAvailableAssetName } from "utils/minting/stamp.ts";
 
+/**
+ * @swagger
+ * /api/v2/olga/mint:
+ *   post:
+ *     summary: Mint a new OLGA STAMP.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/MintStampInputData'
+ *     responses:
+ *       '200':
+ *         description: Successful response with the hex value.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 hex:
+ *                   type: string
+ *                   description: The hex value.
+ *       '400':
+ *         description: Bad request error response.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: The error message.
+ */
+
 export const handler: Handlers<TX | TXError> = {
   async POST(req: Request, _ctx: FreshContext) {
     const body: MintStampInputData = await req.json();
