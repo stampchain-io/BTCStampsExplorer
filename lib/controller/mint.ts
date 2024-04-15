abf9fe46e9d6d555267a364edef23096406d66fa
top0xdev <superdev0x7e7@gmail.com>
2024-04-15 11:00:57 -0400

    update mint page

---
+++ lib/controller/mint.ts
@@ -0,0 +1,9 @@
+import axiod from "https://deno.land/x/axiod@0.26.2/mod.ts";
+
+export async function api_post_mint(data: any) {
+  try {
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
