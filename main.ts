0914e2b2374d7ecbfb599861d2f2cbed22f929ad
Reinamora <reinamora@stampchain.io>
2024-05-08 02:23:22 +0000

    chore: redis caching

--- main.ts
+++ main.ts
@@ -10,4 +10,18 @@ import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import config from "./fresh.config.ts";
 
-await start(manifest, config);
+// Import connectToRedis from the appropriate module
+import { connectToRedis } from "./lib/utils/cache.ts";
+
+async function startApp() {
+  // Connect to Redis
+  await connectToRedis();
+
+  // Start your server
+  await start(manifest, config);
+}
+
+startApp().catch((error) => {
+  console.error("Failed to start application:", error);
+  process.exit(1);
+});
