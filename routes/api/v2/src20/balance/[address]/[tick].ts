15c03db8b02df516ed0d0384320640d707901e10
Reinamora <kevin@moonblink.com>
2024-05-24 18:59:32 +0000

    fix: api output: https://app.apiary.io/public/tests/run/e4c84f71-76e2-44d0-9564-83067aaa52f2

--- routes/api/v2/src20/balance/[address]/[tick].ts
+++ routes/api/v2/src20/balance/[address]/[tick].ts
@@ -1,47 +1,32 @@
 import { CommonClass, getClient, Src20Class } from "$lib/database/index.ts";
 import { Client } from "$mysql/mod.ts";
-import {
-  AddressHandlerContext,
-  PaginatedSrc20BalanceResponseBody,
-} from "globals";
+import { AddressTickHandlerContext, Src20BalanceResponseBody } from "globals";
 import { ResponseUtil } from "utils/responseUtil.ts"; // Import the responseUtil helper
 
 export const handler = async (
-  req: Request,
-  ctx: AddressHandlerContext,
+  _req: Request,
+  ctx: AddressTickHandlerContext,
 ): Promise<Response> => {
-  const { address } = ctx.params;
-  const url = new URL(req.url);
-  const params = url.searchParams;
-  const limit = Number(params.get("limit")) || 1000;
-  const page = Number(params.get("page")) || 1;
-  const amt = params.get("amt");
-  const sort = params.get("sort") || "ASC";
+  const { address, tick } = ctx.params;
   try {
     const client = await getClient();
     const last_block = await CommonClass.get_last_block_with_client(client);
     const src20 = await Src20Class.get_src20_balance_with_client(
       client as Client,
       address,
-      null,
-      Number(amt) || undefined,
-      limit,
-      page,
-      sort,
+      tick.toString(),
     );
-    const total = src20.length;
 
-    const body: PaginatedSrc20BalanceResponseBody = {
-      page: page,
-      limit: limit,
-      totalPages: Math.ceil(total / limit),
-      total: total,
+    if (!src20) {
+      return ResponseUtil.error(`Error: SRC20 balance not found`, 404);
+    }
+
+    const body: Src20BalanceResponseBody = {
       last_block: last_block.rows[0]["last_block"],
       data: src20,
     };
     return ResponseUtil.success(body);
-  } catch (error) {
-    console.error("Failed to get SRC20 balance:", error);
+  } catch (_error) {
     return ResponseUtil.error(`Error: Internal server error`);
   }
 };
