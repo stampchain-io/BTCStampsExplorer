a234c25477b1711ac3e810d0ad7298508fcbde52
top0xdev <superdev0x7e7@gmail.com>
2024-05-25 22:28:50 -0400

    update price

--- routes/quicknode/getPrice.ts
+++ routes/quicknode/getPrice.ts
@@ -3,7 +3,6 @@ import { fetch_quicknode } from "utils/quicknode.ts";
 export async function handler(req: Request): Promise<Response> {
   const { name, params } = await req.json();
   const stampBalance = await fetch_quicknode(name, params);
-  console.log(stampBalance);
   return new Response(
     JSON.stringify({ price: stampBalance.result.bitcoin.usd }),
     {
