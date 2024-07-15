818f0437afab74a83aee3d4f8f55805f91803735
dev4pear <tommycole519@gmail.com>
2024-07-15 12:18:50 -0400

    update twind

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -54,6 +54,7 @@ import * as $Footer from "./islands/Footer.tsx";
 import * as $Header from "./islands/Header.tsx";
 import * as $MempoolWeather from "./islands/MempoolWeather.tsx";
 import * as $Navigator_navigator from "./islands/Navigator/navigator.tsx";
+import * as $PageControl from "./islands/PageControl.tsx";
 import * as $StampNavigator from "./islands/StampNavigator.tsx";
 import * as $StampSearch from "./islands/StampSearch.tsx";
 import * as $Toast_ToastComponent from "./islands/Toast/ToastComponent.tsx";
@@ -155,6 +156,7 @@ const manifest = {
     "./islands/Header.tsx": $Header,
     "./islands/MempoolWeather.tsx": $MempoolWeather,
     "./islands/Navigator/navigator.tsx": $Navigator_navigator,
+    "./islands/PageControl.tsx": $PageControl,
     "./islands/StampNavigator.tsx": $StampNavigator,
     "./islands/StampSearch.tsx": $StampSearch,
     "./islands/Toast/ToastComponent.tsx": $Toast_ToastComponent,
