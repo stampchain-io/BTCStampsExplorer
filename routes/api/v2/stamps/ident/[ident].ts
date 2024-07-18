0009b6e250d044f935364d54d0ba687e9c7a0615
reinamora <reinamora@stampchain.io>
2024-07-18 21:40:36 +0000

    fix: ident query

--- routes/api/v2/stamps/ident/[ident].ts
+++ routes/api/v2/stamps/ident/[ident].ts
@@ -1,28 +1,55 @@
-import { CommonClass, getClient, StampsClass } from "$lib/database/index.ts";
+import { withDatabaseClient } from "$lib/services/databaseService.ts";
+import { CommonClass, StampsClass } from "$lib/database/index.ts";
 import { PROTOCOL_IDENTIFIERS } from "$lib/utils/protocol.ts";
-import { IdentHandlerContext, StampResponseBody } from "globals";
+import { IdentHandlerContext, PaginatedStampResponseBody } from "globals";
 import { ResponseUtil } from "utils/responseUtil.ts";
+import { getPaginationParams } from "$lib/utils/paginationUtils.ts";
+import { paginate } from "$lib/utils/util.ts";
 
 export const handler = async (
-  _req: Request,
+  req: Request,
   ctx: IdentHandlerContext,
 ): Promise<Response> => {
   const { ident } = ctx.params;
   if (!PROTOCOL_IDENTIFIERS.includes(ident.toUpperCase())) {
     return ResponseUtil.error(`Error: ident: ${ident} not found`, 404);
   }
+
+  const url = new URL(req.url);
+  const { limit, page } = getPaginationParams(url);
+  const sort_order = (url.searchParams.get("sort_order") as "asc" | "desc") ||
+    "asc";
+
   try {
-    const client = await getClient();
-    const data = await StampsClass.get_stamps(client, {
-      type: "stamps",
-      ident: ident.toUpperCase(),
+    return await withDatabaseClient(async (client) => {
+      const [data, totalResult, lastBlock] = await Promise.all([
+        StampsClass.get_stamps(client, {
+          limit,
+          page,
+          sort_order,
+          type: "stamps",
+          ident: ident.toUpperCase(),
+          all_columns: true,
+        }),
+        StampsClass.get_total_stamp_count(
+          client,
+          "stamps",
+          ident.toUpperCase(),
+        ),
+        CommonClass.get_last_block_with_client(client),
+      ]);
+
+      const total = totalResult.rows[0]["total"];
+      const pagination = paginate(total, page, limit);
+
+      const body: PaginatedStampResponseBody = {
+        ...pagination,
+        last_block: lastBlock.rows[0]["last_block"],
+        data: data.rows,
+      };
+
+      return ResponseUtil.success(body);
     });
-    const last_block = await CommonClass.get_last_block_with_client(client);
-    const body: StampResponseBody = {
-      last_block: last_block.rows[0]["last_block"],
-      data: data.rows[0], // Assuming get_stamps returns an array, we take the first (and only) element
-    };
-    return ResponseUtil.success(body);
   } catch (error) {
     console.error("Error:", error);
     return ResponseUtil.error(
