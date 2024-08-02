accbcd19392714581df3444032d9464ac305e757
dev4pear <tommycole519@gmail.com>
2024-08-02 09:07:18 -0400

    feat: update tab contents for wallet page

--- islands/Wallet/TransactionsContent.tsx
+++ islands/Wallet/TransactionsContent.tsx
@@ -1,4 +1,5 @@
 import { useState } from "preact/hooks";
+import { TransactionsTable } from "$components/wallet/TransactionsTable.tsx";
 
 export const TransactionsContent = () => {
   const [selectedButton, setSelectedButton] = useState("Send");
@@ -7,28 +8,25 @@ export const TransactionsContent = () => {
     <>
       <div class="flex items-center justify-end">
         <button
-          className={
-            "border-2 border-[#2B0E49] min-w-[120px] py-[20px] text-[19px] font-semibold " +
+          className={"border-2 border-[#2B0E49] min-w-[120px] py-[20px] text-[19px] font-semibold " +
             (selectedButton == "Send"
               ? "bg-[#321A43] text-[#7A00F5]"
-              : "bg-[#4A3757] text-[#B9B9B9]")
-          }
+              : "bg-[#4A3757] text-[#B9B9B9]")}
           onClick={() => setSelectedButton("Send")}
         >
           Send
         </button>
         <button
-          className={
-            "border-2 border-[#2B0E49] min-w-[120px] py-[20px] text-[19px] font-semibold " +
+          className={"border-2 border-[#2B0E49] min-w-[120px] py-[20px] text-[19px] font-semibold " +
             (selectedButton == "Receive"
               ? "bg-[#321A43] text-[#7A00F5]"
-              : "bg-[#4A3757] text-[#B9B9B9]")
-          }
+              : "bg-[#4A3757] text-[#B9B9B9]")}
           onClick={() => setSelectedButton("Receive")}
         >
           Receive
         </button>
       </div>
+      <TransactionsTable />
     </>
   );
 };
