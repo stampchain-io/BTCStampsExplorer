6dae36e2ddfbb4580995645fdca0b8d1531636b9
dev4pear <tommycole519@gmail.com>
2024-08-02 09:07:18 -0400

    feat: update tab contents for wallet page

--- islands/Wallet/MyItemsContent.tsx
+++ islands/Wallet/MyItemsContent.tsx
@@ -3,6 +3,8 @@ import { useState } from "preact/hooks";
 import { StampNavigator } from "$islands/stamp/StampNavigator.tsx";
 import { StampSearchClient } from "$islands/stamp/StampSearch.tsx";
 
+import { MyItemCard } from "$components/wallet/MyItemCard.tsx";
+
 export const MyItemsContent = ({
   filterBy,
   sortBy,
@@ -15,31 +17,25 @@ export const MyItemsContent = ({
   return (
     <>
       <div class="flex flex-col-reverse md:flex-row justify-between items-center w-full">
-        <div class="hidden md:flex gap-6 md:gap-8 items-center">
+        <div class="hidden md:flex gap-[10px] items-center">
           <img
-            src={
-              selectedCategory === "BigTiles"
-                ? "/img/icon_big_tiles.png"
-                : "/img/icon_big_tiles.png"
-            }
+            src={selectedCategory === "BigTiles"
+              ? "/img/icon_big_tiles.png"
+              : "/img/icon_big_tiles.png"}
             onClick={() => setSelectedCategory("BigTiles")}
             class="bg-[#3F2A4E] p-4 w-14 h-14 cursor-pointer"
           />
           <img
-            src={
-              selectedCategory === "SmallTiles"
-                ? "/img/icon_small_tiles_selected.png"
-                : "/img/icon_small_tiles_selected.png"
-            }
+            src={selectedCategory === "SmallTiles"
+              ? "/img/icon_small_tiles_selected.png"
+              : "/img/icon_small_tiles_selected.png"}
             onClick={() => setSelectedCategory("SmallTiles")}
             class="bg-[#3F2A4E] p-4 w-14 h-14 cursor-pointer"
           />
           <img
-            src={
-              selectedCategory === "List"
-                ? "/img/icon_lists.png"
-                : "/img/icon_lists.png"
-            }
+            src={selectedCategory === "List"
+              ? "/img/icon_lists.png"
+              : "/img/icon_lists.png"}
             onClick={() => setSelectedCategory("List")}
             class="bg-[#3F2A4E] py-5 px-2 w-14 h-14 cursor-pointer"
           />
@@ -57,6 +53,9 @@ export const MyItemsContent = ({
           <StampNavigator initFilter={filterBy} initSort={sortBy} />
           <StampSearchClient />
         </div>
+      </div>
+      <div class="grid grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-4 py-6 transition-opacity duration-700 ease-in-out">
+        {[...Array(5)].map((item) => <MyItemCard item={item} />)}
       </div>
     </>
   );
--- islands/Wallet/WalletHeader.tsx
+++ islands/Wallet/WalletHeader.tsx
@@ -11,109 +11,93 @@ export const WalletHeader = ({
 
   return (
     <div class="flex flex-col gap-3">
-      <div class="flex gap-4">
-        <img
-          src="/img/mock.png"
-          loading="lazy"
-          onError={(e) => {
-            e.currentTarget.src = `/not-available.png`;
-          }}
-          alt="collection image"
-          class="h-24 w-24 object-contain items-center standalone:h-24 standalone:w-auto pixelart image-rendering-pixelated"
-        />
-        <div class="flex flex-col justify-between">
-          <p class="font-semibold text-xl text-white">Collection 1</p>
-          <div class="flex flex-col md:flex-row gap-4">
-            <div class="flex gap-4">
-              <div>
-                <p class="text-sm text-[#B9B9B9]">FLOOR</p>
-                <p class="text-lg font-semibold text-white">
-                  0.00018{" "}
-                  <span class="text-sm font-normal text-[#B9B9B9]">BTC</span>
-                </p>
-              </div>
-              <div>
-                <p class="text-sm text-[#B9B9B9]">TOTAL VOL</p>
-                <p class="text-lg font-semibold text-white">
-                  0.0019{" "}
-                  <span class="text-sm font-normal text-[#B9B9B9]">BTC</span>
-                </p>
-              </div>
-            </div>
-            <div class="flex gap-4">
-              <div>
-                <p class="text-sm text-[#B9B9B9]">OWNERS</p>
-                <p class="text-lg font-semibold text-white">1.1K</p>
-              </div>
-              <div>
-                <p class="text-sm text-[#B9B9B9]">LISTED</p>
-                <p class="text-lg font-semibold text-white">41</p>
-              </div>
-              <div>
-                <p class="text-sm text-[#B9B9B9]">TOTAL SUPLY</p>
-                <p class="text-lg font-semibold text-white">21K</p>
-              </div>
-            </div>
+      <div className="flex justify-between">
+        <div class="flex gap-4">
+          <img
+            src="/img/mock.png"
+            loading="lazy"
+            onError={(e) => {
+              e.currentTarget.src = `/not-available.png`;
+            }}
+            alt="collection image"
+            class="h-24 w-24 object-contain items-center standalone:h-24 standalone:w-auto pixelart image-rendering-pixelated"
+          />
+          <div class="flex flex-col justify-between">
+            <p class="font-semibold text-[18px] text-white flex gap-2 items-center">
+              <img src="/img/icon_btc.png" className="w-5 h-5" alt="" />
+              <span className="hidden md:block">
+                bc1p2dds1a421xvf53e2dcx6vxcg423sl64r
+              </span>
+              <span className="block md:hidden">bc1p2.....sl64r</span>
+              <img
+                src="/img/icon_copy_to_clipboard.png"
+                className="w-4 h-4"
+                alt=""
+              />
+            </p>
+            <p class="font-semibold text-[26px] text-white">
+              0.1326342 BTC
+            </p>
           </div>
         </div>
+        <div className="w-[137px] hidden md:block">
+          <div className="flex justify-between items-center">
+            <p className="text-[#B9B9B9] text-[14px]">Display name</p>
+            <img src="/img/wallet/icon-edit.svg" alt="" />
+          </div>
+          <p className="text-[18px] text-[#F5F5F5] mb-[19px]">NameHere</p>
+          <div className="flex justify-between items-center">
+            <p className="text-[#B9B9B9] text-[14px]">X (Twitter)</p>
+            <img src="/img/wallet/icon-edit.svg" alt="" />
+          </div>
+          <p className="text-[18px] text-[#F5F5F5]">NameHere</p>
+        </div>
       </div>
-      <div class="flex gap-6 md:gap-8 items-end">
+      <div class="flex gap-6 md:gap-8 items-end overflow-auto">
         <p
-          class={
-            selectedTab === "my_items"
-              ? "text-[19px] text-[#7A00F5] font-semibold cursor-pointer pb-4 border-b-4 border-b-[#7A00F5]"
-              : "text-[19px] text-[#B9B9B9] cursor-pointer pb-4"
-          }
+          class={selectedTab === "my_items"
+            ? "text-[19px] text-[#7A00F5] font-semibold cursor-pointer pb-4 border-b-4 border-b-[#7A00F5]"
+            : "text-[19px] text-[#B9B9B9] cursor-pointer pb-4"}
           onClick={() => setTypeOption("wallet/" + address, "my_items")}
         >
           My Items
         </p>
         <p
-          class={
-            selectedTab === "stamps"
-              ? "text-[19px] text-[#7A00F5] font-semibold cursor-pointer pb-4 border-b-4 border-b-[#7A00F5]"
-              : "text-[19px] text-[#B9B9B9] cursor-pointer pb-4"
-          }
+          class={selectedTab === "stamps"
+            ? "text-[19px] text-[#7A00F5] font-semibold cursor-pointer pb-4 border-b-4 border-b-[#7A00F5]"
+            : "text-[19px] text-[#B9B9B9] cursor-pointer pb-4"}
           onClick={() => setTypeOption("wallet/" + address, "stamps")}
         >
           STAMPS
         </p>
         <p
-          class={
-            selectedTab === "src20"
-              ? "text-[19px] text-[#7A00F5] font-semibold cursor-pointer pb-4 border-b-4 border-b-[#7A00F5]"
-              : "text-[19px] text-[#B9B9B9] cursor-pointer pb-4"
-          }
+          class={selectedTab === "src20"
+            ? "text-[19px] text-[#7A00F5] font-semibold cursor-pointer pb-4 border-b-4 border-b-[#7A00F5]"
+            : "text-[19px] text-[#B9B9B9] cursor-pointer pb-4"}
           onClick={() => setTypeOption("wallet/" + address, "src20")}
         >
           Src20
         </p>
         <p
-          class={
-            selectedTab === "src721"
-              ? "text-[19px] text-[#7A00F5] font-semibold cursor-pointer pb-4 border-b-4 border-b-[#7A00F5]"
-              : "text-[19px] text-[#B9B9B9] cursor-pointer pb-4"
-          }
+          class={selectedTab === "src721"
+            ? "text-[19px] text-[#7A00F5] font-semibold cursor-pointer pb-4 border-b-4 border-b-[#7A00F5]"
+            : "text-[19px] text-[#B9B9B9] cursor-pointer pb-4"}
           onClick={() => setTypeOption("wallet/" + address, "src721")}
         >
           Src721
         </p>
         <p
-          class={
-            selectedTab === "transactions"
-              ? "text-[19px] text-[#7A00F5] font-semibold cursor-pointer pb-4 border-b-4 border-b-[#7A00F5]"
-              : "text-[19px] text-[#B9B9B9] cursor-pointer pb-4"
-          }
+          class={selectedTab === "transactions"
+            ? "text-[19px] text-[#7A00F5] font-semibold cursor-pointer pb-4 border-b-4 border-b-[#7A00F5]"
+            : "text-[19px] text-[#B9B9B9] cursor-pointer pb-4"}
           onClick={() => setTypeOption("wallet/" + address, "transactions")}
         >
           Transactions
         </p>
         <p
-          class={
-            selectedTab === "history"
-              ? "text-[19px] text-[#7A00F5] font-semibold cursor-pointer pb-4 border-b-4 border-b-[#7A00F5]"
-              : "text-[19px] text-[#B9B9B9] cursor-pointer pb-4"
-          }
+          class={selectedTab === "history"
+            ? "text-[19px] text-[#7A00F5] font-semibold cursor-pointer pb-4 border-b-4 border-b-[#7A00F5]"
+            : "text-[19px] text-[#B9B9B9] cursor-pointer pb-4"}
           onClick={() => setTypeOption("wallet/" + address, "history")}
         >
           History
