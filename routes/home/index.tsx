c2f21c72bf2dafe3004dd188ce288ff4ffe08940
reinamora <reinamora@stampchain.io>
2024-08-07 14:52:14 +0000

    chore: restore pagination on stamps page, fix home page
    - sanitized the home page to remove query params
    - fixes pagination on stamp page
    
    additional work needed on display of cursed vs posh on the /stamp route. Still need to fetch the proper data for top/new tables on home page.

--- routes/home/index.tsx
+++ routes/home/index.tsx
@@ -1,16 +1,8 @@
 import { StampRow } from "globals";
-
-import { Pagination } from "$islands/pagination/Pagination.tsx";
 import { Handlers } from "$fresh/server.ts";
-
-import { StampNavigator } from "$islands/stamp/StampNavigator.tsx";
-import { StampSearchClient } from "$islands/stamp/StampSearch.tsx";
-
 import { HomeHeader } from "$islands/home/HomeHeader.tsx";
 import { HomeTable } from "$islands/home/HomeTable.tsx";
 import { HomeStampPreview } from "$islands/home/HomeStampPreview.tsx";
-import { HomeStampPreviewDetails } from "$islands/home/HomeStampPreviewDetails.tsx";
-
 import { StampController } from "$lib/controller/stampController.ts";
 
 type HomePageProps = {
@@ -27,36 +19,14 @@ type HomePageProps = {
     filterBy: any[];
     sortBy: string;
     src20s: any[];
-    page_src20: number;
-    pages_src20: number;
-    page_size_src20: number;
     type: string;
   };
 };
 
 export const handler: Handlers = {
-  async GET(req: Request, ctx) {
+  async GET(_req: Request, ctx) {
     try {
-      const url = new URL(req.url);
-      const type = url.searchParams.get("type");
-      const page = Number(url.searchParams.get("page")) || 0;
-      const page_size = Number(url.searchParams.get("limit")) ||
-        (type ? 24 : 10);
-      const filterBy = url.searchParams.get("filterBy")?.split(",") || [];
-      const sortBy = url.searchParams.get("sortBy") || "none";
-      const orderByParam = url.searchParams.get("orderBy")?.toUpperCase();
-      const orderBy = orderByParam === "DESC" || orderByParam === "DESC"
-        ? orderByParam
-        : "DESC";
-
-      const result = await StampController.getHomePageData(
-        type,
-        page,
-        page_size,
-        filterBy,
-        sortBy,
-        orderBy,
-      );
+      const result = await StampController.getHomePageData();
 
       return ctx.render(result);
     } catch (error) {
@@ -73,83 +43,22 @@ export default function Home(props: HomePageProps) {
     stamps_art,
     stamps_src20,
     stamps_posh,
-    stamps,
     src20s,
-    type,
-    pages_stamp,
-    page_stamp,
-    page_size_stamp,
-    pages_src20,
-    page_src20,
-    page_size_src20,
-    filterBy,
-    sortBy,
   } = props.data;
 
-  const stampDetailsInfoTitle: { [key: string]: string } = {
-    "recent": "Recent Sales", // TODO: need to fecth these from the xcp api
-    "src721": "Src721",
-    "classic": "Classic",
-    "src20": "Src20",
-    "posh": "Posh",
-  };
-
   return (
-    <>
-      {type
-        ? (
-          <div class="">
-            <div class="flex flex-col-reverse md:flex-row items-start justify-between border-b border-[#3F2A4E]">
-              <a
-                href="/home"
-                class="text-[#7A00F5] text-[26px] pb-4 border-b-4 border-[#7A00F5] flex gap-1"
-              >
-                <img
-                  src="/img/icon_arrow_left.png"
-                  className="w-5 h-10"
-                  alt=""
-                />
-                {stampDetailsInfoTitle[type]}
-              </a>
-              <div class="flex gap-6">
-                <StampNavigator initFilter={filterBy} initSort={sortBy} />
-                <StampSearchClient />
-              </div>
-            </div>
-            <HomeStampPreviewDetails
-              stamps={stamps}
-            />
-            <Pagination
-              data_length={stamps.length}
-              page={page_stamp}
-              pages={pages_stamp}
-              page_size={page_size_stamp}
-              type="home"
-            />
-          </div>
-        )
-        : (
-          <div class="flex flex-col gap-24 text-white">
-            <HomeHeader />
-            <div class="flex flex-col gap-10">
-              <HomeTable data={src20s} />
-              <Pagination
-                data_length={src20s.length}
-                page={page_src20}
-                pages={pages_src20}
-                page_size={page_size_src20}
-                type="home"
-              />
-            </div>
-            <HomeStampPreview
-              stamps_recent={stamps_recent}
-              stamps_src721={stamps_src721}
-              stamps_art={stamps_art}
-              stamps_src20={stamps_src20}
-              stamps_posh={stamps_posh}
-            />
-          </div>
-        )}
-    </>
+    <div class="flex flex-col gap-24 text-white">
+      <HomeHeader />
+      <div class="flex flex-col gap-10">
+        <HomeTable data={src20s} />
+      </div>
+      <HomeStampPreview
+        stamps_recent={stamps_recent}
+        stamps_src721={stamps_src721}
+        stamps_art={stamps_art}
+        stamps_src20={stamps_src20}
+        stamps_posh={stamps_posh}
+      />
+    </div>
   );
 }
