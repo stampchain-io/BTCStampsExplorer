7b3b0fe4edde9d4c2f4ac71740fb8bac1d938125
reinamora <reinamora@stampchain.io>
2024-08-21 16:49:30 +0000

    chore: reactivate carousel for more testing

--- islands/Carousel.tsx
+++ islands/Carousel.tsx
@@ -2,107 +2,97 @@ import { useSignal } from "@preact/signals";
 import { useEffect } from "preact/hooks";
 import IconCircleChevronsRight from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/circle-chevrons-right.tsx";
 import IconCircleChevronsLeft from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/circle-chevrons-left.tsx";
-import { asset } from "$fresh/runtime.ts";
-// import { tw } from "twind";
+import { IS_BROWSER } from "$fresh/runtime.ts";
 
 const SLIDE_DATA = [
-  { url: asset("/snow_den.png"), alt: "snowy deno" },
-  { url: asset("/v1.png"), alt: "deno world is raining" },
-  { url: asset("/deno_news.png"), alt: "deno news" },
-  { url: asset("/deno_city.jpeg"), alt: "deno city" },
+  { url: "img/home/carousel1.png", alt: "snowy deno" },
+  { url: "img/home/carousel2.png", alt: "deno world is raining" },
+  { url: "img/home/carousel3.png", alt: "deno news" },
+  { url: "img/home/carousel2.png", alt: "deno city" },
 ];
 
-const Slide = (props) => {
-  const { index, data } = props;
-  if (props.class === undefined) props.class = "";
+const Slide = (
+  props: { index: number; data: { url: string; alt: string }; class?: string },
+) => {
   return (
     <img
-      src={data.url}
-      alt={data.alt}
-      index={index}
-      class={props.class}
+      src={props.data.url}
+      alt={props.data.alt}
+      class={`slide absolute top-0 left-0 transition-all ease-in-out duration-700 transform ${props.class}`}
+      style="background-color: white; border: 1px solid red;" // Add this line
     />
   );
 };
 
-const Carousel = (props) => {
+const Carousel = (
+  props: {
+    showNavigation?: boolean;
+    automatic?: boolean;
+    interval?: number;
+    class?: string;
+  },
+) => {
   const NAVIGATION_COLOR = `text-white`;
   const CHEVRON_STYLE =
     `absolute z-30 w-10 h-10 hover:text-grey ${NAVIGATION_COLOR} cursor-pointer`;
-  const SHOW_NAVIGATION = props.showNavigation === false ? false : true;
-  const SLIDE_INTERVAL = parseInt(props.interval)
-    ? parseInt(props.interval)
-    : 3500;
-  const currentSlide = useSignal(
-    parseInt(props.currentSlide) ? parseInt(props.currentSlide) : 0,
-  );
-  const automatic = useSignal(props.automatic === false ? false : true);
+  const SHOW_NAVIGATION = props.showNavigation ?? true;
+  const SLIDE_INTERVAL = props.interval ?? 3500;
+  const currentSlide = useSignal(0);
+  const automatic = useSignal(props.automatic ?? true);
 
-  const slideClasses = (idx = 0) => {
+  const slideClasses = (idx: number) => {
     let outgoingSlide = currentSlide.value - 1;
     let incomingSlide = currentSlide.value + 1;
     if (outgoingSlide === -1) outgoingSlide = SLIDE_DATA.length - 1;
     if (incomingSlide === SLIDE_DATA.length) incomingSlide = 0;
-    // console.log(outgoingSlide, currentSlide.value, incomingSlide)
-    const TRANSITION_CLASS = () => {
-      if (currentSlide.value === idx) return "translate-x-0 z-20";
-      if (incomingSlide === idx) return "translate-x-full z-10";
-      if (outgoingSlide === idx) return "-translate-x-full z-10";
-      return "translate-x-full";
-    };
-    // return tw`slide absolute top-0 left-0 transition-all ease-in-out duration-700 transform ${TRANSITION_CLASS}`;
+
+    if (currentSlide.value === idx) return "translate-x-0 z-20";
+    if (incomingSlide === idx) return "translate-x-full z-10";
+    if (outgoingSlide === idx) return "-translate-x-full z-10";
+    return "translate-x-full";
   };
 
   const nextSlide = () => {
-    if (SLIDE_DATA.length === currentSlide.value + 1) {
-      currentSlide.value = 0;
-    } else {
-      currentSlide.value++;
-    }
+    currentSlide.value = (currentSlide.value + 1) % SLIDE_DATA.length;
   };
 
   const previousSlide = () => {
-    if (currentSlide.value === 0) {
-      currentSlide.value = SLIDE_DATA.length - 1;
-    } else {
-      currentSlide.value--;
-    }
+    currentSlide.value = (currentSlide.value - 1 + SLIDE_DATA.length) %
+      SLIDE_DATA.length;
   };
 
-  const chevronClick = (doCallback = () => {}) => {
+  const chevronClick = (doCallback: () => void) => {
     if (automatic.value) automatic.value = false;
-    return doCallback();
+    doCallback();
   };
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (automatic.value) nextSlide();
-    }, SLIDE_INTERVAL);
-    return () => clearInterval(interval);
+    if (IS_BROWSER) {
+      const interval = setInterval(() => {
+        if (automatic.value) nextSlide();
+      }, SLIDE_INTERVAL);
+      return () => clearInterval(interval);
+    }
   }, []);
 
-  const ArrowKeyNavigation = () => {
-    const keydownHandler = (event) => {
-      if (automatic.value) automatic.value = false;
-      switch (event.code) {
-        case "ArrowLeft":
+  useEffect(() => {
+    if (IS_BROWSER) {
+      const keydownHandler = (event: KeyboardEvent) => {
+        if (automatic.value) automatic.value = false;
+        if (event.key === "ArrowLeft") {
           event.preventDefault();
           previousSlide();
-          break;
-        case "ArrowRight":
+        } else if (event.key === "ArrowRight") {
           event.preventDefault();
           nextSlide();
-          break;
-        default:
-          break;
-      }
-    };
-    document.addEventListener("keydown", keydownHandler);
-    return () => document.removeEventListener("keydown", keydownHandler);
-  };
-  useEffect(ArrowKeyNavigation, []);
+        }
+      };
+      document.addEventListener("keydown", keydownHandler);
+      return () => document.removeEventListener("keydown", keydownHandler);
+    }
+  }, []);
 
-  const goToSlide = (slide_index = 0) => {
+  const goToSlide = (slide_index: number) => {
     if (automatic.value) automatic.value = false;
     currentSlide.value = slide_index;
   };
@@ -111,25 +101,25 @@ const Carousel = (props) => {
     <div
       class={`slide_nav z-30 w-full ${NAVIGATION_COLOR} absolute bottom-0 flex justify-center cursor-pointer`}
     >
-      {SLIDE_DATA.map((_item, idx) => {
-        return (
-          <div
-            class="px-1 hover:text-grey"
-            onClick={() => {
-              goToSlide(idx);
-            }}
-          >
-            {idx === currentSlide.value ? <>●</> : <>○</>}
-          </div>
-        );
-      })}
+      {SLIDE_DATA.map((_item, idx) => (
+        <div
+          class="px-1 hover:text-grey"
+          onClick={() => goToSlide(idx)}
+        >
+          {idx === currentSlide.value ? "●" : "○"}
+        </div>
+      ))}
     </div>
   );
+
+  if (!IS_BROWSER) {
+    return null; // or a loading placeholder
+  }
 
   return (
     <div
       class={`slideshow relative flex-1 flex-end p-0 overflow-hidden ${
-        props.class !== undefined ? props.class : ""
+        props.class ?? ""
       }`}
     >
       <IconCircleChevronsLeft
@@ -146,15 +136,10 @@ const Carousel = (props) => {
         <Slide
           data={item}
           index={idx}
-          class={slideClasses(idx, SLIDE_DATA.length)}
+          class={slideClasses(idx)}
         />
       ))}
-      {SHOW_NAVIGATION &&
-        <DotsNavigation />}
-      <Slide
-        data={SLIDE_DATA[0]}
-        class="opacity-0 pointer-events-none"
-      />
+      {SHOW_NAVIGATION && <DotsNavigation />}
     </div>
   );
 };
