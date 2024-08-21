5fef00c2db3429f9dd69c533fc7f2732a394708e
reinamora <reinamora@stampchain.io>
2024-08-21 18:07:52 +0000

    chore: functional carousel *full screen width*

--- islands/Carousel.tsx
+++ islands/Carousel.tsx
@@ -1,56 +1,36 @@
 import { useSignal } from "@preact/signals";
 import { useEffect } from "preact/hooks";
+import { IS_BROWSER } from "$fresh/runtime.ts";
 import IconCircleChevronsRight from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/circle-chevrons-right.tsx";
 import IconCircleChevronsLeft from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/circle-chevrons-left.tsx";
-import { IS_BROWSER } from "$fresh/runtime.ts";
+
+interface CarouselProps {
+  showNavigation?: boolean;
+  automatic?: boolean;
+  interval?: number;
+  class?: string;
+}
 
 const SLIDE_DATA = [
-  { url: "img/home/carousel1.png", alt: "snowy deno" },
-  { url: "img/home/carousel2.png", alt: "deno world is raining" },
-  { url: "img/home/carousel3.png", alt: "deno news" },
-  { url: "img/home/carousel2.png", alt: "deno city" },
+  {
+    url: "http://dev.bitcoinstamps.xyz/img/home/carousel1.png",
+    alt: "Slide 1",
+  },
+  {
+    url: "http://dev.bitcoinstamps.xyz/img/home/carousel2.png",
+    alt: "Slide 2",
+  },
+  {
+    url: "http://dev.bitcoinstamps.xyz/img/home/carousel3.png",
+    alt: "Slide 3",
+  },
 ];
 
-const Slide = (
-  props: { index: number; data: { url: string; alt: string }; class?: string },
-) => {
-  return (
-    <img
-      src={props.data.url}
-      alt={props.data.alt}
-      class={`slide absolute top-0 left-0 transition-all ease-in-out duration-700 transform ${props.class}`}
-      style="background-color: white; border: 1px solid red;" // Add this line
-    />
-  );
-};
-
-const Carousel = (
-  props: {
-    showNavigation?: boolean;
-    automatic?: boolean;
-    interval?: number;
-    class?: string;
-  },
-) => {
-  const NAVIGATION_COLOR = `text-white`;
-  const CHEVRON_STYLE =
-    `absolute z-30 w-10 h-10 hover:text-grey ${NAVIGATION_COLOR} cursor-pointer`;
-  const SHOW_NAVIGATION = props.showNavigation ?? true;
-  const SLIDE_INTERVAL = props.interval ?? 3500;
+const Carousel = (props: CarouselProps) => {
   const currentSlide = useSignal(0);
-  const automatic = useSignal(props.automatic ?? true);
-
-  const slideClasses = (idx: number) => {
-    let outgoingSlide = currentSlide.value - 1;
-    let incomingSlide = currentSlide.value + 1;
-    if (outgoingSlide === -1) outgoingSlide = SLIDE_DATA.length - 1;
-    if (incomingSlide === SLIDE_DATA.length) incomingSlide = 0;
-
-    if (currentSlide.value === idx) return "translate-x-0 z-20";
-    if (incomingSlide === idx) return "translate-x-full z-10";
-    if (outgoingSlide === idx) return "-translate-x-full z-10";
-    return "translate-x-full";
-  };
+  const automatic = useSignal(props.automatic ?? false);
+  const SLIDE_INTERVAL = props.interval ?? 5000;
+  const SHOW_NAVIGATION = props.showNavigation ?? true;
 
   const nextSlide = () => {
     currentSlide.value = (currentSlide.value + 1) % SLIDE_DATA.length;
@@ -61,85 +41,65 @@ const Carousel = (
       SLIDE_DATA.length;
   };
 
-  const chevronClick = (doCallback: () => void) => {
+  const goToSlide = (index: number) => {
     if (automatic.value) automatic.value = false;
-    doCallback();
+    currentSlide.value = index;
   };
 
   useEffect(() => {
-    if (IS_BROWSER) {
-      const interval = setInterval(() => {
-        if (automatic.value) nextSlide();
-      }, SLIDE_INTERVAL);
+    if (IS_BROWSER && automatic.value) {
+      const interval = setInterval(nextSlide, SLIDE_INTERVAL);
       return () => clearInterval(interval);
     }
-  }, []);
-
-  useEffect(() => {
-    if (IS_BROWSER) {
-      const keydownHandler = (event: KeyboardEvent) => {
-        if (automatic.value) automatic.value = false;
-        if (event.key === "ArrowLeft") {
-          event.preventDefault();
-          previousSlide();
-        } else if (event.key === "ArrowRight") {
-          event.preventDefault();
-          nextSlide();
-        }
-      };
-      document.addEventListener("keydown", keydownHandler);
-      return () => document.removeEventListener("keydown", keydownHandler);
-    }
-  }, []);
-
-  const goToSlide = (slide_index: number) => {
-    if (automatic.value) automatic.value = false;
-    currentSlide.value = slide_index;
-  };
-
-  const DotsNavigation = () => (
-    <div
-      class={`slide_nav z-30 w-full ${NAVIGATION_COLOR} absolute bottom-0 flex justify-center cursor-pointer`}
-    >
-      {SLIDE_DATA.map((_item, idx) => (
-        <div
-          class="px-1 hover:text-grey"
-          onClick={() => goToSlide(idx)}
-        >
-          {idx === currentSlide.value ? "●" : "○"}
-        </div>
-      ))}
-    </div>
-  );
+  }, [automatic.value]);
 
   if (!IS_BROWSER) {
-    return null; // or a loading placeholder
+    return <div>Loading carousel...</div>;
   }
 
   return (
-    <div
-      class={`slideshow relative flex-1 flex-end p-0 overflow-hidden ${
-        props.class ?? ""
-      }`}
-    >
-      <IconCircleChevronsLeft
-        class={`left-0 ${CHEVRON_STYLE}`}
-        style="top: calc(50% - 20px)"
-        onClick={() => chevronClick(previousSlide)}
-      />
-      <IconCircleChevronsRight
-        class={`right-0 ${CHEVRON_STYLE}`}
-        style="top: calc(50% - 20px)"
-        onClick={() => chevronClick(nextSlide)}
-      />
-      {SLIDE_DATA.map((item, idx) => (
-        <Slide
-          data={item}
-          index={idx}
-          class={slideClasses(idx)}
-        />
+    <div class={`relative overflow-hidden ${props.class ?? ""}`}>
+      {SLIDE_DATA.map((slide, index) => (
+        <div
+          key={index}
+          class={`absolute w-full h-full transition-opacity duration-500 ${
+            index === currentSlide.value ? "opacity-100" : "opacity-0"
+          }`}
+        >
+          <img
+            src={slide.url}
+            alt={slide.alt}
+            class="w-full h-full object-cover"
+          />
+        </div>
       ))}
-      {SHOW_NAVIGATION && <DotsNavigation />}
+      {SHOW_NAVIGATION && (
+        <>
+          <button
+            onClick={previousSlide}
+            class="absolute left-2 top-1/2 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 rounded-full"
+          >
+            <IconCircleChevronsLeft />
+          </button>
+          <button
+            onClick={nextSlide}
+            class="absolute right-2 top-1/2 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 rounded-full"
+          >
+            <IconCircleChevronsRight />
+          </button>
+          <div class="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
+            {SLIDE_DATA.map((_, index) => (
+              <button
+                key={index}
+                onClick={() => goToSlide(index)}
+                class={`w-3 h-3 rounded-full ${
+                  index === currentSlide.value ? "bg-white" : "bg-gray-400"
+                }`}
+              />
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 };
