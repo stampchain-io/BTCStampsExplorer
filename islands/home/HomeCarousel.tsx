add0a882d3d384f14a3ba6bee129ee2032f44fc4
reinamora <reinamora@stampchain.io>
2024-08-21 18:07:52 +0000

    chore: functional carousel *full screen width*

--- islands/home/HomeCarousel.tsx
+++ islands/home/HomeCarousel.tsx
@@ -1,13 +1,19 @@
+import { IS_BROWSER } from "$fresh/runtime.ts";
 import Carousel from "../Carousel.tsx";
 
 export const HomeCarousel = () => {
   return (
     <div class="w-full h-[400px]">
-      <Carousel
-        showNavigation={true}
-        automatic={true}
-        interval={5000}
-      />
+      {IS_BROWSER
+        ? (
+          <Carousel
+            showNavigation={true}
+            automatic={true}
+            interval={5000}
+            class="h-full"
+          />
+        )
+        : <div>Loading carousel...</div>}
     </div>
   );
 };
