902767fbd95c1d2f712f27d95468d9e699b37681
reinamora <reinamora@stampchain.io>
2024-08-21 16:49:30 +0000

    chore: reactivate carousel for more testing

--- islands/home/HomeCarousel.tsx
+++ islands/home/HomeCarousel.tsx
@@ -1,34 +1,13 @@
-// import Flicking, {
-//   MoveEvent,
-//   WillChangeEvent,
-// } from "https://esm.sh/@egjs/preact-flicking@4.11.0";
+import Carousel from "../Carousel.tsx";
 
 export const HomeCarousel = () => {
   return (
-    // <Flicking
-    //   viewportTag="div"
-    //   cameraTag="div"
-    //   cameraClass=""
-    //   align="center"
-    //   onMove={(e: MoveEvent) => {}}
-    //   onWillChange={(e: WillChangeEvent) => {}}
-    //   horizontal={true}
-    //   circular={true}
-    // >
-    //   <div>panel 0</div>
-    //   <div>panel 1</div>
-    //   <div>panel 2</div>
-    // </Flicking>
-    <div>
-      <div>
-        <img src="/img/home/carousel1.png" />
-      </div>
-      <div>
-        <img src="/img/home/carousel2.png" />
-      </div>
-      <div>
-        <img src="/img/home/carousel3.png" />
-      </div>
+    <div class="w-full h-[400px]">
+      <Carousel
+        showNavigation={true}
+        automatic={true}
+        interval={5000}
+      />
     </div>
   );
 };
