7bad69e4b7cf641d868f670c5feeda5d373c0696
reinamora <reinamora@stampchain.io>
2024-08-27 15:45:25 +0000

    chore: finalize v2 API migration for XCP

--- routes/api/v2/balance/getStampsBalance.ts
+++ routes/api/v2/balance/getStampsBalance.ts
@@ -1,9 +1,9 @@
-import { get_stamps_balance } from "$lib/services/xcpService.ts";
+import { XcpManager } from "$lib/services/xcpService.ts";
 
 // FIXME: This is a temporary endpoint to get the stamps balance for a given address
 export async function handler(req: Request): Promise<Response> {
   const { address } = await req.json();
-  const stampBalance = await get_stamps_balance(address);
+  const stampBalance = await XcpManager.getXcpBalancesByAddress(address);
   return new Response(JSON.stringify({ stampBalance }), {
     headers: { "Content-Type": "application/json" },
   });
