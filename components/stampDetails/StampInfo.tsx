f5a9820fc6a397d42bbb8a9703d7bff3f43225c3
dev4pear <tommycole519@gmail.com>
2024-08-28 11:04:17 -0400

    chore: fix mobile responsive on stamp-details page

--- components/stampDetails/StampInfo.tsx
+++ components/stampDetails/StampInfo.tsx
@@ -21,7 +21,7 @@ export function StampInfo({ stamp }: { stamp: StampRow }) {
         </p>
         {/* <StampKind kind={kind} /> */}
       </div>
-      <div class="flex flex-col gap-1 truncate border-b border-[#60626F] text-[#F5F5F5] px-6 py-4">
+      <div class="flex flex-row justify-between items-center md:flex-col md:items-start gap-1 truncate border-b border-[#60626F] text-[#F5F5F5] px-6 py-4">
         <p class="text-xl font-semibold">CPID</p>
         <a
           href={`https://xcp.dev/asset/${stamp.cpid}`}
@@ -32,10 +32,10 @@ export function StampInfo({ stamp }: { stamp: StampRow }) {
           {stamp.cpid}
         </a>
       </div>
-      <div class="flex justify-between items-end gap-1 truncate border-b border-[#60626F] text-[#F5F5F5] px-6 py-4">
-        <div className="flex flex-col">
-          <p class="text-xl font-semibold">Creator</p>
-          <p class="text-[#60626F]">
+      <div class="flex flex-row justify-between items-center md:flex-col md:items-start gap-1 truncate border-b border-[#60626F] text-[#F5F5F5] px-6 py-4">
+        <p class="text-xl font-semibold">Creator</p>
+        <div class="flex justify-between items-center md:w-full gap-2">
+          <p class="block md:hidden text-[#60626F]">
             {stamp.creator_name
               ? stamp.creator_name
               : (
@@ -44,52 +44,68 @@ export function StampInfo({ stamp }: { stamp: StampRow }) {
                 </a>
               )}
           </p>
+          <p class="hidden md:block text-[#60626F] overflow-hidden text-ellipsis whitespace-nowrap">
+            {stamp.creator_name
+              ? stamp.creator_name
+              : (
+                <a href={`/wallet/${stamp.creator}`}>
+                  {stamp.creator}
+                </a>
+              )}
+          </p>
+          <img
+            src="/img/icon_copy_to_clipboard.png"
+            className="w-4 h-5 cursor-pointer"
+          />
         </div>
-        <img
-          src="/img/icon_copy_to_clipboard.png"
-          className="w-4 h-5 cursor-pointer"
-        />
       </div>
       <div class="flex justify-between items-end gap-1 truncate border-b border-[#60626F] text-[#F5F5F5] px-6 py-4">
-        {/* <div class="flex flex-col gap-1 truncate text-[#F5F5F5] px-6 py-4"> */}
         <p class="text-xl font-semibold">Created</p>
         <p class="text-[#60626F]">
           {timestamp.toLocaleDateString()} ({dayjs(timestamp).fromNow()})
         </p>
       </div>
-      <div class="flex justify-between items-end gap-1 truncate border-b border-[#60626F] text-[#F5F5F5] px-6 py-4">
-        <div className="flex flex-col">
-          <p class="text-xl font-semibold">Block #</p>
+      <div class="flex flex-row justify-between items-center md:flex-col md:items-start gap-1 truncate border-b border-[#60626F] text-[#F5F5F5] px-6 py-4">
+        <p class="text-xl font-semibold">Block #</p>
+        <div class="flex justify-between items-center md:w-full gap-2">
           <a
             href={`/block/${stamp.block_index}`}
             class="text-[#60626F] hover:underline"
           >
             {stamp.block_index}
           </a>
+          <img
+            src="/img/icon_copy_to_clipboard.png"
+            className="w-4 h-5 cursor-pointer"
+          />
         </div>
-        <img
-          src="/img/icon_copy_to_clipboard.png"
-          className="w-4 h-5 cursor-pointer"
-        />
       </div>
-      <div class="flex justify-between items-end gap-1 truncate border-b border-[#60626F] text-[#F5F5F5] px-6 py-4">
-        <div className="flex flex-col">
-          <p class="text-xl font-semibold">TX hash</p>
+      <div class="flex flex-row justify-between items-center md:flex-col md:items-start gap-1 truncate border-b border-[#60626F] text-[#F5F5F5] px-6 py-4">
+        <p class="text-xl font-semibold">TX hash</p>
+        <div class="flex justify-between items-center md:w-full gap-2">
           <a
             href={`https://www.blockchain.com/explorer/transactions/btc/${stamp.tx_hash}`}
             target="_blank"
             rel="noopener noreferrer"
-            class="text-[#60626F]"
+            class="block md:hidden text-[#60626F]"
           >
             {abbreviateAddress(stamp.tx_hash, 12)}
           </a>
+          <a
+            href={`https://www.blockchain.com/explorer/transactions/btc/${stamp.tx_hash}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            class="hidden md:block text-[#60626F] overflow-hidden text-ellipsis whitespace-nowrap"
+          >
+            {stamp.tx_hash}
+          </a>
+          <img
+            src="/img/icon_copy_to_clipboard.png"
+            className="w-4 h-5 cursor-pointer"
+          />
         </div>
-        <img
-          src="/img/icon_copy_to_clipboard.png"
-          className="w-4 h-5 cursor-pointer"
-        />
       </div>
-      <div class="flex flex-col gap-1 truncate border-b border-[#60626F] text-[#F5F5F5] px-6 py-4">
+      <div class="flex flex-row justify-between items-center md:flex-col md:items-start gap-1 truncate border-b border-[#60626F] text-[#F5F5F5] px-6 py-4">
         <p class="text-xl font-semibold">Supply</p>
         <p class="text-[#60626F]">
           {stamp.divisible
@@ -99,19 +115,19 @@ export function StampInfo({ stamp }: { stamp: StampRow }) {
             : stamp.supply}
         </p>
       </div>
-      <div class="flex flex-col gap-1 truncate border-b border-[#60626F] text-[#F5F5F5] px-6 py-4">
+      <div class="flex flex-row justify-between items-center md:flex-col md:items-start gap-1 truncate border-b border-[#60626F] text-[#F5F5F5] px-6 py-4">
         <p class="text-xl font-semibold">Locked</p>
         <p class="text-[#60626F]">{stamp.locked ?? false ? "Yes" : "No"}</p>
       </div>
-      <div class="flex flex-col gap-1 truncate border-b border-[#60626F] text-[#F5F5F5] px-6 py-4">
+      <div class="flex flex-row justify-between items-center md:flex-col md:items-start gap-1 truncate border-b border-[#60626F] text-[#F5F5F5] px-6 py-4">
         <p class="text-xl font-semibold">Divisible</p>
         <p class="text-[#60626F]">{stamp.divisible ? "Yes" : "No"}</p>
       </div>
-      <div class="flex flex-col gap-1 truncate border-b border-[#60626F] text-[#F5F5F5] px-6 py-4">
+      <div class="flex flex-row justify-between items-center md:flex-col md:items-start gap-1 truncate border-b border-[#60626F] text-[#F5F5F5] px-6 py-4">
         <p class="text-xl font-semibold">Keyburned</p>
         <p class="text-[#60626F]">{stamp.keyburn ?? false ? "Yes" : "No"}</p>
       </div>
-      <div class="flex flex-col gap-1 truncate border-b border-[#60626F] text-[#F5F5F5] px-6 py-4">
+      <div class="flex flex-row justify-between items-center md:flex-col md:items-start gap-1 truncate border-b border-[#60626F] text-[#F5F5F5] px-6 py-4">
         <p class="text-xl font-semibold">Floor Price</p>
         <p class="text-[#60626F]">
           {typeof stamp.floorPrice === "number"
@@ -119,7 +135,7 @@ export function StampInfo({ stamp }: { stamp: StampRow }) {
             : stamp.floorPrice}
         </p>
       </div>
-      <div class="flex flex-col gap-1 truncate border-b border-[#60626F] text-[#F5F5F5] px-6 py-4">
+      <div class="flex flex-row justify-between items-center md:flex-col md:items-start gap-1 truncate border-b border-[#60626F] text-[#F5F5F5] px-6 py-4">
         <p class="text-xl font-semibold">Market Cap</p>
         <p class="text-[#60626F]">
           {typeof stamp.marketCap === "number"
@@ -127,8 +143,7 @@ export function StampInfo({ stamp }: { stamp: StampRow }) {
             : stamp.marketCap}
         </p>
       </div>
-      <div class="flex flex-col gap-1 truncate text-[#F5F5F5] px-6 py-4">
-        {/* <div class="flex flex-col gap-1 truncate border-b border-[#60626F] text-[#F5F5F5] px-6 py-4"> */}
+      <div class="flex flex-row justify-between items-center md:flex-col md:items-start gap-1 truncate border-b border-[#60626F] text-[#F5F5F5] px-6 py-4">
         <p class="text-xl font-semibold">Vault Address</p>
         <p class="text-[#60626F]">pending</p>
       </div>
