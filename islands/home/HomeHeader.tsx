1e5f418deb88f77260d9ddaf02078e4199ed1ed4
reinamora <reinamora@stampchain.io>
2024-08-30 15:42:20 +0000

    chore: update font styling and header text

--- islands/home/HomeHeader.tsx
+++ islands/home/HomeHeader.tsx
@@ -1,12 +1,15 @@
 export const HomeHeader = () => {
   return (
-    <div class="flex flex-col items-center justify-center gap-8">
-      <p class="max-w-[1000px] text-3xl md:text-6xl leading-normal text-center text-[#F5F5F5]">
-        Unprunable{" "}
-        <span class="text-[#7A00F5] font-bold">UTXO Art</span>, Because Sats
-        Don't Exist.
+    <div class="flex flex-col items-center justify-center gap-4">
+      <p class="max-w-[1000px] text-center">
+        <span class="text-3xl md:text-5xl lg:text-6xl leading-tight text-[#F5F5F5] font-black uppercase block">
+          Unprunable <span class="text-[#7A00F5]">UTXO Art</span>
+        </span>
+        <span class="text-2xl md:text-4xl lg:text-5xl leading-tight text-[#DBDBDB] font-black uppercase block mt-2">
+          Because Sats Don't Exist
+        </span>
       </p>
-      <p class="max-w-[850px] text-lg md:text-2xl text-[#DBDBDB] text-center">
+      <p class="max-w-[850px] text-lg md:text-xl lg:text-2xl text-[#DBDBDB] text-center font-light mt-4">
         Welcome to the forefront of digital collectibles, where each stamp is a
         unique piece of art intertwined with the immutability of the blockchain.
       </p>
