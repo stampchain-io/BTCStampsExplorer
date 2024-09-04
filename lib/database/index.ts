f7172cac844c9b3c03896180545200dbde629f3b
reinamora <reinamora@stampchain.io>
2024-09-04 15:58:46 +0000

    chore: more api refactoring into controller functions
    - fix pagination on src20 balance

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -3,4 +3,3 @@ export { BlockRepository } from "$lib/database/blockRepository.ts";
 export { CollectionRepository } from "$lib/database/collectionRepository.ts";
 
 export { summarize_issuances } from "$lib/database/summary.ts";
-export { Src20Class } from "$lib/database/src20Class.ts";
