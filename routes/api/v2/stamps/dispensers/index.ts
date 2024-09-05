9802f0c93ed97557271493f9cf7c4456f11e6a68
reinamora <reinamora@stampchain.io>
2024-09-05 03:30:43 +0000

    chore: pause dispenser endpoint

--- routes/api/v2/stamps/dispensers/index.ts
+++ routes/api/v2/stamps/dispensers/index.ts
@@ -1,25 +1,23 @@
 import { Handlers } from "$fresh/server.ts";
-import { paginate } from "$lib/utils/util.ts";
+// import { paginate } from "$lib/utils/util.ts";
 import { ResponseUtil } from "$lib/utils/responseUtil.ts";
-import { getPaginationParams } from "$lib/utils/paginationUtils.ts";
-import { BlockController } from "$lib/controller/blockController.ts";
-import { DispenserManager } from "$lib/services/xcpService.ts";
+// import { getPaginationParams } from "$lib/utils/paginationUtils.ts";
+// import { BlockController } from "$lib/controller/blockController.ts";
+// import { DispenserManager } from "$lib/services/xcpService.ts";
 
 export const handler: Handlers = {
   async GET(req) {
     try {
-      const url = new URL(req.url);
-      const { limit, page } = getPaginationParams(url);
+      // const url = new URL(req.url);
+      // const { limit, page } = getPaginationParams(url);
 
-      const [lastBlock, { total, dispensers }] = await Promise.all([
-        BlockController.getLastBlock(),
-        DispenserManager.getAllOpenStampDispensers(page, limit),
-      ]);
+      // const [lastBlock, { total, dispensers }] = await Promise.all([
+      //   BlockController.getLastBlock(),
+      //   DispenserManager.getAllOpenStampDispensers(page, limit),
+      // ]);
 
       const body = {
-        ...paginate(total, page, limit),
-        last_block: lastBlock,
-        dispensers,
+        message: "Pending implementation",
       };
 
       return ResponseUtil.success(body);
