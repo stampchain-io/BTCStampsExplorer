16300ba548a0bb4de314c82252c6b98524bc186d
reinamora <reinamora@stampchain.io>
2024-09-12 16:07:35 +0000

    fix: check if string is already an emoji before conversion

--- lib/utils/util.ts
+++ lib/utils/util.ts
@@ -157,7 +157,16 @@ export function jsonStringifyBigInt(obj: object): string {
 }
 
 export function convertToEmoji(tick: string): string {
-  return tick.replace(/\\u([a-fA-F0-9]{8})/g, (match, grp) => {
+  // Check if the string is already an emoji
+  if (
+    /^[\u{1F300}-\u{1F5FF}\u{1F600}-\u{1F64F}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{1FA70}-\u{1FAFF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{2B50}-\u{2B55}\u{1F004}\u{1F0CF}]+$/u
+      .test(tick)
+  ) {
+    return tick;
+  }
+
+  // Convert Unicode code points to emojis
+  return tick.replace(/\\u([a-fA-F0-9]{8})/gi, (match, grp) => {
     const codePoint = parseInt(grp, 16);
     if (!isNaN(codePoint)) {
       return String.fromCodePoint(codePoint);
