fca12912ecebcb636a3703ead8bcb6aad88e1725
dev4pear <tommycole519@gmail.com>
2024-09-20 11:26:13 -0400

    feat: move certain components to islands/modules; update initial layout for src-20 stamping;

--- islands/stamping/DeployContent.tsx
+++ islands/stamping/DeployContent.tsx
@@ -102,199 +102,224 @@ export function DeployContent() {
 
   return (
     <div class="flex flex-col w-full items-center gap-8">
-      <p class="text-[#5503A6] text-[43px] font-medium mt-6 w-full text-left">
-        DEPLOY SRC-20
+      <p class="text-[#5503A6] text-3xl md:text-6xl font-black mt-6 w-full text-center">
+        DEPLOY
       </p>
 
-      <div>
-        <div class="flex flex-col md:flex-row gap-8">
-          <div
-            id="image-preview"
-            class="relative max-w-sm border border-[#F5F5F5] rounded-md items-center mx-auto text-center cursor-pointer w-[324px] h-[324px] content-center bg-[#2B0E49]"
-          >
-            <input
-              id="upload"
-              type="file"
-              class="hidden"
-              accept="image/*"
-              onChange={handleFileChange}
-            />
-            {formState.file !== null && (
-              <img
-                width={324}
-                style={{
-                  height: "100%",
-                  objectFit: "contain",
-                  imageRendering: "pixelated",
-                  backgroundColor: "rgb(0,0,0)",
-                  borderRadius: "6px",
-                }}
-                src={URL.createObjectURL(formState.file)}
+      <div
+        className={"flex flex-col md:flex-row gap-6 bg-gradient-to-br from-[#1F002E00] via-[#14001F7F] to-[#1F002EFF] p-2 md:p-6 w-full"}
+      >
+        <div>
+          <div class="flex flex-col md:flex-row gap-8">
+            <div
+              id="image-preview"
+              class="relative max-w-sm border border-[#F5F5F5] rounded-md items-center mx-auto text-center cursor-pointer w-[324px] h-[324px] content-center bg-[#2B0E49]"
+            >
+              <input
+                id="upload"
+                type="file"
+                class="hidden"
+                accept="image/*"
+                onChange={handleFileChange}
               />
-            )}
-            {formState.file === null && (
-              <label
-                for="upload"
-                class="cursor-pointer h-full flex flex-col items-center justify-center gap-3"
-              >
+              {formState.file !== null && (
                 <img
-                  src="/img/mint/icon-image-upload.png"
-                  class="w-20 h-20"
-                  alt=""
+                  width={324}
+                  style={{
+                    height: "100%",
+                    objectFit: "contain",
+                    imageRendering: "pixelated",
+                    backgroundColor: "rgb(0,0,0)",
+                    borderRadius: "6px",
+                  }}
+                  src={URL.createObjectURL(formState.file)}
                 />
-                <h5 class="text-[#F5F5F5] text-2xl font-semibold">
-                  Upload Image
-                </h5>
-              </label>
-            )}
+              )}
+              {formState.file === null && (
+                <label
+                  for="upload"
+                  class="cursor-pointer h-full flex flex-col items-center justify-center gap-3"
+                >
+                  <img
+                    src="/img/mint/icon-image-upload.png"
+                    class="w-20 h-20"
+                    alt=""
+                  />
+                  <h5 class="text-[#F5F5F5] text-2xl font-semibold">
+                    Upload Image
+                  </h5>
+                </label>
+              )}
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="w-full flex flex-col md:flex-row gap-8 md:gap-5">
-        <div class="w-full">
-          <p class="text-lg font-semibold text-[#F5F5F5] mb-3">
-            Token
-          </p>
-          <input
-            type="text"
-            class="px-3 py-6 bg-[#6E6E6E] text-sm text-[#F5F5F5] w-full"
-            placeholder="Max 5 Chars"
-            value={formState.token}
-            onChange={(e) => handleInputChange(e, "token")}
-            maxLength={5}
-          />
-          {formState.tokenError && (
-            <p class="text-red-500 mt-2">{formState.tokenError}</p>
-          )}
+        <div className={"flex flex-col gap-6 w-full"}>
+          <div class="w-full">
+            {
+              /* <p class="text-lg font-semibold text-[#F5F5F5] mb-3">
+              Token
+            </p> */
+            }
+            <input
+              type="text"
+              class="p-4 bg-[#999999] text-[#333333] placeholder:text-[#333333] font-medium w-full outline-none rounded-md"
+              placeholder="Token ticker name"
+              value={formState.token}
+              onChange={(e) => handleInputChange(e, "token")}
+              maxLength={5}
+            />
+            {formState.tokenError && (
+              <p class="text-red-500 mt-2">{formState.tokenError}</p>
+            )}
+          </div>
+
+          <div class="w-full">
+            {
+              /* <p class="text-lg font-semibold text-[#F5F5F5] mb-3">
+              Limit Per Mint
+            </p> */
+            }
+            <input
+              type="text"
+              inputMode="numeric"
+              pattern="[0-9]*"
+              class="p-4 bg-[#999999] text-[#333333] placeholder:text-[#333333] font-medium w-full outline-none rounded-md"
+              placeholder="Positive Integer (max uint64)"
+              value={formState.lim}
+              onChange={(e) => handleInputChange(e, "lim")}
+            />
+            {formState.limError && (
+              <p class="text-red-500 mt-2">{formState.limError}</p>
+            )}
+          </div>
+
+          <div class="w-full">
+            {
+              /* <p class="text-lg font-semibold text-[#F5F5F5] mb-3">
+              Max Circulation
+            </p> */
+            }
+            <input
+              type="text"
+              inputMode="numeric"
+              pattern="[0-9]*"
+              class="p-4 bg-[#999999] text-[#333333] placeholder:text-[#333333] font-medium w-full outline-none rounded-md"
+              placeholder="Positive Integer (max uint64)"
+              value={formState.max}
+              onChange={(e) => handleInputChange(e, "max")}
+            />
+            {formState.maxError && (
+              <p class="text-red-500 mt-2">{formState.maxError}</p>
+            )}
+          </div>
+
+          <div class="w-full">
+            {
+              /* <p class="text-lg font-semibold text-[#F5F5F5] mb-3">
+              Decimal Places
+            </p> */
+            }
+            <input
+              type="text"
+              inputMode="numeric"
+              pattern="[0-9]*"
+              class="p-4 bg-[#999999] text-[#333333] placeholder:text-[#333333] font-medium w-full outline-none rounded-md"
+              placeholder="Decimal Places (0-18, default: 18)"
+              value={formState.dec}
+              onChange={(e) => handleInputChange(e, "dec")}
+            />
+          </div>
+
+          {
+            /* <div class="w-full">
+            <p class="text-lg font-semibold text-[#F5F5F5] mb-3">X Username</p>
+            <input
+              type="text"
+              class="px-3 py-6 bg-[#6E6E6E] text-sm text-[#F5F5F5] w-full"
+              placeholder="X Username (optional)"
+              value={formState.x}
+              onChange={(e) => handleInputChange(e, "x")}
+            />
+          </div> */
+          }
+
+          {
+            /* <div class="w-full">
+            <p class="text-lg font-semibold text-[#F5F5F5] mb-3">Website</p>
+            <input
+              type="text"
+              class="px-3 py-6 bg-[#6E6E6E] text-sm text-[#F5F5F5] w-full"
+              placeholder="Website (optional)"
+              value={formState.web}
+              onChange={(e) => handleInputChange(e, "web")}
+            />
+          </div> */
+          }
+
+          {
+            /* <div class="w-full">
+            <p class="text-lg font-semibold text-[#F5F5F5] mb-3">Email</p>
+            <input
+              type="email"
+              class="px-3 py-6 bg-[#6E6E6E] text-sm text-[#F5F5F5] w-full"
+              placeholder="Email (optional)"
+              value={formState.email}
+              onChange={(e) => handleInputChange(e, "email")}
+            />
+          </div> */
+          }
         </div>
-        <div class="w-full">
-          <p class="text-lg font-semibold text-[#F5F5F5] mb-3">
-            Limit Per Mint
-          </p>
-          <input
-            type="text"
-            inputMode="numeric"
-            pattern="[0-9]*"
-            class="px-3 py-6 bg-[#6E6E6E] text-sm text-[#F5F5F5] w-full"
-            placeholder="Positive Integer (max uint64)"
-            value={formState.lim}
-            onChange={(e) => handleInputChange(e, "lim")}
-          />
-          {formState.limError && (
-            <p class="text-red-500 mt-2">{formState.limError}</p>
-          )}
-        </div>
       </div>
-
-      <div class="w-full">
-        <p class="text-lg font-semibold text-[#F5F5F5] mb-3">
-          Max Circulation
-        </p>
-        <input
-          type="text"
-          inputMode="numeric"
-          pattern="[0-9]*"
-          class="px-3 py-6 bg-[#6E6E6E] text-sm text-[#F5F5F5] w-full"
-          placeholder="Positive Integer (max uint64)"
-          value={formState.max}
-          onChange={(e) => handleInputChange(e, "max")}
-        />
-        {formState.maxError && (
-          <p class="text-red-500 mt-2">{formState.maxError}</p>
-        )}
-      </div>
-
-      <div class="w-full">
-        <p class="text-lg font-semibold text-[#F5F5F5] mb-3">Decimal Places</p>
-        <input
-          type="text"
-          inputMode="numeric"
-          pattern="[0-9]*"
-          class="px-3 py-6 bg-[#6E6E6E] text-sm text-[#F5F5F5] w-full"
-          placeholder="Decimal Places (0-18, default: 18)"
-          value={formState.dec}
-          onChange={(e) => handleInputChange(e, "dec")}
-        />
-      </div>
-
-      <div class="w-full">
-        <p class="text-lg font-semibold text-[#F5F5F5] mb-3">X Username</p>
-        <input
-          type="text"
-          class="px-3 py-6 bg-[#6E6E6E] text-sm text-[#F5F5F5] w-full"
-          placeholder="X Username (optional)"
-          value={formState.x}
-          onChange={(e) => handleInputChange(e, "x")}
-        />
-      </div>
-
-      <div class="w-full">
-        <p class="text-lg font-semibold text-[#F5F5F5] mb-3">Website</p>
-        <input
-          type="text"
-          class="px-3 py-6 bg-[#6E6E6E] text-sm text-[#F5F5F5] w-full"
-          placeholder="Website (optional)"
-          value={formState.web}
-          onChange={(e) => handleInputChange(e, "web")}
-        />
-      </div>
-
-      <div class="w-full">
-        <p class="text-lg font-semibold text-[#F5F5F5] mb-3">Email</p>
-        <input
-          type="email"
-          class="px-3 py-6 bg-[#6E6E6E] text-sm text-[#F5F5F5] w-full"
-          placeholder="Email (optional)"
-          value={formState.email}
-          onChange={(e) => handleInputChange(e, "email")}
-        />
-      </div>
-
-      <FeeEstimation
-        fee={formState.fee}
-        handleChangeFee={handleChangeFee}
-        type="src20-deploy"
-        fileType="application/json"
-        fileSize={formState.jsonSize}
-        issuance={1}
-        BTCPrice={formState.BTCPrice}
-        onRefresh={fetchFees}
-      />
-
-      {apiError && (
-        <div class="w-full text-red-500 text-center">
-          {apiError}
-        </div>
-      )}
-
-      {fileUploadError && (
-        <div class="w-full text-yellow-500 text-center">
-          {fileUploadError}
-        </div>
-      )}
-
-      {submissionMessage && (
-        <div class="w-full text-center font-bold">
-          {submissionMessage}
-        </div>
-      )}
-
-      {walletError && (
-        <div class="w-full text-red-500 text-center">
-          {walletError}
-        </div>
-      )}
 
       <div
-        class={`w-full text-white text-center font-bold border-[0.5px] border-[#8A8989] rounded-md mt-4 py-6 px-4 ${
-          isSubmitting
-            ? "bg-gray-500 cursor-not-allowed"
-            : "bg-[#5503A6] cursor-pointer"
-        }`}
-        onClick={isSubmitting ? undefined : handleDeploySubmit}
+        className={"bg-gradient-to-br from-[#1F002E00] via-[#14001F7F] to-[#1F002EFF] p-6 w-full"}
       >
-        {isSubmitting ? "Stamping..." : "Stamp Now"}
+        <FeeEstimation
+          fee={formState.fee}
+          handleChangeFee={handleChangeFee}
+          type="src20-deploy"
+          fileType="application/json"
+          fileSize={formState.jsonSize}
+          issuance={1}
+          BTCPrice={formState.BTCPrice}
+          onRefresh={fetchFees}
+        />
+
+        {apiError && (
+          <div class="w-full text-red-500 text-center">
+            {apiError}
+          </div>
+        )}
+
+        {fileUploadError && (
+          <div class="w-full text-yellow-500 text-center">
+            {fileUploadError}
+          </div>
+        )}
+
+        {submissionMessage && (
+          <div class="w-full text-center font-bold">
+            {submissionMessage}
+          </div>
+        )}
+
+        {walletError && (
+          <div class="w-full text-red-500 text-center">
+            {walletError}
+          </div>
+        )}
+
+        <div
+          class={`w-full text-white text-center font-bold border-[0.5px] border-[#8A8989] rounded-md mt-4 py-6 px-4 ${
+            isSubmitting
+              ? "bg-gray-500 cursor-not-allowed"
+              : "bg-[#5503A6] cursor-pointer"
+          }`}
+          onClick={isSubmitting ? undefined : handleDeploySubmit}
+        >
+          {isSubmitting ? "Stamping..." : "Stamp Now"}
+        </div>
       </div>
     </div>
   );
--- islands/stamping/MintContent.tsx
+++ islands/stamping/MintContent.tsx
@@ -34,81 +34,95 @@ export function MintContent() {
 
   return (
     <div class="flex flex-col w-full items-center gap-8">
-      <p class="text-[#5503A6] text-[43px] font-medium mt-6 w-full text-left">
+      <p class="text-[#5503A6] text-3xl md:text-6xl font-black mt-6 w-full text-center">
         MINT SRC-20
       </p>
 
-      <div class="w-full">
-        <p class="text-lg font-semibold text-[#F5F5F5] mb-3">
-          Token
-        </p>
-        <input
-          type="text"
-          class="px-3 py-6 bg-[#6E6E6E] text-sm text-[#F5F5F5] w-full"
-          placeholder="Case Sensitive"
-          value={formState.token}
-          onChange={(e) => handleInputChange(e, "token")}
-        />
-        {formState.tokenError && (
-          <p class="text-red-500 mt-2">{formState.tokenError}</p>
-        )}
+      <div
+        className={"bg-gradient-to-br from-[#1F002E00] via-[#14001F7F] to-[#1F002EFF] p-6 w-full"}
+      >
+        <div className={"flex flex-col gap-6 w-full"}>
+          <div class="w-full">
+            {
+              /* <p class="text-lg font-semibold text-[#F5F5F5] mb-3">
+            Token
+          </p> */
+            }
+            <input
+              type="text"
+              class="p-4 bg-[#999999] text-[#333333] placeholder:text-[#333333] font-medium w-full outline-none rounded-md"
+              placeholder="Token"
+              value={formState.token}
+              onChange={(e) => handleInputChange(e, "token")}
+            />
+            {formState.tokenError && (
+              <p class="text-red-500 mt-2">{formState.tokenError}</p>
+            )}
+          </div>
+
+          <div class="w-full">
+            {
+              /* <p class="text-lg font-semibold text-[#F5F5F5] mb-3">
+              Mint Amount
+            </p> */
+            }
+            <input
+              type="text"
+              inputMode="numeric"
+              pattern="[0-9]*"
+              class="p-4 bg-[#999999] text-[#333333] placeholder:text-[#333333] font-medium w-full outline-none rounded-md"
+              placeholder="Amount"
+              value={formState.amt}
+              onChange={(e) => handleInputChange(e, "amt")}
+            />
+            {formState.amtError && (
+              <p class="text-red-500 mt-2">{formState.amtError}</p>
+            )}
+          </div>
+        </div>
       </div>
-
-      <div class="w-full">
-        <p class="text-lg font-semibold text-[#F5F5F5] mb-3">
-          Mint Amount
-        </p>
-        <input
-          type="text"
-          inputMode="numeric"
-          pattern="[0-9]*"
-          class="px-3 py-6 bg-[#6E6E6E] text-sm text-[#F5F5F5] w-full"
-          placeholder="Number"
-          value={formState.amt}
-          onChange={(e) => handleInputChange(e, "amt")}
-        />
-        {formState.amtError && (
-          <p class="text-red-500 mt-2">{formState.amtError}</p>
-        )}
-      </div>
-
-      <FeeEstimation
-        fee={formState.fee}
-        handleChangeFee={handleChangeFee}
-        type="src20-mint"
-        fileType="application/json"
-        fileSize={formState.jsonSize}
-        BTCPrice={formState.BTCPrice}
-        onRefresh={fetchFees}
-      />
-
-      {apiError && (
-        <div class="w-full text-red-500 text-center">
-          {apiError}
-        </div>
-      )}
-
-      {walletError && (
-        <div class="w-full text-red-500 text-center">
-          {walletError}
-        </div>
-      )}
-
-      {submissionMessage && (
-        <div class="w-full text-center font-bold">
-          {submissionMessage}
-        </div>
-      )}
 
       <div
-        class={`w-full text-white text-center font-bold border-[0.5px] border-[#8A8989] rounded-md mt-4 py-6 px-4 ${
-          isSubmitting
-            ? "bg-gray-500 cursor-not-allowed"
-            : "bg-[#5503A6] cursor-pointer"
-        }`}
-        onClick={isSubmitting ? undefined : handleMintSubmit}
+        className={"bg-gradient-to-br from-[#1F002E00] via-[#14001F7F] to-[#1F002EFF] p-6 w-full"}
       >
-        {isSubmitting ? "Stamping..." : "Stamp Now"}
+        <FeeEstimation
+          fee={formState.fee}
+          handleChangeFee={handleChangeFee}
+          type="src20-mint"
+          fileType="application/json"
+          fileSize={formState.jsonSize}
+          BTCPrice={formState.BTCPrice}
+          onRefresh={fetchFees}
+        />
+
+        {apiError && (
+          <div class="w-full text-red-500 text-center">
+            {apiError}
+          </div>
+        )}
+
+        {walletError && (
+          <div class="w-full text-red-500 text-center">
+            {walletError}
+          </div>
+        )}
+
+        {submissionMessage && (
+          <div class="w-full text-center font-bold">
+            {submissionMessage}
+          </div>
+        )}
+
+        <div
+          class={`w-full text-white text-center font-bold border-[0.5px] border-[#8A8989] rounded-md mt-4 py-6 px-4 ${
+            isSubmitting
+              ? "bg-gray-500 cursor-not-allowed"
+              : "bg-[#5503A6] cursor-pointer"
+          }`}
+          onClick={isSubmitting ? undefined : handleMintSubmit}
+        >
+          {isSubmitting ? "Stamping..." : "Stamp Now"}
+        </div>
       </div>
     </div>
   );
