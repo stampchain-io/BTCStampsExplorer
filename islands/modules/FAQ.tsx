508055bc69ca1af2d4e774f5d0550f3d8562a13f
dev4pear <tommycole519@gmail.com>
2024-09-21 14:50:26 -0400

    chore: update src20-stamping layout

--- islands/modules/FAQ.tsx
+++ islands/modules/FAQ.tsx
@@ -1,44 +1,32 @@
 export const FAQModule = () => {
   return (
     <div
-      className={"flex flex-col md:flex-row justify-between md:items-end gap-16 md:gap-32"}
+      className={"flex flex-col gap-4"}
     >
-      <div>
-        <p
-          className={"italic font-black text-4xl md:text-7xl flex flex-col bg-clip-text text-transparent bg-gradient-to-r from-[#666666] to-[#999999] text-left"}
+      <p
+        className={"italic font-black text-4xl md:text-7xl flex flex-col bg-clip-text text-transparent bg-gradient-to-r from-[#666666] to-[#999999] text-left"}
+      >
+        FAQ
+        <span
+          className={"not-italic text-3xl md:text-6xl font-extralight text-[#999999]"}
         >
-          GET STAMPING
-          <span
-            className={"not-italic text-3xl md:text-6xl font-extralight text-[#999999]"}
-          >
-            IMMORTALISE YOUR ART
-          </span>
-        </p>
+          READ THE MANUAL
+        </span>
+      </p>
 
-        <p className={"text-[#CCCCCC] text-2xl font-medium"}>
-          Stamp Posh or Classic stamps cheap and effortlessly with built in
-          compression tools, custom fee selection and frictionless Posh naming
-          options.<br />
-          <br />
-          Support for png/jpg/gif low-fi pixel art and hi-res svg/html vector
-          art - up to 65kB.
-        </p>
-      </div>
+      <p className={"text-[#CCCCCC] text-2xl font-medium"}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed dolor
+        ac urna bibendum vehicula. Maecenas vel viverra leo. Donec viverra nunc
+        non lacus eleifend tristique. Maecenas eros nibh, ornare eu erat sit
+        amet, tempor iaculis metus. Suspendisse potenti.
+      </p>
 
-      <div className={"flex gap-12 font-extrabold text-xl justify-center"}>
-        <a
-          href="/stamping/stamp"
-          className={"border-2 border-[#999999] text-[#999999] w-[136px] h-[60px] flex justify-center items-center rounded-md"}
-        >
-          FAQ
-        </a>
-        <a
-          href="/faq"
-          className={"bg-[#999999] text-black w-[136px] h-[60px] flex justify-center items-center rounded-md"}
-        >
-          STAMP
-        </a>
-      </div>
+      <a
+        href="/faq"
+        className={"bg-[#999999] text-black w-[136px] h-[60px] flex justify-center items-center rounded-md"}
+      >
+        FAQ
+      </a>
     </div>
   );
 };
