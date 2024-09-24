e1ce507aa3b87ffcab6a1e8fe9ace7e63f2a6fb0
dev4pear <tommycole519@gmail.com>
2024-09-24 07:59:25 -0400

    chore: complete carousel card; update src20-stamping layout;

--- routes/stamping/src20/[action].tsx
+++ routes/stamping/src20/[action].tsx
@@ -26,57 +26,45 @@ export default function StampingSrc20Page(
   const { selectedTab, trxType } = data;
 
   return (
-    <div className={"flex flex-col gap-16"}>
+    <div className="flex flex-col gap-16">
       <StampingSrc20Header selectedTab={selectedTab} />
       <div className="self-center max-w-[680px] mx-auto">
         {selectedTab === "mint" && <MintContent trxType={trxType} />}
         {selectedTab === "deploy" && <DeployContent trxType={trxType} />}
         {selectedTab === "transfer" && <TransferContent trxType={trxType} />}
       </div>
-      <div className={"flex flex-col md:flex-row gap-6 w-full px-2 md:px-0"}>
-        <div className={"w-full md:w-1/2"}>
+      <div className="flex flex-col md:flex-row gap-6 w-full px-2 md:px-0">
+        <div className="w-full md:w-1/2">
           <FAQModule />
         </div>
         {selectedTab === "mint" && (
-          <div
-            className={"w-full md:w-1/2 flex flex-col gap-4 items-start md:items-end"}
-          >
-            <h1
-              className={"bg-clip-text text-transparent bg-gradient-to-r from-[#AA00FF] via-[#660099] to-[#440066] text-3xl md:text-6xl font-black"}
-            >
+          <div className="w-full md:w-1/2 flex flex-col gap-4 items-start md:items-end">
+            <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-[#AA00FF] via-[#660099] to-[#440066] text-3xl md:text-6xl font-black">
               MINTING
             </h1>
-            <p className={"text-2xl md:text-5xl text-[#AA00FF]"}>LOREM IPSUM</p>
+            <p className="text-2xl md:text-5xl text-[#AA00FF]">LOREM IPSUM</p>
             <a class="text-[#660099] text-sm md:text-base font-light border-2 border-[#660099] py-1 text-center min-w-[132px] rounded-md cursor-pointer">
               View All
             </a>
           </div>
         )}
         {selectedTab === "deploy" && (
-          <div
-            className={"w-full md:w-1/2 flex flex-col gap-4 items-start md:items-end"}
-          >
-            <h1
-              className={"bg-clip-text text-transparent bg-gradient-to-r from-[#AA00FF] via-[#660099] to-[#440066] text-3xl md:text-6xl font-black"}
-            >
+          <div className="w-full md:w-1/2 flex flex-col gap-4 items-start md:items-end">
+            <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-[#AA00FF] via-[#660099] to-[#440066] text-3xl md:text-6xl font-black">
               RECENT DEPLOYS
             </h1>
-            <p className={"text-2xl md:text-5xl text-[#AA00FF]"}>LOREM IPSUM</p>
+            <p className="text-2xl md:text-5xl text-[#AA00FF]">LOREM IPSUM</p>
             <a class="text-[#660099] text-sm md:text-base font-light border-2 border-[#660099] py-1 text-center min-w-[132px] rounded-md cursor-pointer">
               View All
             </a>
           </div>
         )}
         {selectedTab === "transfer" && (
-          <div
-            className={"w-full md:w-1/2 flex flex-col gap-4 items-start md:items-end"}
-          >
-            <h1
-              className={"bg-clip-text text-transparent bg-gradient-to-r from-[#AA00FF] via-[#660099] to-[#440066] text-3xl md:text-6xl font-black"}
-            >
+          <div className="w-full md:w-1/2 flex flex-col gap-4 items-start md:items-end">
+            <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-[#AA00FF] via-[#660099] to-[#440066] text-3xl md:text-6xl font-black">
               LATEST TRANSFERS
             </h1>
-            <p className={"text-2xl md:text-5xl text-[#AA00FF]"}>LOREM IPSUM</p>
+            <p className="text-2xl md:text-5xl text-[#AA00FF]">LOREM IPSUM</p>
             <a class="text-[#660099] text-sm md:text-base font-light border-2 border-[#660099] py-1 text-center min-w-[132px] rounded-md cursor-pointer">
               View All
             </a>
