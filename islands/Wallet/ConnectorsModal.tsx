26cba37a9015d15b361c887a604401da300f4414
reinamora <reinamora@stampchain.io>
2024-10-07 17:29:53 +0000

    fix: connector modal sometimes white problem, buy modal simlar problems not displaying

--- islands/Wallet/ConnectorsModal.tsx
+++ islands/Wallet/ConnectorsModal.tsx
@@ -22,9 +22,9 @@ export const ConnectorsModal = (
       onClick={handleCloseModal}
     >
       <div class="relative p-4 w-full max-w-2xl h-auto">
-        <div class="relative bg-white rounded-lg shadow dark:bg-[#0B0B0B] overflow-hidden">
-          <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-[#8800CC]">
-            <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+        <div class="relative bg-[#0B0B0B] rounded-lg shadow overflow-hidden">
+          <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t border-[#8800CC]">
+            <h3 class="text-xl font-semibold text-white">
               Connect your wallet
             </h3>
             <button
@@ -33,12 +33,11 @@ export const ConnectorsModal = (
                 showConnectWalletModal.value = false;
               }}
               type="button"
-              class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-              data-modal-hide="default-modal"
+              class="text-gray-400 hover:bg-gray-600 hover:text-white rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
+              aria-label="Close modal"
             >
               <svg
                 class="w-3 h-3"
-                aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 14 14"
@@ -48,14 +47,13 @@ export const ConnectorsModal = (
                   stroke-linecap="round"
                   stroke-linejoin="round"
                   stroke-width="2"
-                  d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                  d="M1 1l6 6m0 0l6 6M7 7l6-6M7 7L1 13"
                 />
               </svg>
-              <span class="sr-only">Close modal</span>
             </button>
           </div>
 
-          <div class="grid grid-cols-1 sm:grid-cols-2 gap-2 items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-[#8800CC]">
+          <div class="grid grid-cols-1 sm:grid-cols-2 gap-2 items-center p-4 md:p-5 border-t border-[#8800CC] rounded-b">
             {Object.keys(WALLET_PROVIDERS).map((providerKey) => (
               <WalletConnector
                 key={providerKey}
--- islands/stamp/details/StampBuyModal.tsx
+++ islands/stamp/details/StampBuyModal.tsx
@@ -157,24 +157,23 @@ const StampBuyModal = (
 
   return (
     <div
-      class="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden bg-[#181818] bg-opacity-50 backdrop-filter backdrop-blur-sm"
+      class="fixed inset-0 z-50 flex items-center justify-center bg-[#181818] bg-opacity-50 backdrop-filter backdrop-blur-sm"
       onClick={handleCloseModal}
     >
       <div class="relative p-4 w-full max-w-[360px] h-auto">
         <div
-          class="relative bg-white rounded-lg shadow dark:bg-[#0B0B0B] overflow-hidden"
+          class="relative bg-[#0B0B0B] rounded-lg shadow overflow-hidden"
           onClick={(e) => e.stopPropagation()} // Prevent click from closing the modal
         >
           <div class="flex flex-col gap-4 items-center justify-between p-4 md:p-5 rounded-t">
             <button
               onClick={toggleModal}
               type="button"
-              class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-              data-modal-hide="default-modal"
+              class="text-gray-400 hover:bg-gray-600 hover:text-white rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
+              aria-label="Close modal"
             >
               <svg
                 class="w-3 h-3"
-                aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 14 14"
@@ -184,10 +183,9 @@ const StampBuyModal = (
                   stroke-linecap="round"
                   stroke-linejoin="round"
                   stroke-width="2"
-                  d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                  d="M1 1l6 6m0 0l6 6M7 7l6-6M7 7L1 13"
                 />
               </svg>
-              <span class="sr-only">Close modal</span>
             </button>
             <StampImage stamp={stamp} className="w-[200px] !p-2" flag={false} />
             <p
