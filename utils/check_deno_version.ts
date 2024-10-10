7500a5ef587876bcb3f076cd89b8950f68b2d895
reinamora <reinamora@stampchain.io>
2024-10-10 05:10:16 +0000

    chore: readd build to dockerfile, Deno 2.0 initial updates (todo: npm package.json)

---
+++ utils/check_deno_version.ts
@@ -0,0 +1,12 @@
+const requiredVersion = "2.0.0";
+const currentVersion = Deno.version.deno;
+
+if (currentVersion !== requiredVersion) {
+  console.error(
+    `Your Deno version is ${currentVersion}, but version ${requiredVersion} is required.`,
+  );
+  console.error(
+    `Please run \`deno upgrade --version ${requiredVersion}\` to update.`,
+  );
+  Deno.exit(1);
+}
