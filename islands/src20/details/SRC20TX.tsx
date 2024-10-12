700c739cb136f3fbab005e93d6cd87ba48aa25ad
dev4pear <tommycole519@gmail.com>
2024-10-12 17:56:34 -0400

    feat: update src20 details layout;

--- islands/src20/details/SRC20TX.tsx
+++ islands/src20/details/SRC20TX.tsx
@@ -71,23 +71,23 @@ export function SRC20TX(props: SRC20TXProps) {
         return (
           <tr
             key={tx.tx_hash}
-            class="w-full table table-fixed text-xs text-[#F5F5F5] border-b border-[#B9B9B9]"
+            class="w-full table table-fixed text-xs"
           >
-            <td class="px-6 py-4">{tx.block_index}</td>
-            <td class="px-6 py-4">{abbreviateAddress(tx.creator)}</td>
-            <td class="px-6 py-4">{abbreviateAddress(tx.destination)}</td>
-            <td class="px-6 py-4">{stripTrailingZeros(tx.amt)}</td>
+            <td class="px-3 py-2">{tx.block_index}</td>
+            <td class="px-3 py-2">{abbreviateAddress(tx.creator)}</td>
+            <td class="px-3 py-2">{abbreviateAddress(tx.destination)}</td>
+            <td class="px-3 py-2">{stripTrailingZeros(tx.amt)}</td>
           </tr>
         );
       } else if (type === "MINT") {
         return (
           <tr
             key={tx.tx_hash}
-            class="w-full table table-fixed text-xs text-[#F5F5F5] border-b border-[#B9B9B9]"
+            class="w-full table table-fixed text-xs"
           >
-            <td class="px-6 py-4">{tx.block_index}</td>
-            <td class="px-6 py-4">{abbreviateAddress(tx.destination)}</td>
-            <td class="px-6 py-4">{stripTrailingZeros(tx.amt)}</td>
+            <td class="px-3 py-2">{tx.block_index}</td>
+            <td class="px-3 py-2">{abbreviateAddress(tx.destination)}</td>
+            <td class="px-3 py-2">{stripTrailingZeros(tx.amt)}</td>
           </tr>
         );
       }
@@ -97,23 +97,23 @@ export function SRC20TX(props: SRC20TXProps) {
 
   return (
     <div
-      class="relative shadow-md sm:rounded-lg w-full overflow-y-auto max-h-[600px]"
+      class="relative shadow-md sm:rounded-lg w-full overflow-y-auto max-h-[250px]"
       ref={containerRef}
       onScroll={handleScroll}
     >
-      <table class="w-full text-sm text-left rtl:text-right text-gray-500">
-        <thead class="table-fixed text-lg font-semibold uppercase text-[#C184FF] border-b border-[#B9B9B9]">
+      <table class="w-full text-sm text-left rtl:text-right">
+        <thead class="table-fixed text-lg font-semibold uppercase text-[#666666]">
           {tableHeaders()}
         </thead>
-        <tbody class="table-fixed">{renderRows()}</tbody>
+        <tbody class="table-fixed text-[#999999]">{renderRows()}</tbody>
       </table>
       {isFetching && (
-        <div class="flex justify-center items-center py-4 text-white">
+        <div class="flex justify-center items-center py-4 text-[#999999]">
           Loading more data...
         </div>
       )}
       {!hasMoreData && (
-        <div class="flex justify-center items-center py-4 text-white">
+        <div class="flex justify-center items-center py-4 text-[#999999]">
           No more data to load.
         </div>
       )}
--- routes/src20/[tick].tsx
+++ routes/src20/[tick].tsx
@@ -1,9 +1,10 @@
 import { Handlers } from "$fresh/server.ts";
-import { SRC20TickHeader } from "$islands/src20/SRC20TickHeader.tsx";
-import { SRC20DetailsTab } from "$islands/src20/SRC20DetailsTab.tsx";
+import { SRC20TickHeader } from "$islands/src20/details/SRC20TickHeader.tsx";
+import { SRC20DetailsTab } from "$islands/src20/details/SRC20DetailsTab.tsx";
 import { convertEmojiToTick } from "utils/util.ts";
 import { Src20Controller } from "$lib/controller/src20Controller.ts";
 import { set_precision } from "bigfloat/mod.ts";
+import { SRC20HoldersInfo } from "$components/src20/SRC20HoldersInfo.tsx";
 
 export const handler: Handlers = {
   async GET(_req: Request, ctx) {
@@ -58,21 +59,17 @@ function SRC20TickPage(props: SRC20TickPageProps) {
 
   return (
     <div class="flex flex-col gap-8">
-      <SRC20TickHeader
-        deployment={deployment}
-        mint_status={mint_status}
-        total_holders={total_holders}
-        total_mints={total_mints}
-        total_transfers={total_transfers}
-      />
-      <div class="w-full flex flex-col md:flex-row gap-20 justify-center">
-        <div class="w-full h-full bg-gradient-to-br from-[#1F002E00] via-[#14001F7F] to-[#1F002EFF] p-6">
-          <SRC20DetailsTab
-            holders={holders}
-            tick={tick} // Pass tick prop
-          />
-        </div>
+      <div className="flex flex-col lg:flex-row gap-8">
+        <SRC20TickHeader
+          deployment={deployment}
+          mint_status={mint_status}
+          total_holders={total_holders}
+          total_mints={total_mints}
+          total_transfers={total_transfers}
+        />
+        <SRC20HoldersInfo holders={holders} />
       </div>
+      <SRC20DetailsTab tick={tick} />
     </div>
   );
 }
