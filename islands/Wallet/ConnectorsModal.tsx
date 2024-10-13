6dceffd88c47a56c226404a414c24d5dd16fea1a
dev4pear <tommycole519@gmail.com>
2024-10-13 18:04:17 -0400

    feat: update wallet connect & buy modal;

--- islands/Wallet/ConnectorsModal.tsx
+++ islands/Wallet/ConnectorsModal.tsx
@@ -14,6 +14,11 @@ export const ConnectorsModal = (
 ) => {
   console.log("Rendering ConnectorsModal with connectors:", connectors);
 
+  const closeModal = () => {
+    toggleModal();
+    showConnectWalletModal.value = false;
+  };
+
   return (
     <div
       class={`fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden bg-[#181818] bg-opacity-50 backdrop-filter backdrop-blur-sm ${
@@ -21,47 +26,29 @@ export const ConnectorsModal = (
       }`}
       onClick={handleCloseModal}
     >
-      <div class="relative p-4 w-full max-w-2xl h-auto">
-        <div class="relative bg-[#0B0B0B] rounded-lg shadow overflow-hidden">
-          <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t border-[#8800CC]">
-            <h3 class="text-xl font-semibold text-white">
-              Connect your wallet
-            </h3>
-            <button
-              onClick={() => {
-                toggleModal();
-                showConnectWalletModal.value = false;
-              }}
-              type="button"
-              class="text-gray-400 hover:bg-gray-600 hover:text-white rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
-              aria-label="Close modal"
-            >
-              <svg
-                class="w-3 h-3"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 14"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M1 1l6 6m0 0l6 6M7 7l6-6M7 7L1 13"
-                />
-              </svg>
-            </button>
-          </div>
+      <div class="relative w-full max-w-2xl h-auto bg-[#0B0B0B] rounded-lg shadow overflow-hidden p-4 md:p-6 flex flex-col gap-6">
+        <img
+          onClick={closeModal}
+          src="/img/wallet/icon-close.svg"
+          class="w-6 h-6 absolute top-6 right-6 cursor-pointer"
+          alt="Close modal"
+        />
 
-          <div class="grid grid-cols-1 sm:grid-cols-2 gap-2 items-center p-4 md:p-5 border-t border-[#8800CC] rounded-b">
-            {Object.keys(WALLET_PROVIDERS).map((providerKey) => (
-              <WalletConnector
-                key={providerKey}
-                providerKey={providerKey as WalletProviderKey}
-                toggleModal={toggleModal}
-              />
-            ))}
-          </div>
+        <h3 class="text-5xl font-black bg-clip-text text-transparent bg-gradient-to-r from-[#440066] via-[#660099] to-[#8800CC]">
+          CONNECT<br />
+          <span className="text-4xl font-extralight text-[#AA00FF]">
+            YOUR WALLET
+          </span>
+        </h3>
+
+        <div class="grid grid-cols-1 sm:grid-cols-2 gap-2 md:gap-4 items-center">
+          {Object.keys(WALLET_PROVIDERS).map((providerKey) => (
+            <WalletConnector
+              key={providerKey}
+              providerKey={providerKey as WalletProviderKey}
+              toggleModal={toggleModal}
+            />
+          ))}
         </div>
       </div>
     </div>
