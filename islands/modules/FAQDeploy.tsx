2a692d35febc4852a6290035ab6d80089c7c8428
TonyNL <86718065+ARNLTony@users.noreply.github.com>
2024-10-15 22:51:38 +0200

    Update FAQDeploy.tsx
    added text and how to

--- islands/modules/FAQDeploy.tsx
+++ islands/modules/FAQDeploy.tsx
@@ -2,10 +2,10 @@ export const FAQModule = () => {
   return (
     <div className="flex flex-col gap-4">
       <p className="font-black text-4xl md:text-7xl flex flex-col bg-clip-text text-transparent bg-gradient-to-r from-[#666666] via-[#999999] to-[#CCCCCC] text-left">
-        FAQ
+        Deploy
       </p>
       <p className="flex flex-col bg-clip-text text-transparent bg-gradient-to-r from-[#666666] to-[#999999] text-left text-3xl md:text-6xl font-extralight text-[#CCCCCC]">
-        Let's Deploy!
+        Fields and Options explanation
       </p>
 
       <p className="text-[#999999] text-base md:text-2xl font-medium">
@@ -31,7 +31,7 @@ export const FAQModule = () => {
           f-partial="/faq"
           className="border md:border-2 border-[#999999] rounded-md bg-transparent text-[#999999] font-extrabold w-[63px] md:w-[84px] h-[36px] md:h-[48px] flex justify-center items-center"
         >
-          FAQ
+          Step by step How to Deploy
         </a>
       </div>
     </div>
