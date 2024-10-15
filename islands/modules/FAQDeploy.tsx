5cedc2956948a1fa1266a9cc70d734fd7d5f0b62
TonyNL <86718065+ARNLTony@users.noreply.github.com>
2024-10-15 22:21:13 +0200

    Update FAQDeploy.tsx
     <br />  added

--- islands/modules/FAQDeploy.tsx
+++ islands/modules/FAQDeploy.tsx
@@ -9,19 +9,19 @@ export const FAQModule = () => {
       </p>
 
       <p className="text-[#999999] text-base md:text-2xl font-medium">
-        Click the icon to upload your ticker artwork in a supported format.
-        The token ticker name must be unique and no longer than ## characters.
-        Use the TOGGLE to switch between Simple and Expert modes to customize XXXXXXXXXXXXX.
-        Supply defines the number of tokens, between # and ###########.
-        Decimals specify how many decimal places your token will have (similar to Satoshis for Bitcoin).
-        Limit per Mint sets the maximum number of tokens that can be minted in a single session.
-        In the Description field, provide details on the token’s utility or purpose.
-        Fill in additional token information, such as your website, X (Twitter) handle, email, and Telegram.
-        FEES shows the suggested amount, adjustable via the slider. Lowering the fee might slow down the stamping process.
-        Fees are displayed in BTC by default, but you can toggle to switch to USDT.
-        All related costs are detailed in the DETAILS section.
-        Accept the terms and conditions to activate the DEPLOY button.
-        The DEPLOY button will submit your transaction with all the provided details.
+        Click the icon to upload your ticker artwork in a supported format.<br />
+        The token ticker name must be unique and no longer than ## characters.<br />
+        Use the TOGGLE to switch between Simple and Expert modes to customize XXXXXXXXXXXXX.<br />
+        Supply defines the number of tokens, between # and ###########.<br />
+        Decimals specify how many decimal places your token will have (similar to Satoshis for Bitcoin).<br />
+        Limit per Mint sets the maximum number of tokens that can be minted in a single session.<br />
+        In the Description field, provide details on the token’s utility or purpose.<br />
+        Fill in additional token information, such as your website, X (Twitter) handle, email, and Telegram.<br />
+        FEES shows the suggested amount, adjustable via the slider. Lowering the fee might slow down the stamping process.<br />
+        Fees are displayed in BTC by default, but you can toggle to switch to USDT.<br />
+        All related costs are detailed in the DETAILS section.<br />
+        Accept the terms and conditions to activate the DEPLOY button.<br />
+        The DEPLOY button will submit your transaction with all the provided details.<br />
                 
       </p>
 
