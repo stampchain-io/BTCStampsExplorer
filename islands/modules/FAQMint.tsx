7eaa81cdce5f6d081320ad7e9674fed6e8153b48
TonyNL <86718065+ARNLTony@users.noreply.github.com>
2024-10-15 23:58:50 +0200

    Tonynl/faq update (#122)
    * Update FAQStamping.tsx
    
    Change in title
    
    * Update FAQMint.tsx
    
    changed title
    
    * Update FAQTransfer.tsx
    
    updated text
    
    * Update FAQDeploy.tsx
    
    changed title
    
    * Update FAQMint.tsx
    
    font-small

--- islands/modules/FAQMint.tsx
+++ islands/modules/FAQMint.tsx
@@ -2,13 +2,14 @@ export const FAQModule = () => {
   return (
     <div className="flex flex-col gap-4">
       <p className="font-black text-4xl md:text-7xl flex flex-col bg-clip-text text-transparent bg-gradient-to-r from-[#666666] via-[#999999] to-[#CCCCCC] text-left">
-       Mint
+       How to
       </p>
       <p className="flex flex-col bg-clip-text text-transparent bg-gradient-to-r from-[#666666] to-[#999999] text-left text-3xl md:text-6xl font-extralight text-[#CCCCCC]">
-        Fields and Options explanation
+        Mint
       </p>
 
-      <p className="text-[#999999] text-base md:text-2xl font-medium">Start typing a token name to filter and select the correct ticker. The ticker logo and name will be shown along with the maximum limit per mint.
+      <p className="text-[#999999] text-base md:text-2xl font-small">
+        Start typing a token name to filter and select the correct ticker. The ticker logo and name will be shown along with the maximum limit per mint.
         Double-check that the displayed information matches the ticker you wish to mint.<br />
         If Progress is near 95%, be cautious as you risk losing funds. At 100% or above, your funds will be lost.<br />
         FEES displays the suggested amount, which can be adjusted using the slider. Reducing the fee may slow down the stamping process.<br />
@@ -24,7 +25,7 @@ export const FAQModule = () => {
           f-partial="/faq"
           className="border md:border-2 border-[#999999] rounded-md bg-transparent text-[#999999] font-extrabold w-[63px] md:w-[84px] h-[36px] md:h-[48px] flex justify-center items-center"
         >
-          Step by step How to Mint
+          How to Mint
         </a>
       </div>
     </div>
