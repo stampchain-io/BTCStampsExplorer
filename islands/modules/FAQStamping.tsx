1000601cb97740eaedae62e6cc870cc6a37ff7b6
TonyNL <86718065+ARNLTony@users.noreply.github.com>
2024-10-16 17:44:54 +0200

    Update FAQStamping.tsx
    Stamping FAQ updated

--- islands/modules/FAQStamping.tsx
+++ islands/modules/FAQStamping.tsx
@@ -2,7 +2,7 @@ export const FAQModule = () => {
   return (
     <div className="flex flex-col gap-4">
       <p className="font-black text-4xl md:text-7xl flex flex-col bg-clip-text text-transparent bg-gradient-to-r from-[#666666] via-[#999999] to-[#CCCCCC] text-left">
-        How to
+        HOW-TO
       </p>
       <p className="flex flex-col bg-clip-text text-transparent bg-gradient-to-r from-[#666666] to-[#999999] text-left text-3xl md:text-6xl font-extralight text-[#CCCCCC]">
         Stamp
@@ -10,14 +10,14 @@ export const FAQModule = () => {
 
       <p className="text-[#999999] text-base md:text-2xl font-medium">
         Click the icon to upload your artwork in a supported format.<br />
-        Use the TOGGLE to switch between CLASSIC and POSH Art, and add a STAMP NAME.<br />
-        EDITIONS sets the number of copies you want to create.<br />
-        The LOCK icon is enabled by default, preventing future changes to the EDITIONS.<br />
-        FEES shows the suggested amount, and you can adjust it with the slider. Lowering the fee may delay your art being stamped.<br />
-        Fees are displayed in BTC by default, but you can switch to USDT using the TOGGLE.<br />
-        All related costs are listed under the DETAILS section.<br />
-        Accept the terms and conditions to enable the STAMP button.<br />
-        The STAMP button will submit your transaction with all the provided information.<br />
+        Use the <b>toggle</b> to switch between <b>classic</b> and <b>posh</b> art, and add a <b>stamp name</b>.<br />
+        <b>Editions</b> sets the number of copies you want to create.<br />
+        The <b>lock</b> icon is enabled by default, preventing future changes to the edtions.<br />
+        <b>Fees</b> section shows the suggested amount, and you can adjust it with the slider. Lowering the fee may delay your art being stamped.<br />
+        Fees are displayed in <b>BTC</b> by default, but you can switch to <b>USDT</b> using the <b>toggle</b>.<br />
+        All related costs are listed under the <b>details</b> section.<br />
+        Accept the terms and conditions to enable the <b>stamp</b> button.<br />
+        The <b>stamp</b> button will submit your transaction with all the provided information.<br />
                 
       </p>
 
@@ -27,7 +27,7 @@ export const FAQModule = () => {
           f-partial="/faq"
           className="border md:border-2 border-[#999999] rounded-md bg-transparent text-[#999999] font-extrabold w-[63px] md:w-[84px] h-[36px] md:h-[48px] flex justify-center items-center"
         >
-          how to stamp
+          READ ALL
         </a>
       </div>
     </div>
