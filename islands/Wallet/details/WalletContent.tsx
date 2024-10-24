cd2315f735a55d4800cb190340f018bfa02a6ea2
eliot1011 <henry.scott0x1011@gmail.com>
2024-10-24 23:00:30 +0900

    update the wallet content about stamp, dispenser, token and fix the scroll bar style

--- islands/Wallet/details/WalletContent.tsx
+++ islands/Wallet/details/WalletContent.tsx
@@ -1,25 +1,32 @@
 import { StampCard } from "$islands/stamp/StampCard.tsx";
+import { StampDispensers } from "$components/stampDetails/StampDispensers.tsx";
 import StampingMintingItem from "$islands/stamping/src20/mint/StampingMintingItem.tsx";
+import { SRC20DeployTable } from "$islands/src20/all/SRC20DeployTable.tsx";
 
 interface WalletContentProps {
   stamps: any[];
   src20: any[];
+  showItem: string;
 }
 
-function WalletContent({ stamps, src20 }: WalletContentProps) {
+function WalletContent({ stamps, src20, showItem }: WalletContentProps) {
   return (
     <>
       <div className="grid grid-cols-4 md:grid-cols-3 xl:grid-cols-4 gap-2 md:gap-4">
-        {stamps.map((stamp, index) => (
-          <StampCard
-            key={index}
-            stamp={stamp}
-            kind="stamp"
-            isRecentSale={false}
-            showInfo={true}
-            showDetails={true}
-          />
-        ))}
+        {showItem === "stamp"
+          ? stamps.map((stamp, index) => (
+            <StampCard
+              key={index}
+              stamp={stamp}
+              kind="stamp"
+              isRecentSale={false}
+              showInfo={true}
+              showDetails={true}
+            />
+          ))
+          : showItem === "dispenser"
+          ? <StampDispensers dispensers={[]} />
+          : <SRC20DeployTable data={src20} />}
       </div>
 
       <div className="grid grid-cols-2 gap-2 md:gap-4">
