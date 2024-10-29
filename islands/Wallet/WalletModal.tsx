902093923b2bab61574a21361590e6dc2f88c1b1
dev4pear <tommycole519@gmail.com>
2024-10-29 16:20:05 -0400

    chore: update header & footer remaining issues

--- islands/Wallet/WalletModal.tsx
+++ islands/Wallet/WalletModal.tsx
@@ -292,7 +292,7 @@ export const WalletModal = ({ connectors = [] }: Props) => {
             isConnected && address
               ? "text-[#8800CC] border-[#8800CC] border-2 rounded-md"
               : "bg-[#8800CC] hover:bg-[#9911DD] text-[#080808] "
-          }  px-5 py-3 rounded font-extrabold text-sm xl:text-base`}
+          }  px-5 py-4 rounded font-extrabold text-sm leading-[16px] sm:text-base sm:leading-[19px]`}
         >
           {isConnected && address ? abbreviateAddress(address) : "CONNECT"}
         </button>
