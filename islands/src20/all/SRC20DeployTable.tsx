01e0afdbc43c651c1040348246c72ceffd8974d4
RED127 <121668232+RED127@users.noreply.github.com>
2024-10-29 23:20:05 +0900

    Horizontal table scroll bar does not match figma & BTC Price and Fee-Rate displaying in middle of the page on /home for mobile & scaling on these on the /src20 page is wacky & Stamp tool mint window does not match figma

--- islands/src20/all/SRC20DeployTable.tsx
+++ islands/src20/all/SRC20DeployTable.tsx
@@ -62,9 +62,11 @@ export const SRC20DeployTable = (props: SRC20BalanceTableProps) => {
               return null;
             }
             const href = `/src20/${convertToEmoji(src20.tick)}`;
+            const progress = src20.progress || "0";
+            const progressWidth = `${progress}%`;
             return (
               <div class="bg-gradient-to-br from-transparent from-0% via-[#14001F] to-[#1F002E] text-sm flex justify-between rounded-md hover:border-[#9900EE] hover:shadow-[0px_0px_20px_#9900EE]">
-                <div class="p-3 uppercase cursor-pointer flex gap-6">
+                <div class="p-3 uppercase cursor-pointer flex justify-center items-center gap-6">
                   <img
                     src={`/content/${src20.tx_hash}.svg`}
                     class="w-[65px] h-[65px]"
@@ -104,62 +106,141 @@ export const SRC20DeployTable = (props: SRC20BalanceTableProps) => {
                         )}
                       </div>
                     </a>
-                    <p className="text-lg text-[#666666] font-light">
-                      CREATOR{" "}
+                    <p className="text-base sm:text-lg text-[#666666] font-light">
+                      SUPPLY{" "}
                       <span className="font-bold text-[#999999]">
-                        {src20.destination_name
-                          ? src20.destination_name
-                          : abbreviateAddress(src20.destination)}
+                        {Number(src20.max).toLocaleString()}
                       </span>
                     </p>
+                    {progress == "100"
+                      ? (
+                        <p className="text-lg text-[#666666] font-light">
+                          MARKETCAP{" "}
+                          <span className="font-bold text-[#999999]">
+                            {Number(src20.mcap).toFixed(2).toLocaleString()}
+                          </span>
+                        </p>
+                      )
+                      : (
+                        <p className="text-lg text-[#666666] font-light">
+                          LIMIT{" "}
+                          <span className="font-bold text-[#999999]">
+                            {Number(src20.lim).toFixed(2).toLocaleString()}
+                          </span>
+                        </p>
+                      )}
                   </div>
                 </div>
                 <div class="p-3 text-center flex flex-col justify-center">
-                  <p className="text-lg text-[#666666] font-light">
-                    SUPPLY{" "}
-                    <span className="font-bold text-[#999999]">
-                      {Number(src20.max).toLocaleString()}
-                    </span>
-                  </p>
-                  <p className="text-lg text-[#666666] font-light">
-                    LIMIT{" "}
-                    <span className="font-bold text-[#999999]">
-                      {Number(src20.lim).toLocaleString()}
-                    </span>
-                  </p>
+                  {progress == "100"
+                    ? (
+                      <>
+                        <p className="text-lg text-[#666666] font-light">
+                          PRICE{" "}
+                          <span className="font-bold text-[#999999]">
+                            {Number(src20.floor_unit_price).toFixed(10)
+                              .toLocaleString()}
+                          </span>
+                        </p>
+                        <p className="text-lg text-[#666666] font-light">
+                          CHANGE{" "}
+                          <span className="font-bold text-[#999999]">
+                            {Number(src20.p).toLocaleString()}
+                          </span>
+                        </p>
+                        <p className="text-lg text-[#666666] font-light">
+                          VOLUME{" "}
+                          <span className="font-bold text-[#999999]">
+                            {Number(src20.lim).toLocaleString()}
+                          </span>
+                        </p>
+                      </>
+                    )
+                    : (
+                      <>
+                        <p className="text-lg text-[#666666] font-light">
+                          TOP MINTS{" "}
+                          <span className="font-bold text-[#999999]">
+                            {Number(src20.max).toFixed(10)
+                              .toLocaleString()} %
+                          </span>
+                        </p>
+                        <div className="flex flex-col gap-1">
+                          <p className="text-lg font-light text-[#999999]">
+                            PROGRESS{" "}
+                            <span className="font-bold">{progress}%</span>
+                          </p>
+                          <div className="min-w-[260px] h-1 bg-[#999999] relative rounded-full">
+                            <div
+                              className="absolute left-0 top-0 h-1 bg-[#660099] rounded-full"
+                              style={{ width: progressWidth }}
+                            />
+                          </div>
+                        </div>
+                      </>
+                    )}
                 </div>
-                <div class="p-3 text-sm text-center flex flex-col justify-center">
-                  <p className="text-lg text-[#666666] font-light">
-                    DEPLOY{" "}
-                    <span className="font-bold text-[#999999]">
-                      {new Date(src20.block_time).toLocaleString("default", {
-                        month: "short",
-                        year: "numeric",
-                      })}
-                    </span>
-                  </p>
-                  <p className="text-lg text-[#666666] font-light">
-                    HOLDERS{" "}
-                    <span className="font-bold text-[#999999]">
-                      {Number(src20.holders).toLocaleString()}
-                    </span>
-                  </p>
-                </div>
-                <div class="p-3 text-right flex flex-col justify-center">
-                  <p className="text-lg text-[#666666] font-light">
-                    MARKETCAP{" "}
-                    <span className="font-bold text-[#999999]">
-                      {Number(src20.mcap).toFixed(2).toLocaleString()}
-                    </span>
-                  </p>
-                  <p className="text-lg text-[#666666] font-light">
-                    PRICE{" "}
-                    <span className="font-bold text-[#999999]">
-                      {Number(src20.floor_unit_price).toFixed(10)
-                        .toLocaleString()}
-                    </span>
-                  </p>
-                </div>
+                {(progress == "100" && src20?.amt)
+                  ? (
+                    <div className="flex flex-col justify-end items-end">
+                      <p className="text-2xl text-[#666666] font-light">
+                        VALUE{" "}
+                      </p>
+                      <p className="text-2xl text-[#999999] font-light">
+                        {src20?.amt}{" "}
+                        <span className="font-bold text-[#666666]">
+                          BTC
+                        </span>
+                      </p>
+                    </div>
+                  )
+                  : (
+                    progress == "100" && (
+                      <div class="p-3 text-right flex flex-col justify-center">
+                        <p className="text-lg text-[#666666] font-light text-center">
+                          HOLDERS{" "}
+                          <span className="font-bold text-[#999999]">
+                            {Number(src20.holders).toLocaleString()}
+                          </span>
+                        </p>
+                        <p className="text-lg text-[#666666] font-light text-center">
+                          DEPLOY{" "}
+                          <span className="font-bold text-[#999999]">
+                            {new Date(src20.block_time).toLocaleString(
+                              "default",
+                              {
+                                month: "short",
+                                year: "numeric",
+                              },
+                            )}
+                          </span>
+                        </p>
+                        <p className="text-lg text-[#666666] font-light text-center">
+                          CREATOR{" "}
+                          <span className="font-bold text-[#999999]">
+                            {src20.destination_name
+                              ? src20.destination_name
+                              : abbreviateAddress(src20.destination)}
+                          </span>
+                        </p>
+                      </div>
+                    )
+                  )}
+                {progress !== "100" && (
+                  <div class="flex justify-center items-center p-2">
+                    <a
+                      href={`/stamping/src20/mint?tick=${
+                        encodeURIComponent(
+                          src20.tick,
+                        )
+                      }&trxType=${encodeURIComponent(src20.tx_hash)}`}
+                    >
+                      <button className="bg-[#8800CC] rounded-md text-[#080808] text-sm font-black w-[84px] h-[48px]">
+                        Mint
+                      </button>
+                    </a>
+                  </div>
+                )}
               </div>
             );
           })}
@@ -170,6 +251,9 @@ export const SRC20DeployTable = (props: SRC20BalanceTableProps) => {
           {data.map((src20: SRC20Row) => {
             const href = `/src20/${convertToEmoji(src20.tick)}`;
             // Ensure src20.tick is defined
+            const progress = src20.progress || "0";
+            const progressWidth = `${progress}%`;
+
             return (
               <div class="text-[#F5F5F5] bg-gradient-to-br from-transparent from-0% via-[#14001F] to-[#1F002E] hover:border-[#9900EE] hover:shadow-[0px_0px_20px_#9900EE] p-2">
                 <div class="w-full flex items-center gap-2 mb-2">
@@ -179,7 +263,7 @@ export const SRC20DeployTable = (props: SRC20BalanceTableProps) => {
                     onClick={() =>
                       handleImageInteraction(`/content/${src20.tx_hash}.svg`)}
                   />
-                  <div class="w-full">
+                  <div class="w-full flex flex-col">
                     <div class="flex justify-between">
                       <a href={href} class="flex gap-4 text-xl uppercase">
                         {convertToEmoji(src20.tick)}
@@ -210,43 +294,40 @@ export const SRC20DeployTable = (props: SRC20BalanceTableProps) => {
                           )}
                         </div>
                       </a>
-                      <p className="text-lg text-[#666666] font-light">
-                        SUPPLY{" "}
-                        <span className="font-bold text-[#999999]">
-                          {Number(src20.max).toLocaleString()}
-                        </span>
-                      </p>
                     </div>
-                    <div className="flex justify-between">
-                      <p className="text-lg text-[#666666] font-light">
-                        MARKETCAP{" "}
-                        <span className="font-bold text-[#999999]">
-                          {Number(src20.mcap).toFixed(2).toLocaleString()}
-                        </span>
-                      </p>
-                      <p className="text-lg text-[#666666] font-light">
-                        CREATOR{" "}
-                        <span className="font-bold text-[#999999]">
-                          {src20.destination_name
-                            ? src20.destination_name
-                            : abbreviateAddress(src20.destination)}
-                        </span>
-                      </p>
-                    </div>
-                    <div class="flex justify-between">
-                      <p className="text-lg text-[#666666] font-light">
-                        PRICE{" "}
-                        <span className="font-bold text-[#999999]">
-                          {Number(src20.floor_unit_price).toFixed(10)
-                            .toLocaleString()}
-                        </span>
-                      </p>
-                      <p className="text-lg text-[#666666] font-light">
-                        HOLDERS{" "}
-                        <span className="font-bold text-[#999999]">
-                          {Number(src20.holders).toLocaleString()}
-                        </span>
-                      </p>
+                    <div className="flex flex-col justify-start gap-1">
+                      {progress == "100"
+                        ? (
+                          <>
+                            <p className="text-lg text-[#666666] font-light flex gap-1">
+                              PRICE{" "}
+                              <span className="font-bold text-[#999999]">
+                                {Number(src20.floor_unit_price).toFixed(10)
+                                  .toLocaleString()}
+                              </span>
+                            </p>
+                            <p className="text-lg text-[#666666] font-light flex gap-1">
+                              MARKETCAP{" "}
+                              <span className="font-bold text-[#999999]">
+                                {Number(src20.mcap).toFixed(2).toLocaleString()}
+                              </span>
+                            </p>
+                          </>
+                        )
+                        : (
+                          <div className="flex flex-col gap-1">
+                            <p className="text-lg font-light text-[#999999]">
+                              PROGRESS{" "}
+                              <span className="font-bold">{progress}%</span>
+                            </p>
+                            <div className="min-w-[180px] h-1 bg-[#999999] relative rounded-full">
+                              <div
+                                className="absolute left-0 top-0 h-1 bg-[#660099] rounded-full"
+                                style={{ width: progressWidth }}
+                              />
+                            </div>
+                          </div>
+                        )}
                     </div>
                   </div>
                 </div>
