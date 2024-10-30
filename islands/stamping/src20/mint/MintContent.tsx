c400ca06825f6bedcf82e7beffb5fe67be7b86f7
dev4pear <tommycole519@gmail.com>
2024-10-30 18:52:52 -0400

    feat: fix minor overlapping issues on fee estimation panel

--- islands/stamping/src20/mint/MintContent.tsx
+++ islands/stamping/src20/mint/MintContent.tsx
@@ -205,7 +205,7 @@ export function MintContent({
         />
       </div>
 
-      <div className="dark-gradient p-3 md:p-6 w-full">
+      <div className="dark-gradient p-3 md:p-6 w-full z-[10]">
         <FeeEstimation
           fee={formState.fee}
           handleChangeFee={handleChangeFee}
