0b760cede617ac7bba131342583c896c5d80e98d
TonyNL <86718065+ARNLTony@users.noreply.github.com>
2024-10-30 14:36:19 +0100

    Update index.tsx
    Update how to /deplytoken/howto

--- routes/howto/deploytoken/index.tsx
+++ routes/howto/deploytoken/index.tsx
@@ -36,46 +36,53 @@ export default function HowTo() {
     <div className="text-[#CCCCCC] text-lg font-medium flex flex-col gap-12 mt-20 md:mt-40">
       <div className="max-w-[660px] w-full mx-auto flex flex-col gap-12">
         <section>
-          <h1 className="gray-gradient3 text-6xl font-black">HEADING</h1>
+          <h1 className="gray-gradient3 text-6xl font-black">HOW-TO</h1>
           <h2 className="text-2xl md:text-5xl font-extralight mb-3">
-            SUBHEADING
+            DEPLOY TOKEN
           </h2>
           <p className="mb-12">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id sem
-            vitae lacus venenatis blandit. Curabitur pharetra ipsum id mauris
-            pulvinar auctor. Ut sit amet diam condimentum, vehicula justo in,
-            text link. Aenean tincidunt nisl mattis, bibendum velit at, dictum
-            leo. Nam tempus suscipit velit non interdum. Donec ullamcorper, ante
-            ac condimentum suscipit, diam lorem luctus nulla, at dictum diam
-            nunc ornare ante. Ut elementum porta ante, malesuada lacinia dolor
-            rutrum vel. Vivamus rutrum volutpat sagittis. Vestibulum ante ipsum
-            primis in faucibus orci luctus et ultrices posuere cubilia curae;
-            Nullam vel sem non enim pulvinar suscipit.
+            SRC-20 is a fungible token protocol that records transactions directly on the Bitcoin blockchain, eliminating the need for Counterparty since block 796,000.<br /> 
+            Drawing inspiration from BRC-20, SRC-20 leverages standard BTC miner fees while ensuring data immutability. <br />
+            In this guide, you'll learn how to deploy your own SRC-20 token! <br/> <br/>
+            Note: Before starting, please ensure that your wallet is connected to stmapchain.io and has sufficient funds.
+            
           </p>
           <PlaceholderImage className="w-full md:w-[660px] h-[318px]" />
           <p className="mt-6">
-            Ut elementum porta ante, malesuada lacinia dolor rutrum vel. Vivamus
-            rutrum volutpat sagittis. Vestibulum text link primis in faucibus
-            orci luctus et ultrices posuere cubilia curae; Nullam vel sem non
-            enim pulvinar suscipit.
+            Click the icon to upload your ticker artwork in a supported format.<br/>
+            <br/>
+            The token ticker name must be unique and no longer than ## characters.<br/>
+            <br/>
+            Use the TOGGLE to switch between Simple and Expert modes to customize XXXXXXXXXXXXX.<br />
+            <br />
+            Supply defines the number of tokens, between # and ###########.<br />
+            <br/>
+            Decimals specify how many decimal places your token will have (similar to Satoshis for Bitcoin).<br />
+            <br />
+            Limit per Mint sets the maximum number of tokens that can be minted in a single session.<br />
+            <br />
+            In the Description field, provide details on the token’s utility or purpose.<br />
+            <br />
+            Fill in additional token information, such as your website, X (Twitter) handle, email, and Telegram.<br />
+            <br />
+            FEES shows the suggested amount, adjustable via the slider. Lowering the fee might slow down the stamping process.<br />
+            Fees are displayed in BTC by default, but you can toggle to switch to USDT.<br />
+            All related costs are detailed in the DETAILS section.<br />
+            Accept the terms and conditions to activate the DEPLOY button.<br />
+            <br />
+            The DEPLOY button will submit your transaction with all the provided details.<br />
           </p>
         </section>
 
         <section className="flex flex-col gap-3">
           <h2 className="text-2xl md:text-5xl font-extralight">
-            SUBHEADING
+            Click on DEPLOY BUTTON
           </h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id sem
-            vitae lacus venenatis blandit. Curabitur pharetra ipsum id mauris
-            pulvinar auctor.<br />
+            When clicking on Deploy button you will have to confirm your transaction.<br />
             <br />
-            Ut sit amet diam condimentum, vehicula justo in, vulputate justo.
-            Aenean tincidunt nisl mattis, bibendum velit at, dictum leo. Nam
-            tempus suscipit velit non interdum. Donec ullamcorper, ante ac
-            condimentum suscipit, diam lorem luctus nulla, at dictum diam nunc
-            ornare ante. Ut elementum porta ante, malesuada lacinia dolor rutrum
-            vel.
+            Click on OK.<br />
+            
           </p>
         </section>
 
