723e97d1380a6a452f420721814d21b343bf3566
dev4pear <tommycole519@gmail.com>
2024-11-08 15:43:36 -0500

    feat: sync footer with new design

--- islands/layout/Footer.tsx
+++ islands/layout/Footer.tsx
@@ -4,16 +4,13 @@ interface FooterLink {
   title: string;
   href: string;
   isExternal?: boolean;
+  hiddenOnMobile?: boolean;
 }
 
 const resourcesStampLinks: FooterLink[] = [
   { title: "FAQ", href: "/faq" },
   { title: "HOW-TO", href: "/howto" },
-  {
-    title: "MEDIA",
-    href: "https://github.com/stampchain-io/",
-    isExternal: true,
-  },
+  { title: "MEDIA", href: "/media" },
   {
     title: "PRESS KIT",
     href:
@@ -26,7 +23,7 @@ const stapmChainLinks: FooterLink[] = [
   { title: "ABOUT", href: "/about" },
   { title: "DONATE", href: "#" },
   { title: "CONTACT", href: "#" },
-  { title: "TERMS OF SERVICE", href: "/termsofservice" },
+  { title: "TERMS OF SERVICE", href: "/termsofservice", hiddenOnMobile: true },
 ];
 
 const socialLinks = [
@@ -52,25 +49,53 @@ const socialLinks = [
   },
 ];
 
+const titleClassName =
+  "purple-gradient2 text-[40px] leading-[47px] mobileLg:text-[54px] mobileLg:leading-[64px] tablet:text-[42px] tablet:leading-[50px] desktop:text-[56px] desktop:leading-[66px] italic font-black";
+const subTitleClassName =
+  "text-stamp-purple-bright text-[15px] leading-[17.6px] mobileLg:text-lg mobileLg:leading-[21px] tablet:text-base tablet:leading-[18.77px] desktop:text-xl desktop:leading-[24px] font-light mb-2 mobileLg:mb-3 tablet:mb-2";
+const navTitleClassName =
+  "text-stamp-primary-dark hidden tablet:block text-lg leading-[21.1px] desktop:text-xl desktop:leading-[23.4px] font-black";
+const navContentClassName =
+  "text-stamp-primary-dark text-xs leading-[14px] mobileLg:text-sm mobileLg:leading-[16.4px] desktop:text-base desktop:leading-[18.7px] font-medium hover:text-stamp-purple-bright";
+const copyrightClassName =
+  "text-xs leading-[14px] desktop:text-sm desktop:leading-[16.4px] font-medium text-[#440066]";
+
 export function Footer() {
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
 
   return (
-    <footer className="px-3 tablet:px-6 desktop:px-12 py-6 tablet:py-[72px] text-[#8800CC] font-medium max-w-desktop w-full mx-auto flex flex-col tablet:flex-row justify-between gap-4 text-sm tablet:text-lg">
+    <footer className="
+      px-3 tablet:px-6 desktop:px-12 py-6 tablet:py-[72px]
+      max-w-desktop w-full mx-auto flex flex-col tablet:flex-row justify-between gap-2 mobileLg:gap-3 tablet:gap-4
+    ">
       <div className="w-full flex flex-col gap-1 items-center tablet:items-start">
-        <p className="purple-gradient2 text-[40px] leading-[47px] mobileLg:text-[42px] mobileLg:leading-[50px] desktop:text-[56px] desktop:leading-[66px] italic font-black">
+        <p className={titleClassName}>
           STAMPCHAIN
           <span className="font-extralight pr-1">.IO</span>
         </p>
-        <p className="text-base leading-[19px] desktop:text-xl desktop:leading-[24px] font-light mb-2 tablet:mb-0">
+        <p className={subTitleClassName}>
           IMMORTALISED ART - STORED ON BITCOIN
         </p>
+        <div className="flex flex-row tablet:hidden justify-center w-full gap-[18px] mobileLg:gap-6 leading-4 text-right mb-1 mobileLg:mb-2">
+          {stapmChainLinks.map((link, index) => (
+            <a
+              key={index}
+              href={link.href}
+              f-partial={!link.isExternal ? link.href : undefined}
+              className={navContentClassName +
+                (link?.hiddenOnMobile ? " hidden" : "")}
+              target={link.isExternal ? "_blank" : undefined}
+            >
+              {link.title}
+            </a>
+          ))}
+        </div>
         <div className="flex">
           {socialLinks.map((link, index) => (
             <a key={link.href} href={link.href} target="_blank">
               <img
                 src={hoveredIndex === index ? link.hoverIcon : link.icon}
-                className={`w-11 h-10 ${
+                className={`w-[31px] h-[31px] mobileLg:w-[39px] mobileLg:h-[39px] desktop:w-[46px] desktop:h-[46px] ${
                   index === 0
                     ? "mr-[13px]"
                     : index === 1
@@ -92,14 +117,14 @@ export function Footer() {
 
       <div className="flex flex-col tablet:flex-row justify-end tablet:justify-between w-full">
         <div className="flex flex-col items-center tablet:items-start gap-1 w-full tablet:w-auto">
-          <p className="hidden tablet:block text-lg font-black">RESOURCES</p>
+          <p className={navTitleClassName}>RESOURCES</p>
           <div className="flex flex-row tablet:flex-col justify-center w-full gap-[18px] mobileLg:gap-6 tablet:gap-1 leading-4">
             {resourcesStampLinks.map((link, index) => (
               <a
                 key={index}
                 href={link.href}
                 f-partial={!link.isExternal ? link.href : undefined}
-                className="text-sm leading-[17px] desktop:text-base desktop:leading-[19px] font-medium hover:text-[#AA00FF]"
+                className={navContentClassName}
                 target={link.isExternal ? "_blank" : undefined}
               >
                 {link.title}
@@ -108,37 +133,38 @@ export function Footer() {
             <a
               href="/termsofservice"
               f-partial="/termsofservice"
-              className="block tablet:hidden leading-[19px] hover:text-[#AA00FF]"
+              className={`${navContentClassName} block tablet:hidden`}
             >
               TERMS
             </a>
           </div>
         </div>
 
-        <div className="text-right hidden tablet:flex flex-col gap-1">
-          <p className="text-lg font-black">STAMPCHAIN</p>
+        <div className="hidden tablet:flex flex-col justify-center w-full gap-1 text-right">
+          <p className={navTitleClassName}>STAMPCHAIN</p>
           {stapmChainLinks.map((link, index) => (
             <a
               key={index}
               href={link.href}
               f-partial={!link.isExternal ? link.href : undefined}
-              className="text-sm leading-[17px] desktop:text-base desktop:leading-[19px] font-medium hover:text-[#AA00FF]"
+              className={navContentClassName}
               target={link.isExternal ? "_blank" : undefined}
             >
               {link.title}
             </a>
           ))}
-          <a href="#" className="text-[#440066] mt-5 desktop:mt-2">
-            <span className="italic">STAMPCHAIN</span> @ 2024
-          </a>
+          <p
+            className={`${copyrightClassName} hidden tablet:block mt-5 desktop:mt-2`}
+          >
+            <span className="italic font-bold">STAMPCHAIN</span> @ 2024
+          </p>
         </div>
 
-        <a
-          href="#"
-          className="block tablet:hidden text-[#440066] text-center mt-6"
+        <p
+          className={`${copyrightClassName} block tablet:hidden text-center mt-3 mobileLg:mt-6`}
         >
-          <span className="italic">STAMPCHAIN</span> @ 2024
-        </a>
+          <span className="italic font-bold">STAMPCHAIN</span> @ 2024
+        </p>
       </div>
     </footer>
   );
