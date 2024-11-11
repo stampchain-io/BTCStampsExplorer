3b6acc34537da8295f6bdb2ac62e9bd16f046820
dev4pear <tommycole519@gmail.com>
2024-11-11 12:27:42 -0500

    chore: sync modules islands with new design

--- islands/modules/GetStamping.tsx
+++ islands/modules/GetStamping.tsx
@@ -1,15 +1,15 @@
+import { ModulesStyles } from "$islands/modules/Styles.ts";
+
 export const GetStampingModule = () => {
   return (
-    <div className="flex flex-col tablet:flex-row justify-between tablet:items-end gap-8 tablet:gap-16 desktop:gap-32">
-      <div>
-        <p className="font-black text-4xl tablet:text-7xl flex flex-col gray-gradient4 text-left">
-          GET STAMPING
-          <span className="text-3xl tablet:text-6xl font-extralight text-[#999999]">
-            IMMORTALISE YOUR ART
-          </span>
-        </p>
-
-        <p className="text-[#CCCCCC] text-base tablet:text-2xl font-medium">
+    <div class="
+      grid grid-cols-1 desktop:grid-cols-3 gap-3 mobileLg:gap-6 items-end
+      max-w-desktop w-full mx-auto
+    ">
+      <div className="col-span1 desktop:col-span-2">
+        <h1 className={ModulesStyles.title}>GET STAMPING</h1>
+        <h2 className={ModulesStyles.subTitle}>IMMORTALISE YOUR ART</h2>
+        <p className={ModulesStyles.content}>
           Effortlessly create immutableBitcoin Stamps with custom fee selection
           and optional Posh Stamp Collection naming options. Supports low-fi
           pixel art (png/jpg/gif) and hi-res vector art (svg/html) up to 64kB.
@@ -17,31 +17,33 @@ export const GetStampingModule = () => {
         </p>
       </div>
 
-      <div className="flex flex-col gap-9 min-w-[336px]">
-        <div className="flex tablet:gap-6 gap-1 font-extrabold text-sm tablet:text-xl tablet:justify-center justify-end">
+      <div className="flex flex-col gap-3 mobileLg:gap-6">
+        <div className="flex gap-3 mobileLg:gap-6 font-extrabold justify-end">
           <a
             href="/faq"
             f-partial="/faq"
-            className="border tablet:border-2 border-[#999999] text-[#999999] w-[90px] tablet:w-[136px] h-[40px] tablet:h-[60px] flex justify-center items-center rounded-md"
+            className={ModulesStyles.buttonType1 + " !w-[78px]"}
           >
             FAQ
           </a>
           <a
             href="/stamping/stamp"
             f-partial="/stamping/stamp"
-            className="bg-[#999999] text-black w-[90px] tablet:w-[136px] h-[40px] tablet:h-[60px] flex justify-center items-center rounded-md"
+            className={ModulesStyles.buttonType2}
           >
             STAMP
           </a>
         </div>
-        <div className="flex  tablet:gap-6 gap-1 text-xs tablet:text-lg text-[#666666] tablet:flex-row flex-col">
-          <p className=" text-end tablet:text-start ">
-            $<span className="font-bold">
-              60,935.68
-            </span>
+        <div className="flex gap-3 mobileLg:gap-6 justify-end text-lg text-stamp-grey font-light">
+          <p>
+            <span className="text-stamp-grey-darker">
+              FEE
+            </span>&nbsp;<span className="font-medium">6</span> SAT/vB
           </p>
-          <p className="font-medium  text-end tablet:text-start">
-            Fees: <span className="font-bold">8 sat/vB</span> $0.68
+          <p>
+            <span className="text-stamp-grey-darker">
+              BTC
+            </span>&nbsp;<span className="font-medium">69,420</span> USD
           </p>
         </div>
       </div>
