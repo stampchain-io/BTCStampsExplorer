dc7c1e7bf4ca351032f66f2f6e75799edb6e08c4
dev4pear <tommycole519@gmail.com>
2024-11-11 12:27:42 -0500

    chore: sync modules islands with new design

---
+++ islands/modules/Styles.ts
@@ -0,0 +1,12 @@
+export const ModulesStyles = {
+  title:
+    "font-black text-4xl mobileLg:text-5xl tablet:text-6xl gray-gradient3 mb-1",
+  subTitle:
+    "font-extralight text-2xl mobileLg:text-4xl tablet:text-5xl text-stamp-grey-light mb-3",
+  content:
+    "font-medium text-base mobileLg:text-lg desktop:text-xl text-stamp-grey",
+  buttonType1:
+    "bg-transparent border-2 border-stamp-grey text-stamp-grey w-[96px] h-[48px] flex justify-center items-center rounded-md",
+  buttonType2:
+    "bg-stamp-grey text-black w-[96px] h-[48px] flex justify-center items-center rounded-md",
+} as const;
