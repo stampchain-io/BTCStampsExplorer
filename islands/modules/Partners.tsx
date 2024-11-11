0641a7fca99e27efb0c0f11187d012fd8e85c5ab
dev4pear <tommycole519@gmail.com>
2024-11-11 08:38:47 -0500

    feat: sync partners module with new design

--- islands/modules/Partners.tsx
+++ islands/modules/Partners.tsx
@@ -1,19 +1,74 @@
+import { useState } from "preact/hooks";
+
 interface Partner {
   name: string;
-  imageSrc: string;
+  largeImage: string;
+  largeImageHover: string;
+  smallImage: string;
+  smallImageHover: string;
 }
 
 const partners: Partner[] = [
-  { name: "Bitfinity", imageSrc: "/img/home/partner-bitfinity-banner.svg" },
-  { name: "R8", imageSrc: "/img/home/partner-r8-banner.svg" },
-  { name: "Bitname", imageSrc: "/img/home/partner-bitname-banner.svg" },
+  {
+    name: "Bitfinity",
+    largeImage: "/img/home/partner/bitfinity-large.svg",
+    largeImageHover: "/img/home/partner/bitfinity-large-hover.svg",
+    smallImage: "/img/home/partner/bitfinity-small.svg",
+    smallImageHover: "/img/home/partner/bitfinity-small-hover.svg",
+  },
+  {
+    name: "R8",
+    largeImage: "/img/home/partner/r8-large.svg",
+    largeImageHover: "/img/home/partner/r8-large-hover.svg",
+    smallImage: "/img/home/partner/r8-small.svg",
+    smallImageHover: "/img/home/partner/r8-small-hover.svg",
+  },
+  {
+    name: "Bitname",
+    largeImage: "/img/home/partner/bitname-large.svg",
+    largeImageHover: "/img/home/partner/bitname-large-hover.svg",
+    smallImage: "/img/home/partner/bitname-small.svg",
+    smallImageHover: "/img/home/partner/bitname-small-hover.svg",
+  },
 ];
 
-function PartnerCard({ imageSrc, name }: Partner) {
+function PartnerCard(
+  { name, largeImage, largeImageHover, smallImage, smallImageHover }: Partner,
+) {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div className="relative cursor-pointer">
-      <img src={imageSrc} alt={`${name} banner`} loading="lazy" />
-      <div className="opacity-100 hover:opacity-0 bg-gradient-from-tr bg-gradient-to-bl from-[#CCCCCC00] via-[#9999997F] to-[#666666FF] absolute inset-0 transition-opacity duration-300" />
+    <div className="relative w-full">
+      {/* Add invisible copy of image to set correct height */}
+      <img
+        src={largeImage}
+        alt=""
+        class="hidden mobileLg:block w-full invisible p-[6px]"
+      />
+      <img
+        src={smallImage}
+        alt=""
+        class="block mobileLg:hidden w-full invisible p-[6px]"
+      />
+
+      {/* Overlay elements */}
+      {isHovered && <div className="blur bg-white/50 absolute inset-0" />}
+      <img
+        src={isHovered ? largeImageHover : largeImage}
+        alt={`${name} banner`}
+        loading="lazy"
+        class="hidden mobileLg:block absolute inset-0 cursor-pointer transition-all duration-300 w-full p-[6px]"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      />
+      <img
+        src={isHovered ? smallImageHover : smallImage}
+        alt={`${name} banner`}
+        loading="lazy"
+        class="block mobileLg:hidden absolute inset-0 cursor-pointer transition-all duration-300 w-full p-[6px]"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      />
     </div>
   );
 }
@@ -21,14 +76,13 @@ function PartnerCard({ imageSrc, name }: Partner) {
 export function PartnersModule() {
   return (
     <div class="
-      flex flex-col gap-4
-      px-3 tablet:px-6 desktop:px-12 
+      flex flex-col gap-3 mobileLg:gap-6
       max-w-desktop w-full mx-auto
     ">
-      <h2 class="font-black text-3xl tablet:text-6xl gray-gradient4 mb-3">
+      <h2 class="font-extralight text-2xl mobileLg:text-4xl desktop:text-5xl text-stamp-grey-light">
         PARTNERS
       </h2>
-      <div className="flex justify-between items-center flex-col tablet:flex-row gap-3 tablet:gap-6">
+      <div className="grid grid-cols-3 mobileLg:gap-3">
         {partners.map((partner) => (
           <PartnerCard key={partner.name} {...partner} />
         ))}
