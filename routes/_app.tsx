898c3cc96084f9cbc22cfa841341172d5ea6dabc
eliot1011 <henry.scott0x1011@gmail.com>
2024-11-16 02:52:16 +0900

    update donate

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -92,9 +92,9 @@ export default function App({ Component }: AppProps) {
           content="default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://esm.sh; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' data: https://fonts.gstatic.com; img-src 'self' data: blob: https:; connect-src 'self' https:;"
         />
       </Head>
-      <body class="relative bg-stamp-dark-DEFAULT min-h-screen overflow-x-hidden">
-        <div class="bgGradientTop" aria-hidden="true" />
-        <div class="bgGradientBottom" aria-hidden="true" />
+      <body class="relative bg-stamp-bg-grey-darkest min-h-screen overflow-x-hidden">
+        {/* <div class="bgGradientTop" aria-hidden="true" /> */}
+        {/* <div class="bgGradientBottom" aria-hidden="true" /> */}
 
         <div class="absolute inset-0 bg-gradient-to-b from-transparent via-stamp-dark-DEFAULT/50 to-transparent z-[1]" />
 
