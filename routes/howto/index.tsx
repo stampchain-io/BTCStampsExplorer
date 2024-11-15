a95234f7013bb696b000ed7a7039f0744a05c55e
babalicious-io <142315627+babalicious-io@users.noreply.github.com>
2024-11-15 23:46:41 +0100

    UpdateHow-To - index.tsx
    copied the font definitions from media page
    changed classNames
    testing img width on a couple of sections
    img alt text updated to something generically more SEO friendly
    cleaned out some weird < / b> code
    
    needs a little more tweaking 8buttons), but wanna start with this

--- routes/howto/index.tsx
+++ routes/howto/index.tsx
@@ -1,19 +1,27 @@
 import { Head } from "$fresh/runtime.ts";
 
 export default function HowTo() {
+  const titleClassName =
+    "text-3xl mobileMd:text-4xl mobileLg:text-5xl desktop:text-6xl font-black gray-gradient3";
+  const subTitleClassName =
+    "text-xl mobileMd:text-2xl mobileLg:text-4xl desktop:text-5xl font-extralight text-stamp-grey-light";
+  const defaultTextClassName =
+    "text-sm mobileMd:text-base mobileLg:text-lg desktop:text-xl font-medium text-stamp-grey-light";
+  const buttonClassName =
+    "border-2 border-stamp-grey rounded-md text-base leading-[18.77px] font-extrabold px-6 py-4 text-stamp-grey float-right mobileLg:float-none";
+
   return (
-    <div className="text-[#CCCCCC] text-xl font-medium flex flex-col gap-12 mt-20 tablet:mt-5">
+    <div className="flex flex-col gap-12 mt-20 tablet:mt-5">
       <section className="mb-6">
-        <h1 className="gray-gradient3 text-6xl font-black">HOW-TO</h1>
-        <h2 className="text-2xl tablet:text-5xl font-extralight mb-3">
-          OUR STEP-BY-STEP GUIDES
-        </h2>
-        <p className="mb-12 text-xl">
-          Explore our comprehensive How-To section, where you'll find
+        <h1 className={titleClassName}>HOW-TO</h1>
+        <h2 className={subTitleClassName}>OUR STEP-BY-STEP GUIDES</h2>
+        <p className={defaultTextClassName}>
+          <b>Explore our comprehensive How-To section, where you'll find
           step-by-step guides for the most popular features on our
-          platform.<br />
+          platform.</b><br />
           Whether you're a beginner or a pro, these guides will help you make
           the most out of every tool we offer. <br />
+          <br />
           Need help with something that's not covered? Let us know! <br />
           Reach out to us, and we'll be happy to create new how-tos based on
           your suggestions.
@@ -21,22 +29,21 @@ export default function HowTo() {
       </section>
 
       <section>
-        <h2 className="text-2xl tablet:text-5xl font-extralight mb-3">
-          DEPLOY YOUR OWN TOKEN
-        </h2>
-        <div className="grid grid-cols-1 tablet:grid-cols-3 gap-3">
+        <h2 className={subTitleClassName}>DEPLOY YOUR OWN TOKEN</h2>
+        <div className="grid grid-cols-1 mobileLg:grid-cols-2 desktop:grid-cols-3 gap-3">
           <img
             src="/img/how-tos/deploy/00.png"
-            width="432px"
-            alt="Screenshot"
+            width="100%"
+            alt="Deploy a SRC-20 token on Bitcoin"
           />
-          <div className="md:col-span-2 flex flex-col gap-3">
-            <p>
-              To deploy an SRC-20 token, stamp the transaction on Bitcoin with
+          <div className="desktop:col-span-2 flex flex-col gap-3">
+            <p className={defaultTextClassName}>
+              To deploy a SRC-20 token, stamp the transaction on Bitcoin with
               the token's supply and metadata. This makes the token immutable
               and secured by Bitcoin's blockchain.<br />
-              In this guide, you'll learn how to deploy your very own SRC-20
-              token!
+              <br />
+              <b>In this guide, you'll learn how to deploy your very own SRC-20
+              token!</b>
             </p>
             <a
               href="/howto/deploytoken"
@@ -48,17 +55,16 @@ export default function HowTo() {
           </div>
         </div>
       </section>
-      <b />
+      
       <section>
-        <h2 className="text-2xl tablet:text-5xl font-extralight mb-3">
-          MINT YOUR TOKEN
-        </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
-          <div className="md:col-span-2 flex flex-col gap-3">
-            <p>
+        <h2 className={subTitleClassName}>MINT YOUR TOKEN</h2>
+        <div className="grid grid-cols-1 mobileLg:grid-cols-2 desktop:grid-cols-3 gap-3">
+         <div className="desktop:col-span-2 flex flex-col gap-3">
+            <p className={defaultTextClassName}>
               After deployment, token holders can mint SRC-20 tokens based on
               the initial supply set in the contract.<br />
-              In this guide, you'll learn how to mint a SRC-20 token!
+              <br />
+              <b>In this guide, you'll learn how to mint a SRC-20 token!</b>
             </p>
             <a
               href="/howto/mint"
@@ -71,24 +77,24 @@ export default function HowTo() {
           <img
             src="/img/how-tos/mintsrc20/00.png"
             width="432px"
-            alt="Screenshot"
+            alt="How to mint a SRC-20 Bitcoin stamps token"
           />
         </div>
       </section>
-      <b />
+
       <section>
-        <h2 className="text-2xl md:text-5xl font-extralight mb-3">
-          STAMPING ART
-        </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
+        <h2 className={subTitleClassName}>STAMPING ART</h2>
+        <div className="grid grid-cols-1 mobileLg:grid-cols-2 desktop:grid-cols-3 gap-3">
           <img
             src="/img/how-tos/stamping/00.png"
             width="432px"
-            alt="Screenshot"
+            alt="Guide on how to create NFTs on Bitcoin using the stamps protocol"
           />
-          <div className="md:col-span-2 flex flex-col gap-3">
-            <p>
-              Art is Art.<br />In this guide, you'll learn how to stamp art!
+         <div className="desktop:col-span-2 flex flex-col gap-3">
+            <p className={defaultTextClassName}>
+              Art is Art.<br />
+              <br />
+              <b>In this guide, you'll learn how to stamp art!</b>
             </p>
             <a
               href="/howto/getstamping"
@@ -100,17 +106,16 @@ export default function HowTo() {
           </div>
         </div>
       </section>
-      <b />
+                
       <section>
-        <h2 className="text-2xl md:text-5xl font-extralight mb-3">
-          TRANSFER FUNCTIONALITY
-        </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
-          <div className="md:col-span-2 flex flex-col gap-3">
-            <p>
-              Send an SOS{" "}
-              <b />In this guide, you'll learn how to transfer SRC-20 tokens and
-              stamp art!
+        <h2 className={subTitleClassName}>TRANSFER FUNCTIONALITY</h2>
+        <div className="grid grid-cols-1 mobileLg:grid-cols-2 desktop:grid-cols-3 gap-3">
+         <div className="desktop:col-span-2 flex flex-col gap-3">
+            <p className={defaultTextClassName}>
+              Send an SOS{" "}<br />
+              <br />
+              <b>In this guide, you'll learn how to transfer SRC-20 tokens and
+              stamp art!</b>
             </p>
             <a
               href="/howto/transfer"
@@ -123,25 +128,24 @@ export default function HowTo() {
           <img
             src="/img/how-tos/transfer/00.png"
             width="432px"
-            alt="Screenshot"
+            alt="Transfer SRC-20 tokens on the Bitcoin blockchain"
           />
         </div>
       </section>
-      <b />
+     
       <section>
-        <h2 className="text-2xl md:text-5xl font-extralight mb-3">
-          CREATE A LEATHER WALLET
-        </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
+        <h2 className={subTitleClassName}>CREATE A LEATHER WALLET</h2>
+        <div className="grid grid-cols-1 mobileLg:grid-cols-2 desktop:grid-cols-3 gap-3">
           <img
             src="/img/how-tos/createleatherwallet/00.png"
             width="432px"
-            alt="Screenshot"
+            alt="Create, setup and install Bitcoin Leather wallet"
           />
-          <div className="md:col-span-2 flex flex-col gap-3">
-            <p>
+         <div className="desktop:col-span-2 flex flex-col gap-3">
+            <p className={defaultTextClassName}>
               New to Bitcoin, Stamps, wallet and others? No worries! <br />
-              In this guide, you'll learn how to create a Leather wallet!
+              <br />
+              <b>In this guide, you'll learn how to create a Leather wallet!</b>
             </p>
             <a
               href="/howto/leathercreate"
@@ -153,16 +157,14 @@ export default function HowTo() {
           </div>
         </div>
       </section>
-      <b />
+      
       <section>
-        <h2 className="text-2xl md:text-5xl font-extralight mb-3">
-          CONNECT YOUR LEATHER WALLET
-        </h2>
-        <div className="grid grid-cols-1 tablet:grid-cols-3 gap-3">
-          <div className="md:col-span-2 flex flex-col gap-3">
-            <p>
-              In this guide, you'll learn how connect your Leather wallet to
-              stampchain.io
+        <h2 className={subTitleClassName}>CONNECT YOUR LEATHER WALLET</h2>
+        <div className="grid grid-cols-1 mobileLg:grid-cols-2 desktop:grid-cols-3 gap-3">
+         <div className="desktop:col-span-2 flex flex-col gap-3">
+            <p className={defaultTextClassName}>
+              <b>In this guide, you'll learn how connect your Leather wallet to
+              stampchain.io</b>
             </p>
             <a
               href="/howto/leatherconnect"
@@ -174,8 +176,8 @@ export default function HowTo() {
           </div>
           <img
             src="/img/how-tos/connectleatherwallet/00.png"
-            width="432px"
-            alt="Screenshot"
+            width="100%"
+            alt="Connect your Bitcoin Leather wallet to the Stampchain website"
           />
         </div>
       </section>
