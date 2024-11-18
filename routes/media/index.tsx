467499bed34e216bd7109b41b69cc84746866b27
babalicious-io <baba@babalicious.io>
2024-11-18 13:42:16 +0100

    updated classNames

--- routes/media/index.tsx
+++ routes/media/index.tsx
@@ -1,22 +1,22 @@
 export default function Media() {
   const bodyClassName = "flex flex-col gap-12 mobileLg:gap-24 desktop:gap-36";
-  const titleClassName =
+  const titleGreyDLClassName =
     "text-3xl mobileMd:text-4xl mobileLg:text-5xl desktop:text-6xl font-black gray-gradient3";
-  const subTitleClassName =
+  const subTitleGreyClassName =
     "text-2xl mobileMd:text-3xl mobileLg:text-4xl desktop:text-5xl font-extralight text-stamp-grey-light mb-1.5 mobileLg:mb-3";
   const articleLinkClassName =
     "text-lg mobileMd:text-xl mobileLg:text-3xl desktop:text-4xl font-bold relative bg-clip-text text-transparent bg-gradient-to-r from-[#cccccc] to-[#666666] hover:bg-none hover:text-[#cccccc] transition-colors";
-  const bodyTextClassName =
+  const bodyTextLightClassName =
     "text-base mobileLg:text-lg font-medium text-stamp-grey-light";
-  const buttonClassName =
+  const buttonGreyOutlineClassName =
     "inline-flex items-center justify-center border-2 border-stamp-grey rounded-md text-sm mobileLg:text-base font-extrabold text-stamp-grey tracking-[0.05em] leading-[42px] mobileLg:leading-[48px] px-5 hover:border-stamp-grey-light hover:text-stamp-grey-light transition-colors";
 
   return (
     <div className={bodyClassName}>
       <section>
-        <h1 className={titleClassName}>MEDIA MATTERS</h1>
-        <h2 className={subTitleClassName}>HONOURABLE STAMP MENTIONS</h2>
-        <p className={bodyTextClassName}>
+        <h1 className={titleGreyDLClassName}>MEDIA MATTERS</h1>
+        <h2 className={subTitleGreyClassName}>HONOURABLE STAMP MENTIONS</h2>
+        <p className={bodyTextLightClassName}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
           vulputate, lacus at faucibus fringilla, urna urna pretium magna, et
           porttitor odio massa sit amet arcu. Curabitur dolor elit, ornare at
@@ -27,10 +27,10 @@ export default function Media() {
       </section>
 
       <section>
-        <h1 className={titleClassName}>INTERVIEWS</h1>
+        <h1 className={titleGreyDLClassName}>INTERVIEWS</h1>
         <div className="grid grid-cols-1 mobileLg:grid-cols-2 gap-6 desktop:gap-12">
           <div className="flex flex-col">
-            <h2 className={subTitleClassName}>RICE TVX</h2>
+            <h2 className={subTitleGreyClassName}>RICE TVX</h2>
             <div className="relative w-full pt-[56.25%]">
               {/* 16:9 aspect ratio wrapper */}
               <iframe
@@ -41,7 +41,7 @@ export default function Media() {
                 allowFullScreen
               />
             </div>
-            <p className={bodyTextClassName}>
+            <p className={bodyTextLightClassName}>
               On this episode, I am joined by Mike In Space! He is the creator
               of Bitcoin Stamps & the SRC-20 protocol.<br />
               <br />
@@ -54,7 +54,7 @@ export default function Media() {
             </p>
           </div>
           <div className="flex flex-col">
-            <h2 className={subTitleClassName}>WAGE CUCKING</h2>
+            <h2 className={subTitleGreyClassName}>WAGE CUCKING</h2>
             <div className="relative w-full pt-[56.25%]">
               {/* 16:9 aspect ratio wrapper */}
               <iframe
@@ -65,7 +65,7 @@ export default function Media() {
                 allowFullScreen
               />
             </div>
-            <p className={bodyTextClassName}>
+            <p className={bodyTextLightClassName}>
               This week Jmo and Andreas were joined by the creator of Bitcoin
               STAMPS, Mike In Space!<br />
               <br />
@@ -79,8 +79,8 @@ export default function Media() {
       </section>
 
       <section>
-        <h1 className={titleClassName}>IN THE NEWS</h1>
-        <h2 className={subTitleClassName}>BREAKING</h2>
+        <h1 className={titleGreyDLClassName}>IN THE NEWS</h1>
+        <h2 className={subTitleGreyClassName}>BREAKING</h2>
         <a
           href="https://finance.yahoo.com/video/project-bitcoin-stamps-renews-debate-164902188.html?guccounter=1"
           target="_blank"
@@ -91,8 +91,8 @@ export default function Media() {
       </section>
 
       <section>
-        <h1 className={titleClassName}>ARTICLES</h1>
-        <h2 className={subTitleClassName}>STAMP STORIES</h2>
+        <h1 className={titleGreyDLClassName}>ARTICLES</h1>
+        <h2 className={subTitleGreyClassName}>STAMP STORIES</h2>
         <a
           href="https://hackernoon.com/what-are-bitcoin-stamps"
           target="_blank"
@@ -103,11 +103,11 @@ export default function Media() {
       </section>
 
       <section>
-        <h1 className={titleClassName}>REPORTS</h1>
+        <h1 className={titleGreyDLClassName}>REPORTS</h1>
         <div className="grid grid-cols-1 mobileLg:grid-cols-3 gap-3 mobileMd:gap-6 desktop:gap-12">
           <div className="col-span-1 mobileLg:col-span-2">
-            <h2 className={subTitleClassName}>SQRR - DEEP DIVE</h2>
-            <p className={bodyTextClassName}>
+            <h2 className={subTitleGreyClassName}>SQRR - DEEP DIVE</h2>
+            <p className={bodyTextLightClassName}>
               Stamps is a blockchain protocol created by MikeInSpace that
               enables storing images on-chain on Bitcoin transaction
               outputs.<br />
@@ -122,13 +122,13 @@ export default function Media() {
                 href="https://sqrr.xyz/reports/docs/4/1/Stamps%20Protocol_Final_17_May_2023.pdf"
                 target="_blank"
               >
-                <button className={buttonClassName}>DOWNLOAD</button>
+                <button className={buttonGreyOutlineClassName}>DOWNLOAD</button>
               </a>
             </div>
           </div>
           <div className="col-span-1 mobileLg:text-right">
-            <h2 className={subTitleClassName}>INSIGHTS</h2>
-            <p className={bodyTextClassName}>
+            <h2 className={subTitleGreyClassName}>INSIGHTS</h2>
+            <p className={bodyTextLightClassName}>
               <b>Detailed monthly reports about Bitcoin Stamps.</b>
               <br />
               A comprehensive research into the usage of the Stamps protocol,
@@ -137,7 +137,7 @@ export default function Media() {
             </p>
             <div className="flex justify-end">
               <a href="https://sqrr.xyz/reports/" target="_blank">
-                <button className={buttonClassName}>READ</button>
+                <button className={buttonGreyOutlineClassName}>READ</button>
               </a>
             </div>
           </div>
