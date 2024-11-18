78f2dc604aeba2df1961c3bc46bd28ab243d292c
reinamora <reinamora@stampchain.io>
2024-11-18 20:25:56 +0000

    chore: cleanup dev mode restarts

--- dev.ts
+++ dev.ts
@@ -1,16 +1,15 @@
 #!/usr/bin/env -S deno run -A --watch=static/,routes/
+// Set SKIP_REDIS_CONNECTION before ANY imports
+(globalThis as any).SKIP_REDIS_CONNECTION = true;
+
 import "preact/debug";
 import dev from "$fresh/dev.ts";
+import build from "$fresh/dev.ts";
 import config from "$/fresh.config.ts";
 
-const isBuildMode = Deno.args.includes("build");
-
-if (isBuildMode) {
-  globalThis.SKIP_REDIS_CONNECTION = true;
-}
-
-await dev(import.meta.url, "./main.ts", config);
-
-if (isBuildMode) {
+if (Deno.args.includes("build")) {
+  await build(import.meta.url, "./main.ts", config);
   Deno.exit(0);
+} else {
+  await dev(import.meta.url, "./main.ts", config);
 }
