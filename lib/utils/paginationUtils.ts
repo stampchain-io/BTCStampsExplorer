008a4e659f08a7d910e3ff0e5d0d07776a84a502
dev4pear <tommycole519@gmail.com>
2024-11-19 12:54:27 -0500

    feat: make stamp & src20 pagination independent on wallet page

--- lib/utils/paginationUtils.ts
+++ lib/utils/paginationUtils.ts
@@ -1,16 +1,21 @@
 import { PaginationQueryParams } from "globals";
 
-export function getPaginationParams(url: URL): PaginationQueryParams {
+export function getPaginationParams(
+  url: URL,
+  type?: string,
+): PaginationQueryParams {
   let defaultLimit = 50;
 
   if (url.pathname.includes("/wallet/")) {
     defaultLimit = 32;
+    if (type == "src20") defaultLimit = 8;
   } else {
     defaultLimit = 500;
   }
 
-  const limit = Number(url.searchParams.get("limit")) || defaultLimit;
-  const page = Number(url.searchParams.get("page")) || 1;
+  const prefix = type ? `${type}_` : "";
+  const limit = Number(url.searchParams.get(`${prefix}limit`)) || defaultLimit;
+  const page = Number(url.searchParams.get(`${prefix}page`)) || 1;
   return { limit, page };
 }
 
