a218eee506d31feedbe20609b6ac31b23432a18d
reinamora <reinamora@stampchain.io>
2024-11-19 23:21:51 +0000

    chore: debug launch and tasks more tweaks - annoying

--- local.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# deno run -A main.ts build
-
-# Comment out DB_HOST in .env if not already without chagnging the rest of the line
-# sed -i 's/^#*DB_HOST/#DB_HOST/' .env
-
-# remove comment on the DB_HOST line
-sed -i 's/^#DB_HOST/DB_HOST/' .env
-
