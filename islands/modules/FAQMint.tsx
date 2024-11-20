4a0f45997e4fcc7f25668b320a49e7aa3e1e47c4
babalicious-io <baba@babalicious.io>
2024-11-20 23:03:07 +0100

    import styles/className update/text tweaking/button url update

--- islands/modules/FAQMint.tsx
+++ islands/modules/FAQMint.tsx
@@ -9,16 +9,22 @@ export const FAQModule = () => {
         <p className={ModulesStyles.bodyTextLight}>
           <ul className="list-disc pl-5 space-y-2">
             <li>
-              Start typing a <b>TOKEN</b>{"  "}
-              name to filter and select the correct ticker.
+              Start typing a <b>token name</b>{" "}
+              to filter and select the correct ticker.
             </li>
             <li>
-              The ticker logo and name will be shown along with the maximum
-              limit per mint.
+              The ticker logo image and name will be shown along with the
+              maximum limit per mint.
             </li>
             <li>
               Double-check that the displayed information matches the ticker you
               wish to mint.
+            </li>
+            <li>
+              If the <b>minting progress</b>{" "}
+              is near 95%, be cautious as you risk losing your funds. <br />
+              When a token is minted out, and progress has reached 100%, you
+              will no longer be able to mint it.
             </li>
             <li>
               <b>Fee</b>{" "}
@@ -26,33 +32,31 @@ export const FAQModule = () => {
               slider.{" "}
             </li>
             <li>
+              An <b>estimate</b>{" "}
+              is displayed based on the current fee and transaction size.
+            </li>
+            <li>
               Accept the <b>terms and conditions</b> to enable the mint button.
             </li>
             <li>
-              The <b>mint</b>{"  "}
-              button will submit your transaction with all the provided details.
+              The <b>mint button</b>{" "}
+              will submit your transaction with all the provided details.
             </li>
           </ul>
           <br />
-          If the minting <b>progress</b>{"  "}
-          is near 95%, be cautious as you risk losing your funds. <br />
-          When a token is minted out, and progress has reached 100%, you will no
-          longer be able to mint it.<br />
           Reducing the fee may slow down the minting process.<br />
           Fees are shown in BTC by default, but you can switch to USDT using the
           toggle.<br />
-          <br />
-
           All related costs are listed in the <b>details</b> section.<br />
         </p>
       </div>
       <div className="flex justify-end tablet:justify-start">
         <a
-          href="/faq"
-          f-partial="/faq"
+          href="/howto/mint"
+          f-partial="/howto/mint"
           className={ModulesStyles.buttonGreyOutline}
         >
-          READ ALL
+          READ MORE
         </a>
       </div>
     </div>
--- islands/modules/FAQStamping.tsx
+++ islands/modules/FAQStamping.tsx
@@ -1,56 +1,68 @@
+import { ModulesStyles } from "$islands/modules/Styles.ts";
+
 export const FAQModule = () => {
   return (
-    <div className="flex flex-col gap-4">
-      <p className="font-black text-3xl tablet:text-6xl flex flex-col gray-gradient3 text-left">
-        HOW-TO STAMP ART
-      </p>
-      <p className="flex flex-col gray-gradient4 text-left text-2xl tablet:text-5xl font-extralight text-[#CCCCCC]">
-        BASIC STEPS
-      </p>
-
-      <p className="text-[#999999] text-base tablet:text-lg font-medium">
-        <ul className="list-decimal pl-5 space-y-2">
-          <li>
-            Click the <b>icon</b> to upload your artwork in a supported format.
-          </li>
-          <li>
-            Use the <b>TOGGLE</b> to switch between <b>CLASSIC</b> and{" "}
-            <b>POSH</b> Art, and add a <b>STAMP NAME</b>.
-          </li>
-          <li>
-            <b>EDITIONS</b> sets the number of copies you want to create.
-          </li>
-          <li>
-            The <b>LOCK</b>{" "}
-            icon is enabled by default, preventing future changes to the
-            EDITIONS.
-          </li>
-          <li>
-            <b>FEES</b>{" "}
-            shows the suggested amount, and you can adjust it with the slider.
-          </li>
-          <li>
-            Accept the <b>terms and conditions</b> to enable the STAMP button.
-          </li>
-          <li>
-            The <b>STAMP</b>{" "}
-            button will submit your transaction with all the provided details.
-          </li>
-        </ul>
-        <br />
-        All related costs are listed under the <b>DETAILS</b> section. <br />
-        Lowering the fee may delay your art being stamped.<br />
-        Fees are displayed in BTC by default, but you can switch to USDT using
-        the <b>TOGGLE</b>.<br />
-      </p>
+    <div className="flex flex-col gap-3 mobileMd:gap-6">
+      <div className="flex flex-col">
+        <h1 className={ModulesStyles.titleGreyDL}>HOW-TO</h1>
+        <h2 className={ModulesStyles.subTitleGrey}>STAMP ART</h2>
+        <p className={ModulesStyles.bodyTextLight}>
+          <ul className="list-disc pl-5 space-y-2">
+            <li>
+              Click the <b>image icon</b>{" "}
+              to upload your artwork in a supported format.
+            </li>
+            <li>
+              <b>Editions</b> sets the amount of copies you want to create.
+            </li>
+            <li>
+              Use the <b>toggle switch</b> to access advanced options.
+            </li>
+            <li>
+              In the advanced options you can choose a custom name for your art.
+              Select between a <b>custom CPID</b> or <b>posh name</b>{" "}
+              with the toggle.
+            </li>
+            <li>
+              The <b>lock button</b>{" "}
+              is enabled by default, preventing future changes to the amount of
+              editions.
+            </li>
+            <li>
+              You can preview your art by clicking the <b>fullscreen button</b>.
+            </li>
+            <li>
+              <b>Fee</b>{" "}
+              displays the suggested amount, and you can adjust it with the
+              slider.
+            </li>
+            <li>
+              A fee <b>estimate</b>{" "}
+              is displayed based on the current fee and transaction size.
+            </li>
+            <li>
+              Accept the <b>terms and conditions</b> to enable the stamp button.
+            </li>
+            <li>
+              The <b>stamp button</b>{" "}
+              will submit your transaction with all the provided details.
+            </li>
+          </ul>
+          <br />
+          All related costs are listed under the <b>details</b> section. <br />
+          Lowering the fee may delay your art being stamped.<br />
+          Fees are displayed in BTC by default, but you can switch to USDT using
+          the <b>TOGGLE</b>.<br />
+        </p>
+      </div>
 
       <div className="flex justify-end tablet:justify-start">
         <a
-          href="/faq"
-          f-partial="/faq"
-          className="uppercase border tablet:border-2 border-[#999999] rounded-md bg-transparent text-[#999999] font-extrabold w-[63px] tablet:w-[84px] h-[36px] tablet:h-[48px] flex justify-center items-center"
+          href="/howto/getstamping"
+          f-partial="/howto/getstamping"
+          className={ModulesStyles.buttonGreyOutline}
         >
-          Read All
+          READ MORE
         </a>
       </div>
     </div>
