c5fa6c1572c41582ddd04bf5e769e5879f911eb5
dev4pear <tommycole519@gmail.com>
2024-11-20 06:22:42 -0500

    feat: validate styling and breakpoints on wallet page

--- islands/Wallet/details/WalletContent.tsx
+++ islands/Wallet/details/WalletContent.tsx
@@ -94,7 +94,7 @@ const ItemHeader = (
   return (
     <div class="flex flex-row justify-between items-center gap-3 w-full">
       <div class="flex gap-6 tablet:gap-8 items-end">
-        <p class="cursor-pointer pb-1 tablet:pb-3 text-2xl tablet:text-4xl desktop:text-5xl uppercase text-[#8800CC] font-light">
+        <p class="cursor-pointer pb-1 tablet:pb-3 text-2xl tablet:text-4xl desktop:text-5xl uppercase text-stamp-primary font-extralight">
           {title}
         </p>
       </div>
@@ -147,72 +147,72 @@ function DispenserItem() {
         {dispensers.map((dispenser) => {
           // Ensure src20.tick is defined
           return (
-            <div class="bg-gradient-to-br from-transparent from-0% via-[#14001F] to-[#1F002E] text-sm flex justify-between rounded-md hover:border-[#9900EE] hover:shadow-[0px_0px_20px_#9900EE]">
+            <div class="bg-gradient-to-br from-transparent from-0% via-[#14001F] to-[#1F002E] text-sm flex justify-between rounded-md hover:border-stamp-primary-light hover:shadow-[0px_0px_20px_#9900EE]">
               <div class="p-3 uppercase cursor-pointer flex gap-6 w-full">
                 <img
                   src={`/content/${dispenser.tx_hash}.svg`}
                   class="w-[134px] h-[134px]"
                 />
-                <div className="flex flex-col  w-full">
-                  <div className="flex flex-col justify-between  w-full">
+                <div class="flex flex-col  w-full">
+                  <div class="flex flex-col justify-between  w-full">
                     <a
                       href="#"
-                      className="text-3xl text-[#666666] font-bold hover:text-[#AA00FF] uppercase flex gap-4"
+                      class="text-3xl text-[#666666] font-bold hover:text-stamp-primary-hover uppercase flex gap-4"
                     >
                       {`#${dispenser.block_index}`}
                     </a>
                   </div>
 
-                  <div className="flex justify-between flex-row  w-full">
-                    <p className="text-base text-[#8800CC] font-light text-ellipsis overflow-hidden">
-                      <span className="font-bold text-[#8800CC] text-base">
+                  <div class="flex justify-between flex-row  w-full">
+                    <p class="text-base text-stamp-primary font-light text-ellipsis overflow-hidden">
+                      <span class="font-bold text-stamp-primary text-base">
                         {dispenser.origin
                           ? dispenser.origin
                           : abbreviateAddress(dispenser.origin)}
                       </span>
                     </p>
-                    <div className="flex flex-row gap-1">
+                    <div class="flex flex-row gap-1">
                       <img
                         src="/img/wallet/icon-copy.svg"
-                        className="w-6 h-6 cursor-pointer"
+                        class="w-6 h-6 cursor-pointer"
                         alt="Copy"
                       />
                       <img
                         src="/img/wallet/icon-history.svg"
-                        className="w-6 h-6 cursor-pointer"
+                        class="w-6 h-6 cursor-pointer"
                         alt="History"
                       />
                     </div>
                   </div>
                   <div class="text-center flex justify-between">
-                    <p className="text-lg text-[#666666] font-light">
+                    <p class="text-base mobileLg:text-lg text-stamp-grey-darker font-light">
                       GIVE{" "}
-                      <span className="font-bold text-[#999999]">
+                      <span class="font-bold text-stamp-grey">
                         {Number(dispenser.give_quantity).toLocaleString()}
                       </span>
                     </p>
                   </div>
-                  <div className="flex flex-row justify-between w-full">
-                    <p className="text-lg text-[#666666] font-light">
+                  <div class="flex flex-row justify-between w-full">
+                    <p class="text-base mobileLg:text-lg text-stamp-grey-darker font-light">
                       QUANTITY{" "}
-                      <span className="font-bold text-[#999999]">
+                      <span class="font-bold text-stamp-grey">
                         {dispenser.give_remaining}/{dispenser.escrow_quantity}
                       </span>
                     </p>
-                    <p className="text-[#666666] text-lg">
+                    <p class="text-stamp-grey-darker text-lg">
                       VALUE
                     </p>
                   </div>
-                  <div className="flex flex-row justify-between  w-full">
-                    <p className="text-base text-[#666666] font-light">
+                  <div class="flex flex-row justify-between  w-full">
+                    <p class="text-base mobileLg:text-lg text-stamp-grey-darker font-light">
                       PRICE{" "}
-                      <span className="font-bold text-[#999999]">
+                      <span class="font-bold text-stamp-grey">
                         {`${Number(dispenser.btcrate).toLocaleString()} BTC`}
                       </span>
                     </p>
-                    <p className="text-base text-[#666666] font-light">
+                    <p class="text-base text-stamp-grey-darker font-light">
                       {Number(dispenser.btcrate).toLocaleString()}{" "}
-                      <span className="font-bold text-[#999999]">
+                      <span class="font-bold text-stamp-grey">
                         BTC
                       </span>
                     </p>
@@ -229,72 +229,72 @@ function DispenserItem() {
         {dispensers.map((dispenser) => {
           // Ensure src20.tick is defined
           return (
-            <div class="bg-gradient-to-br from-transparent from-0% via-[#14001F] to-[#1F002E] text-sm flex justify-between rounded-md hover:border-[#9900EE] hover:shadow-[0px_0px_20px_#9900EE]">
+            <div class="bg-gradient-to-br from-transparent from-0% via-[#14001F] to-[#1F002E] text-sm flex justify-between rounded-md hover:border-stamp-primary-light hover:shadow-[0px_0px_20px_#9900EE]">
               <div class="p-3 uppercase cursor-pointer flex gap-6 w-full">
                 <img
                   src={`/content/${dispenser.tx_hash}.svg`}
                   class="w-[117px] h-[117px]"
                 />
-                <div className="flex flex-col  w-full">
-                  <div className="flex flex-col justify-between  w-full">
+                <div class="flex flex-col  w-full">
+                  <div class="flex flex-col justify-between  w-full">
                     <a
                       href="#"
-                      className="text-3xl text-[#666666] font-bold hover:text-[#AA00FF] uppercase flex gap-4"
+                      class="text-3xl text-stamp-grey-darker font-bold hover:text-stamp-primary-hover uppercase flex gap-4"
                     >
                       {`#${dispenser.block_index}`}
                     </a>
                   </div>
 
-                  <div className="flex justify-between flex-row  w-full">
-                    <p className="text-base text-[#8800CC] font-light text-ellipsis overflow-hidden tablet:w-full max-w-48">
-                      <span className="font-bold text-[#8800CC] text-base w-full">
+                  <div class="flex justify-between flex-row  w-full">
+                    <p class="text-base text-stamp-primary font-light text-ellipsis overflow-hidden tablet:w-full max-w-48">
+                      <span class="font-bold text-stamp-primary text-base w-full">
                         {dispenser.origin
                           ? dispenser.origin
                           : abbreviateAddress(dispenser.origin)}
                       </span>
                     </p>
-                    <div className="flex flex-row gap-1">
+                    <div class="flex flex-row gap-1">
                       <img
                         src="/img/wallet/icon-copy.svg"
-                        className="w-6 h-6 cursor-pointer"
+                        class="w-6 h-6 cursor-pointer"
                         alt="Copy"
                       />
                       <img
                         src="/img/wallet/icon-history.svg"
-                        className="w-6 h-6 cursor-pointer"
+                        class="w-6 h-6 cursor-pointer"
                         alt="History"
                       />
                     </div>
                   </div>
                   <div class="flex justify-between">
-                    <p className="text-lg text-[#666666] font-light">
+                    <p class="text-lg text-stamp-grey-darker font-light">
                       GIVE{" "}
-                      <span className="font-bold text-[#999999]">
+                      <span class="font-bold text-stamp-grey">
                         {Number(dispenser.give_quantity).toLocaleString()}
                       </span>
                     </p>
                   </div>
-                  <div className="flex flex-row justify-between w-full">
-                    <p className="text-lg text-[#666666] font-light">
+                  <div class="flex flex-row justify-between w-full">
+                    <p class="text-lg text-stamp-grey-darker font-light">
                       QUANTITY{" "}
-                      <span className="font-bold text-[#999999]">
+                      <span class="font-bold text-stamp-grey">
                         XXXX/XXXX
                       </span>
                     </p>
-                    <p className="text-[#666666] text-lg">
+                    <p class="hidden mobileLg:block text-stamp-grey-darker text-lg">
                       VALUE
                     </p>
                   </div>
-                  <div className="flex flex-row justify-between  w-full">
-                    <p className="text-base text-[#666666] font-light">
+                  <div class="flex flex-row justify-between  w-full">
+                    <p class="text-base text-stamp-grey-darker font-light">
                       PRICE{" "}
-                      <span className="font-bold text-[#999999]">
+                      <span class="font-bold text-stamp-grey">
                         {`${Number(dispenser.btcrate).toLocaleString()} BTC`}
                       </span>
                     </p>
-                    <p className="text-base text-[#666666] font-light">
+                    <p class="hidden mobileLg:block text-base text-stamp-grey-darker font-light">
                       {Number(dispenser.btcrate).toLocaleString()}{" "}
-                      <span className="font-bold text-[#999999]">
+                      <span class="font-bold text-stamp-grey">
                         BTC
                       </span>
                     </p>
@@ -377,13 +377,18 @@ function WalletContent(
       mobileLg:gap-[24px]
       tablet:gap-[24px]
       desktop:gap-[12px]
-      grid-cols-2
-      mobileSm:grid-cols-4
+      grid-cols-4
       mobileLg:grid-cols-6
       tablet:grid-cols-6
       desktop:grid-cols-8
       auto-rows-fr
     `,
+    displayCounts: {
+      "mobileSm": 16, // 4 columns x 4 rows
+      "mobileLg": 24, // 6 columns x 4 rows
+      "tablet": 24, // 6 columns x 4 rows
+      "desktop": 32, // 8 columns x 4 rows
+    },
     pagination: {
       page: stamps.pagination.page,
       pageSize: stamps.pagination.limit,
@@ -396,7 +401,7 @@ function WalletContent(
     <>
       <div>
         <ItemHeader
-          title="STAMP"
+          title="STAMPS"
           sortBy={sortBy}
           isOpen={openS}
           handleOpen={handleOpen}
@@ -409,10 +414,12 @@ function WalletContent(
           handleOpenFilter={() => {}}
           handleOpenSetting={handleOpenSetting}
         />
-        <StampSection {...stampSection} />
+        <div class="mt-3 mobileLg:mt-6">
+          <StampSection {...stampSection} />
+        </div>
       </div>
 
-      <div className="mt-48">
+      <div class="mt-48">
         <ItemHeader
           title="TOKENS"
           sortBy={sortBy}
@@ -427,7 +434,7 @@ function WalletContent(
           handleOpenFilter={() => {}}
           handleOpenSetting={() => {}}
         />
-        <div className="mt-6">
+        <div class="mt-3 mobileLg:mt-6">
           <SRC20DeployTable
             data={src20.data}
           />
@@ -447,7 +454,7 @@ function WalletContent(
         />
       )}
 
-      <div className="mt-48">
+      <div class="mt-48">
         <ItemHeader
           title="LISTINGS"
           sortBy={sortBy}
@@ -462,7 +469,7 @@ function WalletContent(
           handleOpenFilter={handleOpenFilter}
           handleOpenSetting={() => {}}
         />
-        <div className="mt-6">
+        <div class="mt-3 mobileLg:mt-6">
           <DispenserItem />
         </div>
       </div>
