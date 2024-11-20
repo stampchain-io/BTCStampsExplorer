2694e754f611b371b6cdf7cae7cbb8cd85f32e7a
babalicious-io <baba@babalicious.io>
2024-11-20 18:34:24 +0100

    stampchain module - general styling update

--- islands/modules/StampChain.tsx
+++ islands/modules/StampChain.tsx
@@ -3,33 +3,33 @@ import { ModulesStyles } from "$islands/modules/Styles.ts";
 export const StampChainModule = () => {
   return (
     <div class="
-      grid grid-cols-1 desktop:grid-cols-3 gap-3 mobileLg:gap-6 items-end
+      grid grid-cols-1 desktop:grid-cols-3 gap-3 mobileMd:gap-6 items-end
       max-w-desktop w-full mx-auto
     ">
       <div className="col-span1 desktop:col-span-2">
-        <h1 className={ModulesStyles.title}>STAMPCHAIN</h1>
-        <h2 className={ModulesStyles.subTitle}>
+        <h1 className={ModulesStyles.titleGreyDL}>STAMPCHAIN</h1>
+        <h2 className={ModulesStyles.subTitleGrey}>
           THE CREATORS OF BITCOIN STAMPS
         </h2>
-        <p className={ModulesStyles.content}>
+        <p className={ModulesStyles.bodyTextLight}>
           Your premier destination for all things Bitcoin Stamps. As the OG
           resource, we offer unparalleled expertise and tools for the Stamps
           ecosystem.
         </p>
       </div>
 
-      <div className="flex gap-3 mobileLg:gap-6 font-extrabold justify-end">
+      <div className="flex gap-3 mobileMd:gap-6 font-extrabold justify-end">
         <a
-          href="#"
-          f-partial="#"
-          className={ModulesStyles.buttonType1 + " !w-[108px]"}
+          href="/about/#donate"
+          f-partial="/about/#donate"
+          className={ModulesStyles.buttonGreyOutline}
         >
           DONATE
         </a>
         <a
           href="/about"
           f-partial="/about"
-          className={ModulesStyles.buttonType2}
+          className={ModulesStyles.buttonGreyFlat}
         >
           ABOUT
         </a>
