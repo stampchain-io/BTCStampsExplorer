c3832abc88d5c01419639d1f4b38d3035c7d00eb
reinamora <reinamora@stampchain.io>
2024-11-21 18:10:33 +0000

    chore: issue Modules #330 USD decimal precision

--- islands/modules/GetStamping.tsx
+++ islands/modules/GetStamping.tsx
@@ -1,4 +1,5 @@
 import { ModulesStyles } from "$islands/modules/Styles.ts";
+import { formatBTCAmount } from "$lib/utils/formatUtils.ts";
 
 interface GetStampingModuleProps {
   btcPrice: number;
@@ -12,7 +13,10 @@ export const GetStampingModule = ({
   console.log("GetStampingModule received:", { btcPrice, recommendedFee });
 
   const displayPrice = typeof btcPrice === "number"
-    ? btcPrice.toLocaleString()
+    ? btcPrice.toLocaleString(undefined, {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    })
     : "0";
   const displayFee = typeof recommendedFee === "number" ? recommendedFee : "0";
 
