755358d2a47823fe399bf911a3d9a95bfcd0bd62
babalicious-io <baba@babalicious.io>
2024-11-21 21:54:05 +0100

    updated styling/spacing/columns

--- islands/stamping/src20/transfer/LatestTransfer.tsx
+++ islands/stamping/src20/transfer/LatestTransfer.tsx
@@ -9,17 +9,27 @@ const LatestTransfer = ({ transactions }) => {
     // Ensure all required properties for StampCard are included
   }));
 
+  const titlePurpleDLClassName =
+    "text-3xl mobileMd:text-4xl mobileLg:text-5xl desktop:text-6xl font-black purple-gradient1";
+  const titlePurpleLDClassName =
+    "text-3xl mobileMd:text-4xl mobileLg:text-5xl desktop:text-6xl font-black purple-gradient3";
+  const subTitlePurpleClassName =
+    "text-2xl mobileMd:text-3xl mobileLg:text-4xl desktop:text-5xl font-extralight text-stamp-purple-highlight mb-1.5 mobileLg:mb-3";
+
   return (
-    <div className="flex flex-col gap-4 items-start tablet:items-end">
-      <h1 className="purple-gradient4 text-3xl tablet:text-6xl font-black">
+    <div className="flex flex-col items-start tablet:items-end">
+      <h1 class={`${titlePurpleDLClassName} tablet:hidden`}>
+        RECENT TRANSFERS
+      </h1>
+      <h1 class={`hidden tablet:block ${titlePurpleLDClassName}`}>
         RECENT TRANSFERS
       </h1>
       {stamps.length > 0 && (
-        <p className="text-2xl tablet:text-5xl text-[#AA00FF]">
+        <h2 className={subTitlePurpleClassName}>
           BLOCK #{stamps[0].block_index}
-        </p>
+        </h2>
       )}
-      <div className="grid grid-cols-2 tablet:grid-cols-3 desktop:grid-cols-4 gap-2 tablet:gap-4">
+      <div className="grid grid-cols-4 mobileMd:grid-cols-4 mobileLg:grid-cols-6 tablet:grid-cols-4 desktop:grid-cols-4 gap-3 mobileMd:gap-6">
         {stamps.map((stamp, index) => (
           <StampCard
             key={index}
