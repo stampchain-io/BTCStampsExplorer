8eb4ea5a4c6fd787bb166e2eac1356a2cfbcdcfd
babalicious-io <baba@babalicious.io>
2024-11-21 21:54:05 +0100

    updated styling/spacing/columns

--- islands/stamping/src20/mint/PopularMinting.tsx
+++ islands/stamping/src20/mint/PopularMinting.tsx
@@ -6,13 +6,19 @@ const PopularMinting = ({ transactions }) => {
     return null;
   }
 
+  const titlePurpleDLClassName =
+    "text-3xl mobileMd:text-4xl mobileLg:text-5xl desktop:text-6xl font-black purple-gradient1";
+  const titlePurpleLDClassName =
+    "text-3xl mobileMd:text-4xl mobileLg:text-5xl desktop:text-6xl font-black purple-gradient3";
+  const subTitlePurpleClassName =
+    "text-2xl mobileMd:text-3xl mobileLg:text-4xl desktop:text-5xl font-extralight text-stamp-purple-highlight mb-1.5 mobileLg:mb-3";
+
   return (
-    <div className="flex flex-col gap-4 items-start tablet:items-end">
-      <h1 className="purple-gradient4 text-3xl tablet:text-6xl font-black">
-        TRENDING
-      </h1>
-      <p className="text-2xl tablet:text-5xl text-[#AA00FF]">POPULAR TOKENS</p>
-      <div class="w-full flex flex-col gap-4">
+    <div className="flex flex-col items-start tablet:items-end">
+      <h1 class={`${titlePurpleDLClassName} tablet:hidden`}>TRENDING</h1>
+      <h1 class={`hidden tablet:block ${titlePurpleLDClassName}`}>TRENDING</h1>
+      <h2 className={subTitlePurpleClassName}>POPULAR TOKENS</h2>
+      <div class="w-full flex flex-col gap-3 mobileMd:gap-6">
         {transactions.map((src20) => (
           src20.progress !== "100"
             ? (
