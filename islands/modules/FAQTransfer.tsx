22ad0415868175911fc1ff45fab832be5c885eae
babalicious-io <142315627+babalicious-io@users.noreply.github.com>
2024-11-21 15:44:43 +0100

    Babalicious/modules (#333)
    * updated classNames - text size/color
    
    * stampchain module - general styling update
    
    * partner module - updated classNames/padding fix/imgUrl
    
    * minor fix
    
    * faq mint - import styles/update classNames
    
    * minor code cleanup
    
    * import styles/className update/text tweaking/button url update
    
    * updated the style definitions
    
    * fix build issue
    
    ---------
    
    Co-authored-by: dev4pear <tommycole519@gmail.com>

--- islands/modules/FAQTransfer.tsx
+++ islands/modules/FAQTransfer.tsx
@@ -1,49 +1,51 @@
+import { ModulesStyles } from "$islands/modules/Styles.ts";
 import { ReadAllButton } from "$components/shared/ReadAllButton.tsx";
 
 export const FAQModule = () => {
   return (
-    <div className="flex flex-col gap-4">
-      <p className="font-black text-3xl tablet:text-6xl flex flex-col gray-gradient3 text-left">
-        HOW-TO TRANSFER
-      </p>
-      <p className="flex flex-col gray-gradient4 text-left text-2xl tablet:text-5xl font-extralight text-[#CCCCCC]">
-        BASIC STEPS
-      </p>
-
-      <p className="text-[#999999] text-base tablet:text-lg font-medium">
-        <ul className="list-decimal pl-5 space-y-2">
-          <li>
-            <b>Recepient address</b>{" "}
-            must start with 1 if classic, or with bc1q if SegWit.
-          </li>
-          <li>
-            Start typing the <b>token</b> to access to your list of tokens.
-          </li>
-          <li>
-            Enter the <b>amount</b> of tokens that you want to transfer.
-          </li>
-          <li>
-            Accept the <b>terms and conditions</b> to enable the <b>Transfer</b>
-            {" "}
-            button.
-          </li>
-          <li>
-            Accept the <b>terms and conditions</b> to enable the STAMP button.
-          </li>
-          <li>
-            The <b>Transfer</b>{" "}
-            button will submit your transaction with all the provided
-            information.
-          </li>
-        </ul>
-        <br />
-        All related costs are listed under the <b>DETAILS</b> section. <br />
-        <b>FEES</b>{" "}
-        shows the suggested amount, and you can adjust it with the slider.
-        Lowering the fee may delay your art being stamped.<br />
-        Fees are displayed in <b>BTC</b> by default, but you can switch to{" "}
-        <b>USDT</b> using the <b>TOGGLE</b>.<br />
-      </p>
+    <div className="flex flex-col gap-3 mobileMd:gap-6">
+      <div className="flex flex-col">
+        <h1 className={ModulesStyles.titleGreyDL}>HOW-TO</h1>
+        <h2 className={ModulesStyles.subTitleGrey}>TRANSFER A TOKEN</h2>
+        <p className={ModulesStyles.bodyTextLight}>
+          <ul className="list-disc pl-5 space-y-2">
+            <li>
+              The Bitcoin <b>recepient address</b>{" "}
+              must start with 1 if classic, or with bc1q if SegWit.
+            </li>
+            <li>
+              Start typing the <b>token name</b>{" "}
+              to access to your list of tokens.
+            </li>
+            <li>
+              Enter the <b>amount</b> of tokens that you want to transfer.
+            </li>
+            <li>
+              <b>Fee</b>{" "}
+              displays the suggested amount, and you can adjust it with the
+              slider.
+            </li>
+            <li>
+              A fee <b>estimate</b>{" "}
+              is displayed based on the current fee and transaction size.
+            </li>
+            <li>
+              Accept the <b>terms and conditions</b> to enable the{" "}
+              <b>transfer</b> button.
+            </li>
+            <li>
+              The <b>transfer</b>{" "}
+              button will submit your transaction with all the provided
+              information.
+            </li>
+          </ul>
+          <br />
+          Reducing the fee may slow down the transfer process.<br />
+          Fees are shown in BTC by default, but you can change to USDT using the
+          toggle switch.<br />
+          All related costs are listed in the <b>details</b> section.<br />
+        </p>
+      </div>
 
       <ReadAllButton />
     </div>
