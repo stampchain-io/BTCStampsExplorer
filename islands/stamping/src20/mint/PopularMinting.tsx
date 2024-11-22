67153e6aa2d80374d2855f7544fe463e7b158ffa
reinamora <reinamora@stampchain.io>
2024-11-22 22:52:54 +0000

    chore: shared design across stampsection, deploy, mint, transfer

--- islands/stamping/src20/mint/PopularMinting.tsx
+++ islands/stamping/src20/mint/PopularMinting.tsx
@@ -1,23 +1,25 @@
+import { ModulesStyles } from "$islands/modules/Styles.ts";
 import { SRC20TokenMintingCard } from "$islands/src20/cards/SRC20TokenMintingCard.tsx";
 import { SRC20TokenOutmintedCard } from "$islands/src20/cards/SRC20TokenOutmintedCard.tsx";
+import type { SRC20MintingProps } from "$lib/types/stamping.ts";
+import type { JSX } from "preact";
 
-const PopularMinting = ({ transactions }) => {
+export default function PopularMinting(
+  { transactions }: SRC20MintingProps,
+): JSX.Element {
   if (!transactions || transactions.length === 0) {
-    return null;
+    return <div></div>;
   }
-
-  const titlePurpleDLClassName =
-    "inline-block text-3xl mobileMd:text-4xl mobileLg:text-5xl desktop:text-6xl font-black purple-gradient1";
-  const titlePurpleLDClassName =
-    "inline-block text-3xl mobileMd:text-4xl mobileLg:text-5xl desktop:text-6xl font-black purple-gradient3";
-  const subTitlePurpleClassName =
-    "text-2xl mobileMd:text-3xl mobileLg:text-4xl desktop:text-5xl font-extralight text-stamp-purple-highlight mb-1.5 mobileLg:mb-3";
 
   return (
     <div className="flex flex-col items-start tablet:items-end">
-      <h1 class={`${titlePurpleDLClassName} tablet:hidden`}>TRENDING</h1>
-      <h1 class={`hidden tablet:block ${titlePurpleLDClassName}`}>TRENDING</h1>
-      <h2 className={subTitlePurpleClassName}>POPULAR TOKENS</h2>
+      <h1 class={`${ModulesStyles.titlePurpleDLClassName} tablet:hidden`}>
+        TRENDING
+      </h1>
+      <h1 class={`hidden tablet:block ${ModulesStyles.titlePurpleLDClassName}`}>
+        TRENDING
+      </h1>
+      <h2 className={ModulesStyles.subTitlePurple}>POPULAR TOKENS</h2>
       <div class="w-full flex flex-col gap-3 mobileMd:gap-6">
         {transactions.map((src20) => (
           src20.progress !== "100"
@@ -41,6 +43,4 @@ const PopularMinting = ({ transactions }) => {
       </div>
     </div>
   );
-};
-
-export default PopularMinting;
+}
