1a15956f9f9afcbd730f26cb439fd69f287e429c
reinamora <reinamora@stampchain.io>
2024-11-22 08:12:30 +0000

    chore: add outer div to fix scaling problem on page load

--- islands/stamp/StampContent.tsx
+++ islands/stamp/StampContent.tsx
@@ -1,7 +1,6 @@
 import { StampCard } from "$islands/stamp/StampCard.tsx";
 import { StampRow } from "globals";
 
-// FIXME: transition this to stampsection
 export function StampContent({ stamps, isRecentSales = false }: {
   stamps: (StampRow & {
     sale_data?: { btc_amount: number; block_index: number; tx_hash: string };
@@ -9,17 +8,21 @@ export function StampContent({ stamps, isRecentSales = false }: {
   isRecentSales?: boolean;
 }) {
   return (
-    <div className="grid grid-cols-2 mobileLg:grid-cols-4 gap-2 mobileLg:gap-4 tablet:gap-6 py-6 
-      transition-opacity duration-700 ease-in-out">
-      {stamps.map((stamp) => (
-        <StampCard
-          key={stamp.tx_hash}
-          stamp={stamp}
-          isRecentSale={isRecentSales}
-          showDetails={true}
-          variant="grey"
-        />
-      ))}
+    <div class="w-full pt-[2px] pb-[18px] mobileSm:pt-[2px] mobileSm:pb-[18px] 
+      mobileLg:pt-[2px] mobileLg:pb-[36px] tablet:pt-[2px] tablet:pb-[72px] 
+      desktop:pt-[2px] desktop:pb-[72px]">
+      <div class="grid grid-cols-2 mobileSm:grid-cols-2 mobileLg:grid-cols-4 w-full gap-2  mobileLg:gap-4 
+        tablet:gap-6 desktop:gap-[24px] tablet:grid-cols-3 desktop:grid-cols-4 auto-rows-fr">
+        {stamps.map((stamp) => (
+          <StampCard
+            key={stamp.tx_hash}
+            stamp={stamp}
+            isRecentSale={isRecentSales}
+            showDetails={true}
+            variant="grey"
+          />
+        ))}
+      </div>
     </div>
   );
 }
