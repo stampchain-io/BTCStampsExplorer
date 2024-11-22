5c1ba1c10a1b4a64556b9680787bea04ec2f4925
babalicious-io <baba@babalicious.io>
2024-11-23 00:32:48 +0100

    padding/column changes

--- islands/stamp/StampContent.tsx
+++ islands/stamp/StampContent.tsx
@@ -8,11 +8,8 @@ export function StampContent({ stamps, isRecentSales = false }: {
   isRecentSales?: boolean;
 }) {
   return (
-    <div class="w-full pt-[2px] pb-[18px] mobileSm:pt-[2px] mobileSm:pb-[18px] 
-      mobileLg:pt-[2px] mobileLg:pb-[36px] tablet:pt-[2px] tablet:pb-[72px] 
-      desktop:pt-[2px] desktop:pb-[72px]">
-      <div class="grid grid-cols-2 mobileSm:grid-cols-2 mobileLg:grid-cols-4 w-full gap-2  mobileLg:gap-4 
-        tablet:gap-6 desktop:gap-[24px] tablet:grid-cols-3 desktop:grid-cols-4 auto-rows-fr">
+    <div class="w-full pt-3 pb-12 mobileLg:pt-6 mobileLg:pb-[72px]">
+      <div class="grid grid-cols-3 mobileLg:grid-cols-4 tablet:grid-cols-5 desktop:grid-cols-6 gap-3 mobileMd:gap-6 w-full auto-rows-fr">
         {stamps.map((stamp) => (
           <StampCard
             key={stamp.tx_hash}
