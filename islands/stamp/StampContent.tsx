fc070bfd3f4f6456e933523c4ec530a8e91b9fcf
babalicious-io <baba@babalicious.io>
2024-11-23 18:13:20 +0100

    finetuned the cards - different colors for type+price // stamp# gradient color + hover color added

--- islands/stamp/StampContent.tsx
+++ islands/stamp/StampContent.tsx
@@ -8,8 +8,8 @@ export function StampContent({ stamps, isRecentSales = false }: {
   isRecentSales?: boolean;
 }) {
   return (
-    <div class="w-full pt-3 pb-12 mobileLg:pt-6 mobileLg:pb-[72px]">
-      <div class="grid grid-cols-3 mobileLg:grid-cols-4 tablet:grid-cols-5 desktop:grid-cols-6 gap-3 mobileMd:gap-6 w-full auto-rows-fr">
+    <div class="w-full pt-3 pb-12 mobileMd:pt-6 mobileMd:pb-[72px]">
+      <div class="grid grid-cols-2 mobileMd:grid-cols-3 mobileLg:grid-cols-4 tablet:grid-cols-5 desktop:grid-cols-6 gap-3 mobileMd:gap-6 w-full auto-rows-fr">
         {stamps.map((stamp) => (
           <StampCard
             key={stamp.tx_hash}
