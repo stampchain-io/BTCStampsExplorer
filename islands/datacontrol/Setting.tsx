9d54b8f4f051830cdc8f1a3bf911f91ac80344a0
babalicious-io <baba@babalicious.io>
2024-11-24 05:44:43 +0100

    icon buttons - color change

--- islands/datacontrol/Setting.tsx
+++ islands/datacontrol/Setting.tsx
@@ -49,7 +49,7 @@ export function Setting({
   return (
     <div
       class={`rounded-md flex flex-col items-center gap-1 h-fit relative z-[10] ${
-        open ? "px-6 py-4 border-2 border-[#660099] bg-[#0B0B0B]" : ""
+        open ? "px-6 py-4 border-2 border-[#8800CC] bg-[#0B0B0B]" : ""
       }`}
     >
       {open
@@ -68,7 +68,7 @@ export function Setting({
                 class={`cursor-pointer text-xs tablet:text-sm font-black ${
                   localFilters.includes(filter)
                     ? "text-[#AA00FF]"
-                    : "text-[#660099] hover:text-[#AA00FF]"
+                    : "text-[#8800CC] hover:text-[#AA00FF]"
                 }`}
                 onClick={() => handleFilterChange(filter)}
               >
@@ -80,8 +80,8 @@ export function Setting({
         : (
           <img
             src="/img/wallet/icon_setting.svg"
-            alt="Search icon"
-            class="bg-[#660099] rounded-md p-[12px] cursor-pointer"
+            alt="Tools icon"
+            class="bg-[#8800CC] rounded-md p-[12px] cursor-pointer"
             onClick={() => handleOpen(true)}
           />
         )}
