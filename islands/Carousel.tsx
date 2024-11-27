bfcd2f972ed9f2272e4bb16ccc404dd17228296b
itttm127 <itttm127@gmail.com>
2024-11-28 05:36:40 +0900

    feat: update pagination & make clickable

--- islands/Carousel.tsx
+++ islands/Carousel.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "preact/hooks";
 import { IS_BROWSER } from "$fresh/runtime.ts";
 import { StampRow } from "globals";
 import createCarouselSlider from "$client/utils/carousel-slider.ts";
+import { abbreviateAddress } from "$lib/utils/formatUtils.ts";
 
 interface CarouselProps {
   stamps: StampRow[];
@@ -53,40 +54,51 @@ export default function Carousel(props: CarouselProps) {
                   key={`${stamp.tx_hash}-${index}`}
                   data-hash={stamp.tx_hash}
                 >
-                  <div className="hover-gradient hover:bg-stamp-purple-bright hover:shadow-stamp p-0.5 rounded-xl">
-                    <div className="mobileLg:p-[12px] p-[6px] rounded-xl bg-stamp-card-bg hover:bg-black relative  desktop:min-h-[408px] tablet:min-h-[269px] mobileLg:min-h-[200px] mobileMd:min-h-[242px] min-h-[150px]">
-                      <img
-                        src={`/content/${stamp.tx_hash}.${extension}`}
-                        alt={`Stamp #${stamp.stamp}`}
-                        loading="lazy"
-                        class="rounded-xl object-contain cursor-pointer desktop:min-w-[408px] tablet:min-w-[269px] mobileLg:min-w-[200px] mobileMd:min-w-[242px] min-w-[150px]"
-                        onLoad={handleLoad}
-                      />
-                      {activeSlideIndex - 1 == index &&
-                        (
-                          <div
-                            id="hover"
-                            className="mobileLg:w-calc-24 w-calc-12 h-[160px] hover-dark-gradient absolute bottom-[2%] opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-end"
-                          >
-                            <div className="w-full mobileLg:pb-4 pb-1">
-                              <div className="w-full flex justify-center items-center">
-                                <h3 className="desktop:text-4xl mobileLg:text-2xl text-xl text-stamp-primary font-extrabold">
-                                  #456327
-                                </h3>
-                              </div>
-                              <div className="px-4 py-1 flex justify-between items-center">
-                                <h4 className="desktop:text-2xl mobileLg:text-lg text-base text-stamp-grey font-bold">
-                                  18she...0w4T6
-                                </h4>
-                                <h5 className="desktop:text-xl mobileLg:text-base text-sm text-stamp-grey-darker font-bold mobileMd:block hidden">
-                                  21
-                                </h5>
+                  <a href={`/stamp/${stamp.tx_hash}`}>
+                    <div className="hover-gradient hover:bg-stamp-purple-bright hover:shadow-stamp p-0.5 rounded-xl">
+                      <div className="mobileLg:p-[12px] p-[6px] rounded-xl bg-stamp-card-bg hover:bg-black relative  desktop:min-h-[408px] tablet:min-h-[269px] mobileLg:min-h-[200px] mobileMd:min-h-[242px] min-h-[150px]">
+                        <img
+                          src={`/content/${stamp.tx_hash}.${extension}`}
+                          alt={`Stamp #${stamp.stamp}`}
+                          loading="lazy"
+                          class="rounded-xl object-contain cursor-pointer desktop:min-w-[408px] tablet:min-w-[269px] mobileLg:min-w-[200px] mobileMd:min-w-[242px] min-w-[150px]"
+                          onLoad={handleLoad}
+                        />
+                        {activeSlideIndex - 1 == index &&
+                          (
+                            <div
+                              id="hover"
+                              className="mobileLg:w-calc-24 w-calc-12 h-[160px] hover-dark-gradient absolute bottom-[2%] opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-end"
+                            >
+                              <div className="w-full mobileLg:pb-4 pb-1">
+                                <div className="w-full flex justify-center items-center">
+                                  <h3 className="desktop:text-4xl mobileLg:text-2xl text-xl text-stamp-primary font-extrabold">
+                                    <span className="font-light">#</span>
+                                    <span className="font-black">
+                                      {stamp.stamp}
+                                    </span>
+                                  </h3>
+                                </div>
+                                <div className="px-4 py-1 flex justify-between items-center">
+                                  <h4 className="desktop:text-2xl mobileLg:text-lg text-base text-stamp-grey font-bold">
+                                    {stamp.creator_name
+                                      ? stamp.creator_name
+                                      : abbreviateAddress(stamp.creator, 4)}
+                                  </h4>
+                                  <h5 className="desktop:text-xl mobileLg:text-base text-sm text-stamp-grey-darker font-bold mobileMd:block hidden">
+                                    {stamp.divisible
+                                      ? (stamp.supply / 100000000).toFixed(2)
+                                      : stamp.supply > 100000
+                                      ? "+100000"
+                                      : stamp.supply}
+                                  </h5>
+                                </div>
                               </div>
                             </div>
-                          </div>
-                        )}
+                          )}
+                      </div>
                     </div>
-                  </div>
+                  </a>
                 </div>
               );
             })}
