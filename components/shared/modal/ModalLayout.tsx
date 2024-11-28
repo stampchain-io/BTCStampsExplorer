1c086fcfdf044616204327080aa2da4e1518bda1
babalicious-io <baba@babalicious.io>
2024-11-28 01:49:22 +0100

    modal master template - colors/responsiveness

--- components/shared/modal/ModalLayout.tsx
+++ components/shared/modal/ModalLayout.tsx
@@ -17,22 +17,22 @@ export function ModalLayout({
 }: ModalLayoutProps) {
   return (
     <div
-      class={`fixed inset-0 z-50 flex items-center justify-center bg-[#100019] bg-opacity-75 backdrop-filter backdrop-blur-sm ${className}`}
+      class={`fixed inset-0 z-50 flex items-center justify-center bg-[#0b0b0b] bg-opacity-85 backdrop-filter backdrop-blur-sm ${className}`}
       onClick={preventPropagation ? onClose : undefined}
     >
-      <div class="relative w-[360px] h-[600px] p-3 mobileMd:p-6 bg-[#080808] rounded-lg overflow-hidden">
+      <div class="relative w-[360px] mobileLg:w-[420px] h-[600px] mobileLg:h-[680px] p-6 dark-gradient rounded-lg overflow-hidden">
         <div
           class={`relative ${contentClassName}`}
           onClick={preventPropagation ? (e) => e.stopPropagation() : undefined}
         >
           <img
             onClick={onClose}
-            class="w-6 h-6 ms-auto cursor-pointer absolute top-0 right-0"
+            class="w-6 h-6 ms-auto cursor-pointer absolute top-0 right-0 -mr-1.5 -mt-1.5"
             alt="Close modal"
             src="/img/wallet/icon-close.svg"
           />
 
-          <p class="font-black text-4xl mobileLg:text-5xl text-center purple-gradient3 pt-3 mobileLg:pt-6 pb-6 mobileLg:pb-9">
+          <p class="font-black text-4xl mobileLg:text-5xl text-center purple-gradient3 pt-3 mobileLg:pt-6 pb-9 mobileLg:pb-12">
             {title}
           </p>
 
