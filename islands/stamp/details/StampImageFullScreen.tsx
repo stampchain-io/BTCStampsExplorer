f81572765c6a08366b10cad45568b42be5951106
babalicious-io <baba@babalicious.io>
2024-11-29 12:27:32 +0100

    full screen popup - optimising

--- islands/stamp/details/StampImageFullScreen.tsx
+++ islands/stamp/details/StampImageFullScreen.tsx
@@ -25,33 +25,34 @@ const StampImageFullScreen = ({
       onClick={handleCloseModal}
     >
       <div class="relative w-full max-w-[800px] max-h-[800px] p-6 mobileLg:p-12">
-        <div class="relative flex flex-col p-3 mobileMd:p-6 dark-gradient rounded-md overflow-hidden">
-          {contentType === "html"
-            ? (
-              <iframe
-                width="100%"
-                height="100%"
-                scrolling="no"
-                className="aspect-square rounded-sm"
-                sandbox="allow-scripts allow-same-origin"
-                src={imageUrl}
-                loading="lazy"
-                title="Stamp Preview"
-              />
-            )
-            : (
-              <img
-                className="max-w-full rounded-sm pixelart stamp-image aspect-square"
-                style={{
-                  objectFit: "contain",
-                  imageRendering: "pixelated",
-                  backgroundColor: "rgba(0,0,0,0.2)",
-                }}
-                src={imageUrl}
-                onError={handleImageError}
-                alt="Preview"
-              />
-            )}
+        <div class="flex flex-col p-3 mobileMd:p-6 dark-gradient rounded-md overflow-hidden">
+          <div class="flex flex-col stamp-container">
+            {contentType === "html"
+              ? (
+                <iframe
+                  width="100%"
+                  height="100%"
+                  scrolling="no"
+                  className="aspect-square rounded-sm"
+                  sandbox="allow-scripts allow-same-origin"
+                  src={imageUrl}
+                  loading="lazy"
+                  title="Stamp Preview"
+                />
+              )
+              : (
+                <img
+                  className="max-w-full rounded-sm pixelart stamp-image aspect-square"
+                  style={{
+                    objectFit: "contain",
+                    imageRendering: "pixelated",
+                  }}
+                  src={imageUrl}
+                  onError={handleImageError}
+                  alt="Preview"
+                />
+              )}
+          </div>
         </div>
       </div>
     </div>
