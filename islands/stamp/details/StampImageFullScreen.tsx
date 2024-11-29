55f83a8de3103f8285792488d7d5215a6caf1a7b
babalicious-io <baba@babalicious.io>
2024-11-29 12:05:15 +0100

    full creen preview - finetuning

--- islands/stamp/details/StampImageFullScreen.tsx
+++ islands/stamp/details/StampImageFullScreen.tsx
@@ -21,40 +21,37 @@ const StampImageFullScreen = ({
 
   return (
     <div
-      class="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden bg-[#181818] bg-opacity-50 backdrop-filter backdrop-blur-sm"
+      class="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden bg-[#0b0b0b] bg-opacity-95 backdrop-filter backdrop-blur-sm"
       onClick={handleCloseModal}
     >
-      <div class="relative p-4 w-full max-w-[800px] h-auto">
-        <div class="relative bg-white rounded-lg shadow overflow-hidden">
-          <div class="flex flex-col gap-4 items-center justify-between p-4 tablet:p-5 rounded-t">
-            {contentType === "html"
-              ? (
-                <iframe
-                  width="100%"
-                  height="100%"
-                  scrolling="no"
-                  className="aspect-square rounded-lg"
-                  sandbox="allow-scripts allow-same-origin"
-                  src={imageUrl}
-                  loading="lazy"
-                  title="Stamp Preview"
-                />
-              )
-              : (
-                <img
-                  className="w-[300px] tablet:w-[640px] max-w-none object-contain rounded-lg pixelart stamp-image"
-                  style={{
-                    height: "100%",
-                    objectFit: "contain",
-                    imageRendering: "pixelated",
-                    backgroundColor: "rgb(0,0,0)",
-                  }}
-                  src={imageUrl}
-                  onError={handleImageError}
-                  alt="Preview"
-                />
-              )}
-          </div>
+      <div class="relative w-full max-w-[800px] max-h-[800px] p-6 mobileLg:p-12">
+        <div class="relative flex flex-col p-3 mobileMd:p-6 dark-gradient rounded-md overflow-hidden">
+          {contentType === "html"
+            ? (
+              <iframe
+                width="100%"
+                height="100%"
+                scrolling="no"
+                className="aspect-square rounded-sm"
+                sandbox="allow-scripts allow-same-origin"
+                src={imageUrl}
+                loading="lazy"
+                title="Stamp Preview"
+              />
+            )
+            : (
+              <img
+                className="max-w-full rounded-sm pixelart stamp-image aspect-square"
+                style={{
+                  objectFit: "contain",
+                  imageRendering: "pixelated",
+                  backgroundColor: "rgba(0,0,0,0.2)",
+                }}
+                src={imageUrl}
+                onError={handleImageError}
+                alt="Preview"
+              />
+            )}
         </div>
       </div>
     </div>
