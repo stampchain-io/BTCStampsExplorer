fd62b2b10d2e8d61558d92d6dbc3bd5d87b734b7
itttm127 <itttm127@gmail.com>
2024-11-30 06:06:28 +0900

    chore: update the buy modal & autocomplete input on mint page

--- islands/Wallet/details/WalletContent.tsx
+++ islands/Wallet/details/WalletContent.tsx
@@ -596,7 +596,6 @@ export default function WalletContent(
         (
           <WalletTransferModal
             stamps={stamps}
-            src20={src20}
             toggleModal={handleOpenSettingModal}
             handleCloseModal={handleCloseSettingModal}
           />
