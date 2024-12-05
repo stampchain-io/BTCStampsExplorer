744700dee52153da9efe071881d51739d931d101
dev4pear <tommycole519@gmail.com>
2024-12-05 12:54:45 -0500

    restore classname

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -1,6 +1,13 @@
 import { Head } from "$fresh/runtime.ts";
 
 export default function Error404() {
+  const titlePurpleLD =
+    "inline-block text-3xl mobileLg:text-4xl tablet:text-5xl desktop:text-6xl font-black purple-gradient1";
+  const subTitlePurple =
+    "text-2xl mobileLg:text-3xl tablet:text-4xl desktop:text-5xl font-extralight text-stamp-purple-highlight mb-1.5 mobileLg:mb-3";
+  const buttonPurpleFlat =
+    "inline-flex items-center justify-center bg-stamp-purple border-2 border-stamp-purple rounded-md text-sm mobileLg:text-base font-extrabold text-black tracking-[0.05em] h-[42px] mobileLg:h-[48px] px-4 mobileLg:px-5 hover:border-stamp-purple-highlight hover:bg-stamp-purple-highlight transition-colors";
+
   return (
     <>
       <Head>
@@ -9,10 +16,10 @@ export default function Error404() {
       <div class="px-4 py-8 mx-auto">
         <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
           <div class="flex flex-col justify-center items-center">
-            <div class="inline-block text-3xl mobileLg:text-4xl tablet:text-5xl desktop:text-6xl font-black purple-gradient1">
+            <div class={titlePurpleLD}>
               LOOKING
             </div>
-            <div class="text-2xl mobileLg:text-3xl tablet:text-4xl desktop:text-5xl font-extralight text-stamp-purple-highlight mb-1.5 mobileLg:mb-3">
+            <div class={subTitlePurple}>
               A LITTLE LOST
             </div>
           </div>
@@ -21,14 +28,14 @@ export default function Error404() {
             src="/img/broken.png"
             alt="Bitcoin Stamps"
           />
-          <div class="text-stamp-purple-highlight desktop:text-5xl tablet:text-3xl text-2xl font-extralight">
+          <div class={subTitlePurple}>
             WANNA GO HOME
           </div>
           <div class="flex gap-3 mobileMd:gap-6 mt-6">
             <a
               href="/home"
               target="_top"
-              class="inline-flex items-center justify-center bg-stamp-purple border-2 border-stamp-purple rounded-md text-sm mobileLg:text-base font-extrabold text-black tracking-[0.05em] h-[42px] mobileLg:h-[48px] px-4 mobileLg:px-5 hover:border-stamp-purple-highlight hover:bg-stamp-purple-highlight transition-colors"
+              class={buttonPurpleFlat}
             >
               HOME
             </a>
