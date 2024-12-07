1a564ea7e82eb6267073f3187e729162897dc090
itttm127 <itttm127@gmail.com>
2024-12-08 02:47:00 +0900

    Reapply "feat: update data structure on xcp service"
    This reverts commit 942ccac486c6a2eef61e823a7e981420f1b374a4.

--- server/database/stampRepository.ts
+++ server/database/stampRepository.ts
@@ -765,15 +765,19 @@ export class StampRepository {
       const summarized = Object.keys(grouped).map(key => 
         summarize_issuances(grouped[key])
       );
-
       // Use the passed xcpBalances for quantity info
       return summarized.map((summary: StampBalance) => {
-        const xcp_balance = xcpBalances.find(
+        const xcp_balance = xcpBalances.filter(
           balance => balance.cpid === summary.cpid
-        );
+        ).reduce((acc,balance)=>acc+balance.quantity,0);
+        const utxos = xcpBalances.filter(balance => balance.cpid === summary.cpid && balance.utxo_address).map(balance => ({ quantity: balance.quantity, utxo_address: balance.utxo_address}))
+        const unbound_quantity = xcpBalances.filter(balance =>  balance.cpid === summary.cpid && !balance.utxo_address).length
         return {
           ...summary,
-          balance: xcp_balance ? xcp_balance.quantity : 0
+          unbound_quantity,
+          address:xcpBalances.length ? xcpBalances[0].address : '',
+          balance: xcp_balance ? xcp_balance : 0,
+          utxos
         };
       });
     } catch (error) {
