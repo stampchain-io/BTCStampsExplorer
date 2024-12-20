62228c0f600647dfebdbf0acbcc16c0b5c17c1d9
babalicious-io <baba@babalicious.io>
2024-12-20 17:30:13 +0100

    fee calculator - classname update

--- components/shared/fee/FeeCalculatorBase.tsx
+++ components/shared/fee/FeeCalculatorBase.tsx
@@ -145,7 +145,9 @@ export function FeeCalculatorBase({
           className="accent-stamp-purple-dark w-full h-1 mobileLg:h-1.5 rounded-lg appearance-none cursor-pointer bg-stamp-grey [&::-webkit-slider-thumb]:w-[18px] [&::-webkit-slider-thumb]:h-[18px] [&::-webkit-slider-thumb]:mobileLg:w-[22px] [&::-webkit-slider-thumb]:mobileLg:h-[22px] [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:bg-stamp-purple-dark [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:cursor-pointer [&::-moz-range-thumb]:w-[18px] [&::-moz-range-thumb]:h-[18px] [&::-moz-range-thumb]:mobileLg:w-[22px] [&::-moz-range-thumb]:mobileLg:h-[22px] [&::-moz-range-thumb]:appearance-none [&::-moz-range-thumb]:bg-stamp-purple-dark [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:border-0 [&::-moz-range-thumb]:cursor-pointer"
         />
         <div
-          className={`${tooltipFloat} ${isTooltipVisible ? "block" : "hidden"}`}
+          className={`${tooltipButtonOverflow} ${
+            isTooltipVisible ? "block" : "hidden"
+          }`}
           style={{
             left: `${tooltipPosition.x}px`,
             top: `${tooltipPosition.y - 6}px`,
@@ -255,8 +257,8 @@ export function FeeCalculatorBase({
     );
   };
 
-  const tooltipFloat =
-    "fixed text-stamp-grey-light text-[10px] mobileLg:text-xs mb-1 bg-black px-2 py-1 rounded-sm whitespace-nowrap pointer-events-none z-50";
+  const tooltipButtonOverflow =
+    "fixed bg-[#000000BF] px-2 py-1 rounded-sm text-[10px] mobileLg:text-xs text-stamp-grey-light whitespace-nowrap pointer-events-none z-50";
   const buttonPurpleOutline =
     "inline-flex items-center justify-center border-2 border-stamp-purple rounded-md text-sm mobileLg:text-base font-extrabold text-stamp-purple tracking-[0.05em] h-[42px] mobileLg:h-[48px] px-4 mobileLg:px-5 hover:border-stamp-purple-highlight hover:text-stamp-purple-highlight transition-colors";
   const buttonPurpleFlat =
@@ -386,7 +388,7 @@ export function FeeCalculatorBase({
           </div>
         )}
 
-        <div className="flex justify-end gap-6">
+        <div className="flex justify-end gap-6 pt-6 mobileLg:pt-9">
           {isModal && onCancel && (
             <button
               className={`${buttonPurpleOutline} ${
