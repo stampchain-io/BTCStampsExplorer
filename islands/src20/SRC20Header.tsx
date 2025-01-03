fa6c01fd8ff1862954ec74335b783d11e93be81c
itttm127 <itttm127@gmail.com>
2025-01-04 07:58:48 +0900

    chore: update code quality

--- islands/src20/SRC20Header.tsx
+++ islands/src20/SRC20Header.tsx
@@ -55,38 +55,38 @@ export const SRC20Header = (
   }, [filterValue]);
 
   return (
-    <div className="tabs">
-      <div class="flex flex-row justify-between items-center gap-3 w-full relative">
-        <h1 className="hidden tablet:block text-5xl desktop:text-6xl purple-gradient1 font-black">
-          SRC-20 TOKENS
-        </h1>
-        <h1 className="block tablet:hidden text-4xl mobileLg:text-5xl purple-gradient1 font-black">
-          TOKENS
-        </h1>
-        <div class="flex gap-3 justify-between mobileLg:h-9 h-7 items-center">
-          <Filter
-            initFilter={Array.isArray(filterBy) ? filterBy : [filterBy]}
-            open={isOpen1}
-            handleOpen={handleOpen1}
-            setFilterValue={setFilterValue}
-            filterButtons={[
-              "minting",
-              "trending mints",
-              "deploy",
-              "supply",
-              "marketcap",
-              "holders",
-              "volume",
-              "price change",
-            ]}
-            dropdownPosition="absolute top-[100px] right-[-79px] mobileMd:top-[96px] mobileMd:right-[-96px] mobileLg:top-[128px] mobileLg:right-[-96px] desktop:top-[125px] desktop:right-[-108px]"
-          />
-          <div class={isOpen1 ? "opacity-0 invisible" : "opacity-100"}>
-            <Sort initSort={sortBy} />
-          </div>
-          <div class={isOpen1 ? "opacity-0 invisible" : "opacity-100"}>
-            <SRC20SearchClient open2={isOpen2} handleOpen2={handleOpen2} />
-          </div>
+    <div
+      class={`relative flex flex-row justify-between items-start w-full gap-3 ${
+        isOpen1 ? "-mb-[220px] mobileMd:-mb-[216px] mobileLg:-mb-[244px]" : ""
+      }`}
+    >
+      <h1 className={`${titlePurpleDL} block mobileLg:hidden`}>TOKENS</h1>
+      <h1 className={`${titlePurpleDL} hidden mobileLg:block`}>
+        SRC-20 TOKENS
+      </h1>
+      <div class="flex relative items-start justify-between gap-3">
+        <Filter
+          setFilterValue={setFilterValue}
+          initFilter={Array.isArray(filterBy) ? filterBy : [filterBy]}
+          open={isOpen1}
+          handleOpen={handleOpen1}
+          filterButtons={[
+            "minting",
+            "trending mints",
+            "deploy",
+            "supply",
+            "marketcap",
+            "holders",
+            "volume",
+            "price change",
+          ]}
+          dropdownPosition="right-[-84px] mobileLg:right-[-96px]"
+        />
+        <div class={isOpen1 ? "opacity-0 invisible" : "opacity-100"}>
+          <Sort initSort={sortBy} />
+        </div>
+        <div class={isOpen1 ? "opacity-0 invisible" : "opacity-100"}>
+          <SRC20SearchClient open2={isOpen2} handleOpen2={handleOpen2} />
         </div>
       </div>
       {openModal &&
