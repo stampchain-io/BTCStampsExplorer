d785062d55a79644d8a3ea20047b667c145c77e7
babalicious-io <baba@babalicious.io>
2025-01-15 23:18:35 +0100

    token detail - table rows inserted / styled - infinite scroll

--- components/tokenDetails/TokenMints.tsx
+++ components/tokenDetails/TokenMints.tsx
@@ -1,6 +1,7 @@
 import { abbreviateAddress, formatDate } from "$lib/utils/formatUtils.ts";
 import { SRC20Row } from "$globals";
-import { TABLE_STYLES } from "$components/shared/types.ts";
+import { row, tableLabel, tableValue } from "$components/shared/types.ts";
+import { ScrollContainer } from "$components/shared/ScrollContainer.tsx";
 
 interface TokenMintsProps {
   mints: SRC20Row[];
@@ -8,38 +9,44 @@ interface TokenMintsProps {
 
 export function TokenMints({ mints }: TokenMintsProps) {
   return (
-    <table class="w-full">
-      <thead>
-        <tr>
-          <th class={TABLE_STYLES.header}>AMOUNT</th>
-          <th class={TABLE_STYLES.header}>ADDRESS</th>
-          <th class={TABLE_STYLES.header}>DATE</th>
-          <th class={TABLE_STYLES.header}>TX HASH</th>
-          <th class={TABLE_STYLES.header}>BLOCK</th>
-        </tr>
-      </thead>
-      <tbody>
-        {mints?.map((mint) => (
-          <tr key={mint.tx_hash} class={TABLE_STYLES.row}>
-            <td class={TABLE_STYLES.cell}>{mint.amt}</td>
-            <td class={TABLE_STYLES.cell}>
-              {abbreviateAddress(mint.destination)}
-            </td>
-            <td class={TABLE_STYLES.cell}>
-              {formatDate(new Date(mint.block_time))}
-            </td>
-            <td class={TABLE_STYLES.cell}>
-              <span
-                class="cursor-pointer hover:text-white"
-                onClick={() => navigator.clipboard.writeText(mint.tx_hash)}
-              >
-                {abbreviateAddress(mint.tx_hash)}
-              </span>
-            </td>
-            <td class={TABLE_STYLES.cell}>{mint.block_index}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div class="relative w-full">
+      <ScrollContainer>
+        <div class="w-[480px] min-[480px]:w-full">
+          <table class={tableValue}>
+            <colgroup>
+              <col className="w-[20%]" />
+              <col className="w-[20%]" />
+              <col className="w-[20%]" />
+              <col className="w-[20%]" />
+              <col className="w-[20%]" />
+            </colgroup>
+            <thead>
+              <tr>
+                <th class={`${tableLabel} text-left`}>AMOUNT</th>
+                <th class={`${tableLabel} text-center`}>ADDRESS</th>
+                <th class={`${tableLabel} text-center`}>DATE</th>
+                <th class={`${tableLabel} text-center`}>TX HASH</th>
+                <th class={`${tableLabel} text-right`}>BLOCK</th>
+              </tr>
+            </thead>
+            <tbody>
+              {mints?.map((mint) => (
+                <tr key={mint.tx_hash} class={row}>
+                  <td class="text-left">{mint.amt}</td>
+                  <td class="text-center">
+                    {abbreviateAddress(mint.destination)}
+                  </td>
+                  <td class="text-center">
+                    {formatDate(new Date(mint.block_time))}
+                  </td>
+                  <td class="text-center">{abbreviateAddress(mint.tx_hash)}</td>
+                  <td class="text-right">{mint.block_index}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </ScrollContainer>
+    </div>
   );
 }
