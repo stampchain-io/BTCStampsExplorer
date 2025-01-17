81cd83a55f75f937226ae52f626cb9f6518096be
babalicious-io <baba@babalicious.io>
2025-01-17 19:19:27 +0100

    fixed double scrollcontainer

--- components/stampDetails/StampTransfers.tsx
+++ components/stampDetails/StampTransfers.tsx
@@ -1,5 +1,4 @@
 import { abbreviateAddress, formatDate } from "$lib/utils/formatUtils.ts";
-import { ScrollContainer } from "$components/shared/ScrollContainer.tsx";
 import {
   cellAlign,
   colGroup,
@@ -26,106 +25,102 @@ export function StampTransfers({ sends }: StampTransfersProps) {
   const headers = ["FROM", "TO", "QUANTITY", "TX HASH", "DATE"];
 
   return (
-    <div class="relative w-full">
-      <ScrollContainer>
-        <div class="w-[500px] min-[500px]:w-full">
-          <table class={tableValue}>
-            <colgroup>
-              {colGroup().map((col) => (
-                <col key={col.key} className={col.className} />
-              ))}
-            </colgroup>
-            <thead>
-              <tr>
-                {headers.map((header, i) => (
-                  <th
-                    key={i}
-                    class={`${tableLabel} ${cellAlign(i, headers.length)}`}
-                  >
-                    {header}
-                  </th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              {sends?.map((send, index) => (
-                <tr key={`${send.tx_hash}-${index}`} class={row}>
-                  <td class={cellAlign(0, headers.length)}>
-                    {send.source
-                      ? (
-                        <a
-                          href={`/wallet/${send.source}`}
-                          onClick={(e) => {
-                            e.preventDefault();
-                            globalThis.location.href = `/wallet/${send.source}`;
-                          }}
-                          className={tableValueLink}
-                        >
-                          <span className="tablet:hidden">
-                            {abbreviateAddress(send.source, 4)}
-                          </span>
-                          <span className="hidden tablet:inline">
-                            {abbreviateAddress(send.source, 6)}
-                          </span>
-                        </a>
-                      )
-                      : "N/A"}
-                  </td>
-                  <td class={cellAlign(1, headers.length)}>
-                    {send.destination
-                      ? (
-                        <a
-                          href={`/wallet/${send.destination}`}
-                          onClick={(e) => {
-                            e.preventDefault();
-                            globalThis.location.href =
-                              `/wallet/${send.destination}`;
-                          }}
-                          className={tableValueLink}
-                        >
-                          <span className="tablet:hidden">
-                            {abbreviateAddress(send.destination, 4)}
-                          </span>
-                          <span className="hidden tablet:inline">
-                            {abbreviateAddress(send.destination, 6)}
-                          </span>
-                        </a>
-                      )
-                      : "N/A"}
-                  </td>
-                  <td class={cellAlign(2, headers.length)}>
-                    {send.quantity}
-                  </td>
-                  <td class={cellAlign(3, headers.length)}>
+    <div class="w-[500px] min-[500px]:w-full">
+      <table class={tableValue}>
+        <colgroup>
+          {colGroup().map((col) => (
+            <col key={col.key} className={col.className} />
+          ))}
+        </colgroup>
+        <thead>
+          <tr>
+            {headers.map((header, i) => (
+              <th
+                key={i}
+                class={`${tableLabel} ${cellAlign(i, headers.length)}`}
+              >
+                {header}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {sends?.map((send, index) => (
+            <tr key={`${send.tx_hash}-${index}`} class={row}>
+              <td class={cellAlign(0, headers.length)}>
+                {send.source
+                  ? (
                     <a
-                      href={`https://www.blockchain.com/explorer/transactions/btc/${send.tx_hash}`}
-                      target="_blank"
+                      href={`/wallet/${send.source}`}
                       onClick={(e) => {
                         e.preventDefault();
-                        globalThis.open(
-                          `https://www.blockchain.com/explorer/transactions/btc/${send.tx_hash}`,
-                          "_blank",
-                        );
+                        globalThis.location.href = `/wallet/${send.source}`;
                       }}
                       className={tableValueLink}
                     >
-                      <span class="tablet:hidden">
-                        {abbreviateAddress(send.tx_hash, 4)}
+                      <span className="tablet:hidden">
+                        {abbreviateAddress(send.source, 4)}
                       </span>
-                      <span class="hidden tablet:inline">
-                        {abbreviateAddress(send.tx_hash, 6)}
+                      <span className="hidden tablet:inline">
+                        {abbreviateAddress(send.source, 6)}
                       </span>
                     </a>
-                  </td>
-                  <td class={cellAlign(4, headers.length)}>
-                    {formatDate(new Date(send.block_time))}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </ScrollContainer>
+                  )
+                  : "N/A"}
+              </td>
+              <td class={cellAlign(1, headers.length)}>
+                {send.destination
+                  ? (
+                    <a
+                      href={`/wallet/${send.destination}`}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        globalThis.location.href =
+                          `/wallet/${send.destination}`;
+                      }}
+                      className={tableValueLink}
+                    >
+                      <span className="tablet:hidden">
+                        {abbreviateAddress(send.destination, 4)}
+                      </span>
+                      <span className="hidden tablet:inline">
+                        {abbreviateAddress(send.destination, 6)}
+                      </span>
+                    </a>
+                  )
+                  : "N/A"}
+              </td>
+              <td class={cellAlign(2, headers.length)}>
+                {send.quantity}
+              </td>
+              <td class={cellAlign(3, headers.length)}>
+                <a
+                  href={`https://www.blockchain.com/explorer/transactions/btc/${send.tx_hash}`}
+                  target="_blank"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    globalThis.open(
+                      `https://www.blockchain.com/explorer/transactions/btc/${send.tx_hash}`,
+                      "_blank",
+                    );
+                  }}
+                  className={tableValueLink}
+                >
+                  <span class="tablet:hidden">
+                    {abbreviateAddress(send.tx_hash, 4)}
+                  </span>
+                  <span class="hidden tablet:inline">
+                    {abbreviateAddress(send.tx_hash, 6)}
+                  </span>
+                </a>
+              </td>
+              <td class={cellAlign(4, headers.length)}>
+                {formatDate(new Date(send.block_time))}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
