eb10484296f87c7eae0331c45e12ded0d5292c73
babalicious-io <baba@babalicious.io>
2025-01-17 19:19:27 +0100

    fixed double scrollcontainer

--- components/stampDetails/StampListingsAll.tsx
+++ components/stampDetails/StampListingsAll.tsx
@@ -2,7 +2,6 @@ import {
   abbreviateAddress,
   formatSatoshisToBTC,
 } from "$lib/utils/formatUtils.ts";
-import { ScrollContainer } from "$components/shared/ScrollContainer.tsx";
 import {
   cellAlign,
   colGroup,
@@ -38,102 +37,97 @@ export function StampListingsAll({ dispensers }: StampListingsAllProps) {
   ];
 
   return (
-    <div class="relative w-full">
-      <ScrollContainer>
-        <div class="w-[660px] min-[660px]:w-full">
-          <table class={tableValue}>
-            <colgroup>
-              {colGroup([
-                { width: "w-[16%]" },
-                { width: "w-[10%]" },
-                { width: "w-[10%]" },
-                { width: "w-[10%]" },
-                { width: "w-[14%]" },
-                { width: "w-[26%]" },
-                { width: "w-[14%]" },
-              ]).map((col) => <col key={col.key} className={col.className} />)}
-            </colgroup>
-            <thead>
-              <tr>
-                {headers.map((header, i) => (
-                  <th
-                    key={i}
-                    class={`${tableLabel} ${cellAlign(i, headers.length)}`}
-                  >
-                    {header}
-                  </th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              {dispensers?.map((dispenser, index) => {
-                const isEmpty = dispenser.give_remaining === 0;
-                const rowDispensersRemain = `${
-                  isEmpty ? "text-stamp-grey-darker" : ""
-                } ${row}`;
+    <div class="w-[660px] min-[660px]:w-full">
+      <table class={tableValue}>
+        <colgroup>
+          {colGroup([
+            { width: "w-[16%]" },
+            { width: "w-[10%]" },
+            { width: "w-[10%]" },
+            { width: "w-[10%]" },
+            { width: "w-[14%]" },
+            { width: "w-[26%]" },
+            { width: "w-[14%]" },
+          ]).map((col) => <col key={col.key} className={col.className} />)}
+        </colgroup>
+        <thead>
+          <tr>
+            {headers.map((header, i) => (
+              <th
+                key={i}
+                class={`${tableLabel} ${cellAlign(i, headers.length)}`}
+              >
+                {header}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {dispensers?.map((dispenser, index) => {
+            const isEmpty = dispenser.give_remaining === 0;
+            const rowDispensersRemain = `${
+              isEmpty ? "text-stamp-grey-darker" : ""
+            } ${row}`;
 
-                return (
-                  <tr
-                    key={`${dispenser.tx_hash}-${index}`}
-                    class={rowDispensersRemain}
+            return (
+              <tr
+                key={`${dispenser.tx_hash}-${index}`}
+                class={rowDispensersRemain}
+              >
+                <td class={cellAlign(0, headers.length)}>
+                  {formatSatoshisToBTC(dispenser.satoshirate)}
+                </td>
+                <td class={cellAlign(1, headers.length)}>
+                  {dispenser.escrow_quantity}
+                </td>
+                <td class={cellAlign(2, headers.length)}>
+                  {dispenser.give_quantity}
+                </td>
+                <td class={cellAlign(3, headers.length)}>
+                  {dispenser.give_remaining}
+                </td>
+                <td class={cellAlign(4, headers.length)}>
+                  DISPENSER
+                </td>
+                <td class={cellAlign(5, headers.length)}>
+                  <a
+                    href={`/wallet/${dispenser.source}`}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      globalThis.location.href = `/wallet/${dispenser.source}`;
+                    }}
+                    className={tableValueLink}
                   >
-                    <td class={cellAlign(0, headers.length)}>
-                      {formatSatoshisToBTC(dispenser.satoshirate)}
-                    </td>
-                    <td class={cellAlign(1, headers.length)}>
-                      {dispenser.escrow_quantity}
-                    </td>
-                    <td class={cellAlign(2, headers.length)}>
-                      {dispenser.give_quantity}
-                    </td>
-                    <td class={cellAlign(3, headers.length)}>
-                      {dispenser.give_remaining}
-                    </td>
-                    <td class={cellAlign(4, headers.length)}>
-                      DISPENSER
-                    </td>
-                    <td class={cellAlign(5, headers.length)}>
+                    <span class="tablet:hidden">
+                      {abbreviateAddress(dispenser.source, 4)}
+                    </span>
+                    <span class="hidden tablet:inline">
+                      {abbreviateAddress(dispenser.source, 8)}
+                    </span>
+                  </a>
+                </td>
+                <td class={cellAlign(6, headers.length)}>
+                  {!dispenser.close_block_index ||
+                      dispenser.close_block_index <= 0
+                    ? (
+                      "OPEN"
+                    )
+                    : (
                       <a
-                        href={`/wallet/${dispenser.source}`}
-                        onClick={(e) => {
-                          e.preventDefault();
-                          globalThis.location.href =
-                            `/wallet/${dispenser.source}`;
-                        }}
-                        className={tableValueLink}
+                        href={`https://www.blockchain.com/explorer/transactions/btc/${dispenser.tx_hash}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        class="hover:text-stamp-purple-bright"
                       >
-                        <span class="tablet:hidden">
-                          {abbreviateAddress(dispenser.source, 4)}
-                        </span>
-                        <span class="hidden tablet:inline">
-                          {abbreviateAddress(dispenser.source, 8)}
-                        </span>
+                        CLOSED
                       </a>
-                    </td>
-                    <td class={cellAlign(6, headers.length)}>
-                      {!dispenser.close_block_index ||
-                          dispenser.close_block_index <= 0
-                        ? (
-                          "OPEN"
-                        )
-                        : (
-                          <a
-                            href={`https://www.blockchain.com/explorer/transactions/btc/${dispenser.tx_hash}`}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            class="hover:text-stamp-purple-bright"
-                          >
-                            CLOSED
-                          </a>
-                        )}
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
-      </ScrollContainer>
+                    )}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
--- components/stampDetails/StampSales.tsx
+++ components/stampDetails/StampSales.tsx
@@ -3,7 +3,6 @@ import {
   formatDate,
   formatSatoshisToBTC,
 } from "$lib/utils/formatUtils.ts";
-import { ScrollContainer } from "$components/shared/ScrollContainer.tsx";
 import {
   cellAlign,
   colGroup,
@@ -30,84 +29,80 @@ export function StampSales({ dispenses }: StampSalesProps) {
   const headers = ["FROM", "TO", "QUANTITY", "PRICE", "DATE"];
 
   return (
-    <div class="relative w-full">
-      <ScrollContainer>
-        <div class="w-[500px] min-[500px]:w-full">
-          <table class={tableValue}>
-            <colgroup>
-              {colGroup().map((col) => (
-                <col key={col.key} className={col.className} />
-              ))}
-            </colgroup>
-            <thead>
-              <tr>
-                {headers.map((header, i) => (
-                  <th
-                    key={i}
-                    class={`${tableLabel} ${cellAlign(i, headers.length)}`}
-                  >
-                    {header}
-                  </th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              {dispenses?.map((dispense, index) => (
-                <tr key={`${dispense.tx_hash}-${index}`} class={row}>
-                  <td class={cellAlign(0, headers.length)}>
-                    <a
-                      href={`/wallet/${dispense.source}`}
-                      onClick={(e) => {
-                        e.preventDefault();
-                        globalThis.location.href = `/wallet/${dispense.source}`;
-                      }}
-                      className={tableValueLink}
-                    >
-                      <span className="tablet:hidden">
-                        {abbreviateAddress(dispense.source, 4)}
-                      </span>
-                      <span className="hidden tablet:inline">
-                        {abbreviateAddress(dispense.source, 6)}
-                      </span>
-                    </a>
-                  </td>
-                  <td class={cellAlign(1, headers.length)}>
-                    <a
-                      href={`/wallet/${dispense.destination}`}
-                      onClick={(e) => {
-                        e.preventDefault();
-                        globalThis.location.href =
-                          `/wallet/${dispense.destination}`;
-                      }}
-                      className={tableValueLink}
-                    >
-                      <span className="tablet:hidden">
-                        {abbreviateAddress(dispense.destination, 4)}
-                      </span>
-                      <span className="hidden tablet:inline">
-                        {abbreviateAddress(dispense.destination, 6)}
-                      </span>
-                    </a>
-                  </td>
-                  <td class={cellAlign(2, headers.length)}>
-                    {dispense.dispense_quantity}
-                  </td>
-                  <td class={cellAlign(3, headers.length)}>
-                    {formatSatoshisToBTC(dispense.satoshirate, {
-                      includeSymbol: true,
-                      decimals: 8,
-                      stripZeros: true,
-                    })}
-                  </td>
-                  <td class={cellAlign(4, headers.length)}>
-                    {formatDate(new Date(dispense.block_time))}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </ScrollContainer>
+    <div class="w-[500px] min-[500px]:w-full">
+      <table class={tableValue}>
+        <colgroup>
+          {colGroup().map((col) => (
+            <col key={col.key} className={col.className} />
+          ))}
+        </colgroup>
+        <thead>
+          <tr>
+            {headers.map((header, i) => (
+              <th
+                key={i}
+                class={`${tableLabel} ${cellAlign(i, headers.length)}`}
+              >
+                {header}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {dispenses?.map((dispense, index) => (
+            <tr key={`${dispense.tx_hash}-${index}`} class={row}>
+              <td class={cellAlign(0, headers.length)}>
+                <a
+                  href={`/wallet/${dispense.source}`}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    globalThis.location.href = `/wallet/${dispense.source}`;
+                  }}
+                  className={tableValueLink}
+                >
+                  <span className="tablet:hidden">
+                    {abbreviateAddress(dispense.source, 4)}
+                  </span>
+                  <span className="hidden tablet:inline">
+                    {abbreviateAddress(dispense.source, 6)}
+                  </span>
+                </a>
+              </td>
+              <td class={cellAlign(1, headers.length)}>
+                <a
+                  href={`/wallet/${dispense.destination}`}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    globalThis.location.href =
+                      `/wallet/${dispense.destination}`;
+                  }}
+                  className={tableValueLink}
+                >
+                  <span className="tablet:hidden">
+                    {abbreviateAddress(dispense.destination, 4)}
+                  </span>
+                  <span className="hidden tablet:inline">
+                    {abbreviateAddress(dispense.destination, 6)}
+                  </span>
+                </a>
+              </td>
+              <td class={cellAlign(2, headers.length)}>
+                {dispense.dispense_quantity}
+              </td>
+              <td class={cellAlign(3, headers.length)}>
+                {formatSatoshisToBTC(dispense.satoshirate, {
+                  includeSymbol: true,
+                  decimals: 8,
+                  stripZeros: true,
+                })}
+              </td>
+              <td class={cellAlign(4, headers.length)}>
+                {formatDate(new Date(dispense.block_time))}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
