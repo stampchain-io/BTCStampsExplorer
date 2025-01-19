64b08e2bf1ad9f040d87a568a0c3d67718d887c2
itttm127 <itttm127@gmail.com>
2025-01-19 22:09:33 +0900

    feat: show the horizontal scrollbar on stamp table & add the text about empty data on listing, sale, transfer & add the infinite scroll function & update text about invalid date

--- components/stampDetails/StampListingsAll.tsx
+++ components/stampDetails/StampListingsAll.tsx
@@ -50,82 +50,87 @@ export function StampListingsAll({ dispensers }: StampListingsAllProps) {
             { width: "w-[14%]" },
           ]).map((col) => <col key={col.key} className={col.className} />)}
         </colgroup>
-        <thead>
-          <tr>
-            {headers.map((header, i) => (
-              <th
-                key={i}
-                class={`${tableLabel} ${cellAlign(i, headers.length)}`}
-              >
-                {header}
-              </th>
-            ))}
-          </tr>
-        </thead>
+        {dispensers.length > 0 && (
+          <thead>
+            <tr>
+              {headers.map((header, i) => (
+                <th
+                  key={i}
+                  class={`${tableLabel} ${cellAlign(i, headers.length)}`}
+                >
+                  {header}
+                </th>
+              ))}
+            </tr>
+          </thead>
+        )}
         <tbody>
-          {dispensers?.map((dispenser, index) => {
-            const isEmpty = dispenser.give_remaining === 0;
-            const rowDispensersRemain = `${
-              isEmpty ? "text-stamp-grey-darker" : ""
-            } ${row}`;
+          {dispensers.length
+            ? dispensers?.map((dispenser, index) => {
+              const isEmpty = dispenser.give_remaining === 0;
+              const rowDispensersRemain = `${
+                isEmpty ? "text-stamp-grey-darker" : ""
+              } ${row}`;
 
-            return (
-              <tr
-                key={`${dispenser.tx_hash}-${index}`}
-                class={rowDispensersRemain}
-              >
-                <td class={cellAlign(0, headers.length)}>
-                  {formatSatoshisToBTC(dispenser.satoshirate)}
-                </td>
-                <td class={cellAlign(1, headers.length)}>
-                  {dispenser.escrow_quantity}
-                </td>
-                <td class={cellAlign(2, headers.length)}>
-                  {dispenser.give_quantity}
-                </td>
-                <td class={cellAlign(3, headers.length)}>
-                  {dispenser.give_remaining}
-                </td>
-                <td class={cellAlign(4, headers.length)}>
-                  DISPENSER
-                </td>
-                <td class={cellAlign(5, headers.length)}>
-                  <a
-                    href={`/wallet/${dispenser.source}`}
-                    onClick={(e) => {
-                      e.preventDefault();
-                      globalThis.location.href = `/wallet/${dispenser.source}`;
-                    }}
-                    className={tableValueLink}
-                  >
-                    <span class="tablet:hidden">
-                      {abbreviateAddress(dispenser.source, 4)}
-                    </span>
-                    <span class="hidden tablet:inline">
-                      {abbreviateAddress(dispenser.source, 8)}
-                    </span>
-                  </a>
-                </td>
-                <td class={cellAlign(6, headers.length)}>
-                  {!dispenser.close_block_index ||
-                      dispenser.close_block_index <= 0
-                    ? (
-                      "OPEN"
-                    )
-                    : (
-                      <a
-                        href={`https://www.blockchain.com/explorer/transactions/btc/${dispenser.tx_hash}`}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        class="hover:text-stamp-purple-bright"
-                      >
-                        CLOSED
-                      </a>
-                    )}
-                </td>
-              </tr>
-            );
-          })}
+              return (
+                <tr
+                  key={`${dispenser.tx_hash}-${index}`}
+                  class={rowDispensersRemain}
+                >
+                  <td class={cellAlign(0, headers.length)}>
+                    {formatSatoshisToBTC(dispenser.satoshirate)}
+                  </td>
+                  <td class={cellAlign(1, headers.length)}>
+                    {dispenser.escrow_quantity}
+                  </td>
+                  <td class={cellAlign(2, headers.length)}>
+                    {dispenser.give_quantity}
+                  </td>
+                  <td class={cellAlign(3, headers.length)}>
+                    {dispenser.give_remaining}
+                  </td>
+                  <td class={cellAlign(4, headers.length)}>
+                    DISPENSER
+                  </td>
+                  <td class={cellAlign(5, headers.length)}>
+                    <a
+                      href={`/wallet/${dispenser.source}`}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        globalThis.location.href =
+                          `/wallet/${dispenser.source}`;
+                      }}
+                      className={tableValueLink}
+                    >
+                      <span class="tablet:hidden">
+                        {abbreviateAddress(dispenser.source, 4)}
+                      </span>
+                      <span class="hidden tablet:inline">
+                        {abbreviateAddress(dispenser.source, 8)}
+                      </span>
+                    </a>
+                  </td>
+                  <td class={cellAlign(6, headers.length)}>
+                    {!dispenser.close_block_index ||
+                        dispenser.close_block_index <= 0
+                      ? (
+                        "OPEN"
+                      )
+                      : (
+                        <a
+                          href={`https://www.blockchain.com/explorer/transactions/btc/${dispenser.tx_hash}`}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          class="hover:text-stamp-purple-bright"
+                        >
+                          CLOSED
+                        </a>
+                      )}
+                  </td>
+                </tr>
+              );
+            })
+            : "NO LISTINGS"}
         </tbody>
       </table>
     </div>
