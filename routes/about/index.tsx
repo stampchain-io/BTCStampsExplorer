03fd66d86ad3ed4dfa5523a35e84a83a8fc7c42c
babalicious-io <baba@babalicious.io>
2025-02-23 16:22:49 +0100

    about page - background image

--- routes/about/index.tsx
+++ routes/about/index.tsx
@@ -6,22 +6,40 @@ import AboutContact from "$islands/about/AboutContact.tsx";
 
 export default function About() {
   return (
-    <div className="flex flex-col gap-12 mobileLg:gap-24">
-      <AboutHeader />
+    <div>
+      <img
+        src="/img/home/stamps-collage-purpleTransparentGradient-4000.webp"
+        alt="About Bitcoin Stamps and contact Stampchain"
+        class="
+          absolute
+          top-[900px] min-[420px]:top-[700px] mobileMd:top-[550px] mobileLg:top-[350px] tablet:top-[250px] desktop:top-[300px]
+          left-0
+          w-full
+          h-[1200px] min-[420px]:h-[1200px] mobileMd:h-[1200px] mobileLg:h-[1700px] tablet:h-[1600px] desktop:h-[1500px]
+          object-cover
+          opacity-60
+          pointer-events-none
+          z-[-999]
+        "
+      />
 
-      {/* Team */}
-      <AboutTeam />
+      <div className="flex flex-col gap-12 mobileLg:gap-24">
+        <AboutHeader />
 
-      {/* Partners */}
-      <PartnersModule />
+        {/* Team */}
+        <AboutTeam />
 
-      {/* Donate */}
-      <div id="donate"></div>
-      <AboutDonate />
+        {/* Partners */}
+        <PartnersModule />
 
-      {/* Contact */}
-      <div id="contact"></div>
-      <AboutContact />
+        {/* Donate */}
+        <div id="donate"></div>
+        <AboutDonate />
+
+        {/* Contact */}
+        <div id="contact"></div>
+        <AboutContact />
+      </div>
     </div>
   );
 }
