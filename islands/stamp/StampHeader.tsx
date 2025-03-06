ecaa6366a8f5ab663400c1da97105af5c76f493d
babalicious-io <baba@babalicious.io>
2025-03-06 15:48:50 +0100

    reversal of sort button

--- islands/stamp/StampHeader.tsx
+++ islands/stamp/StampHeader.tsx
@@ -1,15 +1,12 @@
 import { useState } from "preact/hooks";
-
-import { STAMP_FILTER_TYPES, STAMP_TYPES as _STAMP_TYPES } from "$globals";
-
+import { STAMP_FILTER_TYPES } from "$globals";
+import {
+  allQueryKeysFromFilters,
+  StampFilterButton,
+} from "$islands/stamp/StampFilter.tsx";
+import { StampFilter } from "$islands/stamp/StampFilter.tsx";
+import { Sort } from "$islands/datacontrol/Sort.tsx";
 import { StampSearchClient } from "$islands/stamp/StampSearch.tsx";
-import { useNavigator as _useNavigator } from "$islands/Navigator/NavigatorProvider.tsx";
-import { Filter } from "$islands/datacontrol/Filter.tsx";
-import { FilterToggle } from "$islands/datacontrol/FilterToggle.tsx";
-import { allQueryKeysFromFilters } from "./StampFilter.tsx";
-import { flags } from "../../lib/flags/flags.ts";
-import { MultiSort } from "$islands/datacontrol/MultiSort.tsx";
-import { StampFilter } from "./StampFilter.tsx";
 
 export const StampHeader = (
   { filterBy, sortBy, search }: {
@@ -18,7 +15,6 @@ export const StampHeader = (
     search: string;
   },
 ) => {
-  const [isOpen1, setIsOpen1] = useState(false);
   const [isOpen2, setIsOpen2] = useState(false);
   const [openSearch, setOpenSearch] = useState(false);
 
@@ -26,12 +22,8 @@ export const StampHeader = (
   const filterCount = allQueryKeysFromFilters.filter((key) => {
     return searchparams.has(key) && searchparams.get(key) != "false";
   }).length;
-  const handleOpen1 = (open: boolean) => {
-    setIsOpen1(open);
-    setIsOpen2(false);
-  };
+
   const handleOpen2 = (open: boolean) => {
-    setIsOpen1(false);
     setIsOpen2(open);
   };
 
@@ -39,48 +31,19 @@ export const StampHeader = (
     "inline-block text-3xl mobileMd:text-4xl mobileLg:text-5xl font-black purple-gradient1";
 
   return (
-    <div
-      class={`relative flex flex-row justify-between items-start w-full gap-3 ${
-        isOpen1 ? "-mb-[150px] mobileMd:-mb-[146px] mobileLg:-mb-[160px]" : ""
-      }`}
-    >
+    <div class="relative flex flex-row justify-between items-start w-full gap-3">
       <h1 className={`${titlePurpleDL} block mobileLg:hidden`}>STAMPS</h1>
       <h1 className={`${titlePurpleDL} hidden mobileLg:block`}>ART STAMPS</h1>
       <div className="flex relative items-start justify-between gap-3">
-        {flags.getBooleanFlag("NEW_ART_STAMP_FILTERS", false) && (
-          <FilterToggle
-            open={openSearch}
-            setOpen={setOpenSearch}
-            count={filterCount}
-          />
-        )}
-        {!flags.getBooleanFlag("NEW_ART_STAMP_FILTERS", false)
-          ? (
-            <Filter
-              initFilter={filterBy}
-              open={isOpen1}
-              handleOpen={handleOpen1}
-              filterButtons={[
-                "pixel",
-                "vector",
-                "for sale",
-                "trending sales",
-                "sold",
-              ]}
-              dropdownPosition="right-[-84px] mobileLg:right-[-96px]"
-              open2={isOpen2}
-            />
-          )
-          : null}
-
-        <div
-          class={isOpen1 ? "opacity-0 invisible" : "opacity-100"}
-        >
-          <MultiSort searchparams={searchparams} initSort={sortBy} />
+        <StampFilterButton
+          open={openSearch}
+          setOpen={setOpenSearch}
+          searchparams={searchparams}
+        />
+        <div class="opacity-100">
+          <Sort initSort={sortBy} />
         </div>
-        <div
-          class={isOpen1 ? "opacity-0 invisible" : "opacity-100"}
-        >
+        <div class="opacity-100">
           <StampSearchClient open2={isOpen2} handleOpen2={handleOpen2} />
         </div>
       </div>
