e2b8d70cca40702a9e3dec66246326e0c5a4b76f
babalicious-io <baba@babalicious.io>
2025-05-20 14:25:24 +0200

    collection detail page - finetuned styling + added search via keyboard shortcut

--- islands/content/CollectionDetailContent.tsx
+++ islands/content/CollectionDetailContent.tsx
@@ -8,7 +8,7 @@ export const CollectionDetailContent = ({ stamps = [] }: {
 }) => {
   return (
     <div name="stamps">
-      <div className="grid grid-cols-2 mobileMd:grid-cols-3 tablet:grid-cols-4 desktop:grid-cols-5 gap-6 transition-opacity duration-700 ease-in-out">
+      <div className="grid grid-cols-2 mobileMd:grid-cols-3 tablet:grid-cols-4 desktop:grid-cols-5 gap-6 transition-colors ease-in-out duration-100">
         {stamps.map((stamp: StampRow) => (
           <StampCard
             key={stamp.tx_hash}
