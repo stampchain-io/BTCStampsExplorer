45d72503b4304cb327d820adfdb9aef7522758f0
babalicious-io <baba@babalicious.io>
2025-05-21 23:10:00 +0200

    src20 overview - finetuning of spacing

--- components/button/ButtonOLD.tsx
+++ components/button/ButtonOLD.tsx
@@ -18,7 +18,7 @@ interface ButtonProps
 
 const ICON_STYLE = "flex items-end justify-end group cursor-pointer";
 const ICON_CLASS =
-  "w-5 h-5 mt-1.5 tablet:mt-2 fill-stamp-purple group-hover:fill-stamp-purple-bright cursor-pointer";
+  "w-5 h-5 mt-2 fill-stamp-purple group-hover:fill-stamp-purple-bright cursor-pointer";
 
 export function Button({
   icon,
