3c122c1f267a1c9f35b1c30cb2cdcd3595b7a1b4
babalicious-io <baba@babalicious.io>
2025-05-21 22:44:47 +0200

    tokencards - no tokens text style update

--- components/card/SRC20CardSmMinting.tsx
+++ components/card/SRC20CardSmMinting.tsx
@@ -1,7 +1,7 @@
 /* reinamora - update Trending calculations */
 import { unicodeEscapeToEmoji } from "$lib/utils/emojiUtils.ts";
 import { Timeframe } from "$layout";
-import { labelXs, textSm, valueDark } from "$text";
+import { labelXs, textSm, valueDarkSm } from "$text";
 import { Button } from "$button";
 import { cellAlign, colGroup } from "$components/layout/types.ts";
 import {
@@ -211,7 +211,7 @@ export function SRC20CardSmMinting({
           )
           : (
             <tr>
-              <td colSpan={headers.length} class={`${valueDark} w-full`}>
+              <td colSpan={headers.length} class={`${valueDarkSm} w-full`}>
                 NO MINTING TOKENS
               </td>
             </tr>
