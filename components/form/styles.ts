e58dd8e091f4d94f265479c48855150987fd4826
babalicious-io <baba@babalicious.io>
2025-08-11 22:32:11 +0200

    refactor: enhance form and layout styles for improved visual consistency
    Updated input field focus styles and adjusted glassmorphism layer properties for better visual coherence. Modified hover effects in image preview components to align with the new design standards, ensuring a consistent user experience across the application.

--- components/form/styles.ts
+++ components/form/styles.ts
@@ -9,7 +9,7 @@ const inputFieldWidth = "!w-10";
 // Input field styles
 const inputFieldStyle = `p-3 w-full
   ${glassmorphismLayer2}
-  focus:[#171417]/40 focus:border-[#171417] focus:outline-none focus-visible:outline-none no-outline
+  focus:[#1d191d] focus:border-[#33333380] focus:outline-none focus-visible:outline-none no-outline
   font-medium text-sm text-stamp-grey-light
   placeholder:font-light placeholder:text-stamp-grey placeholder:uppercase`;
 
