39b6e97a7ea6e39b5744d8f5c46f9b568243bf53
babalicious-io <baba@babalicious.io>
2026-07-20 14:51:33 +0200

    refactor: update author component styles for improved consistency
    - Replaced labelSm with labelXs and textLg with text in the AuthorBase component for better alignment with updated text styles.
    - Adjusted class names to enhance visual consistency and maintainability across the author section.

--- components/section/howto/AuthorBase.tsx
+++ components/section/howto/AuthorBase.tsx
@@ -1,7 +1,7 @@
 /* ===== AUTHOR COMPONENT ===== */
 import { Icon } from "$icon";
 import { container2 } from "$layout";
-import { labelSm, textLg } from "$text";
+import { labelXs, text } from "$text";
 import type { AuthorProps } from "$types/ui.d.ts";
 
 /* ===== TYPES ===== */
@@ -14,11 +14,11 @@ export function AuthorSection({ name, twitter, website }: AuthorProps) {
         class={`flex min-[520px]:flex-col justify-between min-[520px]:justify-end items-start min-[520px]:items-end w-full min-[520px]:w-fit -mt-4 min-[520px]:ml-auto p-3 ${container2}`}
       >
         <div class="flex flex-col min-[520px]:items-end min-[520px]:mb-1">
-          <h6 class={labelSm}>
+          <h6 class={labelXs}>
             ARTICLE BY
           </h6>
           <h5
-            class={`${textLg} tablet:text-base !font-semibold !text-color-grey-semilight min-[520px]:mb-1`}
+            class={`${text} tablet:text-base !font-semibold !text-color-neutral-400 min-[520px]:mb-1`}
           >
             {name}
           </h5>
